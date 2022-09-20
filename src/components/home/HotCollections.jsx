7206b155b36739cde167de0b5680bda9af875c58
Rogers Kibuule <kibuulerogers58@gmail.com>
2022-09-20 20:36:48 +0300

    Pulled data from Api and rendered

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+  
 
 const HotCollections = () => {
+
+
+  const [data, setData] = useState([])
+
+   useEffect(() => {
+    axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections")
+    .then(response => {
+    console.log(response.data)
+      setData([...response.data])})
+   },[])
+
+   console.log(data)
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,30 +26,34 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
+          {/* {new Array(4).fill(0).map((_, index) => ( */ data.map((data, index) => ( 
             <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
               <div className="nft_coll">
+                {/* <div>{JSON.stringify(data)}</div> */}
+                {/* {data.map((item) => <div>{item.title</div>               )} */}
+                <div></div>
                 <div className="nft_wrap">
                   <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
+                    <img src={data.nftImage} className="lazy img-fluid" alt="" />
                   </Link>
                 </div>
                 <div className="nft_coll_pp">
                   <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
+                    <img className="lazy pp-coll" src={data.authorImage} alt="" />
                   </Link>
                   <i className="fa fa-check"></i>
                 </div>
                 <div className="nft_coll_info">
                   <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
+                    <h4>{data.title}</h4>
                   </Link>
-                  <span>ERC-192</span>
+                  <span>{data.code}</span>
                 </div>
               </div>
             </div>
           ))}
         </div>
+        {/* <button onClick={getHotcollections}>testing</button> */}
       </div>
     </section>
   );
