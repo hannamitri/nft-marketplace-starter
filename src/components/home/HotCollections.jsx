65f8887ef1df8665cb10d7ba43a585f9333c7d80
Shivom <shivompaudel@icloud.com>
2022-10-04 17:02:26 -0500

    hot collections dynamic/responsive, implemented carousel

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import { useEffect, useState} from "react";
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
+import Skeleton from "../UI/Skeleton";
+
 
 const HotCollections = () => {
+  const [details, setDetails] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  async function getData(){
+    setLoading(true)
+    const { data } = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections`)
+    setLoading(false)
+    setDetails(data)
+  }
+  useEffect(() => {
+    getData()
+  }, [])
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,33 +30,61 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
-                </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <span>ERC-192</span>
-                </div>
-              </div>
-            </div>
-          ))}
+          {loading ? (
+            <Skeleton/>
+          ) : (
+            <Splide options={{
+              perPage: 4,
+              breakpoints: {
+                1400: {
+                  perPage: 3,
+                },
+                995: {
+                  perPage: 2,
+                },
+                770: {
+                  perPage: 1,
+                }
+              },
+              type: "loop",
+              perMove: 1,
+              pagination: false,
+              focus: 0,
+            }}>
+              {new Array(1).fill(0).map((_, detail) => (
+                (details.map(detail => (
+
+                  <SplideSlide>
+                      <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={detail.id}>
+                        <div className="nft_coll" style={{width: "18.5rem"}}>
+                          <div className="nft_wrap">
+                            <Link to="/item-details">
+                              <img src={detail.nftImage} className="lazy img-fluid" alt="" />
+                            </Link>
+                          </div>
+                          <div className="nft_coll_pp">
+                            <Link to="/author">
+                              <img className="lazy pp-coll" src={detail.authorImage} alt="" />
+                            </Link>
+                            <i className="fa fa-check"></i>
+                          </div>
+                          <div className="nft_coll_info">
+                            <Link to="/explore">
+                              <h4>{detail.title}</h4>
+                            </Link>
+                            <span>ERC-{detail.code}</span>
+                          </div>
+                        </div>
+                      </div>
+                  </SplideSlide>
+                )))
+              ))}
+            </Splide>
+          )}
         </div>
       </div>
     </section>
-  );
+  )
 };
 
 export default HotCollections;
