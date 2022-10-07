92a8b85fab4f1aef16366198a36c6681499312a6
Dellano Blake <dellano.blake@outlook.com>
2022-10-06 19:52:41 -0500

    installed axios, hit backend API, made hotcollections dynamic, implemented responsive carousel, added skeleton loading state

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,72 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import "keen-slider/keen-slider.min.css"
+import { useKeenSlider } from 'keen-slider/react'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import Skeleton from "../UI/Skeleton";
 
 const HotCollections = () => {
+  const axios = require('axios').default;
+  const [loading, setLoading] = useState(false);
+  const [options, setOptions] = useState({});
+  const [collectionsData, setCollectionsData] = useState([]);
+  const [sliderRef, instanceRef] = useKeenSlider(options)
+
+  function Arrow(props) {
+    return (
+      <svg
+        onClick={props.onClick}
+        className={`arrow ${props.left ? "arrow-left" : "arrow-right"}`}
+      >
+        {props.left && <ChevronLeftIcon />}
+        {!props.left && <ChevronRightIcon />}
+      </svg>
+    );
+  }
+
+  async function fetchData() {
+    setLoading(true)
+    const response = await axios.get('https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections#')
+    setCollectionsData(response.data);
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchData();
+    setOptions({
+      loop: true,
+      mode: "free-snap",
+      breakpoints: {
+        "(min-width: 0px)": {
+          slides: {
+            perView: 1,
+          },
+        },
+        "(min-width: 600px)": {
+          slides: {
+            perView: 2,
+            spacing: 12,
+          },
+        },
+        "(min-width: 900px)": {
+          slides: {
+            perView: 3,
+            spacing: 24,
+          },
+        },
+        "(min-width: 1200px)": {
+          slides: {
+            perView: 4,
+            spacing: 24,
+          },
+        },
+      },
+    });
+  }, []);
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,29 +77,70 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
-                </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <span>ERC-192</span>
-                </div>
+          {loading ? (
+            <div className="navigation-wrapper">
+              <div ref={sliderRef} className="keen-slider">
+                {new Array(6).fill(0).map((_, index) => (
+                  <div className="nft_coll keen-slider__slide" key={index}>
+                    <div className="nft_wrap">
+                      <Link to="/item-details">
+                        <Skeleton width="100%" height="195px" />
+                      </Link>
+                    </div>
+                    <div className="nft_coll_pp">
+                      <Link to="/author">
+                        <Skeleton width="50px" height="50px" borderRadius="50%" />
+                      </Link>
+                      <i className="fa fa-check"></i>
+                    </div>
+                    <div className="nft_coll_info">
+                      <Link to="/explore">
+                        <Skeleton width="100px" height="20px" />
+                      </Link>
+                      <div></div>
+                      <Skeleton width="69px" height="20px" />
+                    </div>
+                  </div>
+                ))}
               </div>
+              <>
+                <Arrow left onClick={() => instanceRef.current?.prev()} />
+                <Arrow onClick={() => instanceRef.current?.next()} />
+              </>
             </div>
-          ))}
+          ) : (
+            <div className="navigation-wrapper">
+              <div ref={sliderRef} className="keen-slider">
+                {collectionsData.map((item) => (
+                  <div className="keen-slider__slide" key={item.id}>
+                    <div className="nft_coll">
+                      <div className="nft_wrap">
+                        <Link to="/item-details">
+                          <img src={item.nftImage} className="lazy img-fluid" alt="" />
+                        </Link>
+                      </div>
+                      <div className="nft_coll_pp">
+                        <Link to="/author">
+                          <img className="lazy pp-coll" src={item.authorImage} alt="" />
+                        </Link>
+                        <i className="fa fa-check"></i>
+                      </div>
+                      <div className="nft_coll_info">
+                        <Link to="/explore">
+                          <h4>{item.title}</h4>
+                        </Link>
+                        <span>ERC-{item.code}</span>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <>
+                <Arrow left onClick={() => instanceRef.current?.prev()} />
+                <Arrow onClick={() => instanceRef.current?.next()} />
+              </>
+            </div>
+          )}
         </div>
       </div>
     </section>
