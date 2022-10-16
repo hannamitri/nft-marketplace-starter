686da200c7bf05e8347dfb7e82fa64fa213800b9
AniqKhan03 <aniqahmedkhan123@gmail.com>
2022-10-16 15:37:04 -0400

    removed unneccesary import

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -2,7 +2,6 @@ import axios from "axios";
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
 import Skeleton from "../UI/Skeleton";
 
 const TopSellers = () => {
