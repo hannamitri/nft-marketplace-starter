b7d08efa623439cbd63058f7f4d567347b85bace
hannamitri <hannamitri7@gmail.com>
2022-10-29 10:38:25 +0300

    bug fix

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -36,21 +36,13 @@ const NewItems = () => {
                       <button>Buy Now</button>
                       <div className="nft__item_share">
                         <h4>Share</h4>
-                        <a
-                          href="https://www.facebook.com/sharer/sharer.php?u=https://gigaland.io"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
+                        <a href="" target="_blank" rel="noreferrer">
                           <i className="fa fa-facebook fa-lg"></i>
                         </a>
-                        <a
-                          href="https://twitter.com/intent/tweet?url=https://gigaland.io"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
+                        <a href="" target="_blank" rel="noreferrer">
                           <i className="fa fa-twitter fa-lg"></i>
                         </a>
-                        <a href="mailto:?subject=I wanted you to see this site&amp;body=Check out this site https://gigaland.io">
+                        <a href="">
                           <i className="fa fa-envelope fa-lg"></i>
                         </a>
                       </div>
