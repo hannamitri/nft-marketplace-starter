62262cfb405922533cbca73c338f3d6bbc5bdee7
paul serafim <paul.serafim.3021@gmail.com>
2022-10-31 15:07:37 +1100

    from main branch

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import ItemDetails from "./pages/ItemDetails";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 
+//  from main !! DO NOT REMOVE
+
 function App() {
   return (
     <Router>
