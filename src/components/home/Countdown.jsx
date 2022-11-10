424e63d7ee29b738d79111cb15a0a5168cc0b2ae
KMalizei <kkouroshm@yahoo.com>
2022-11-09 22:16:00 -0700

    Dynamic, Skeleton loading state, Dynamic countdown, Carousel

--- src/components/home/Countdown.jsx
+++ src/components/home/Countdown.jsx
@@ -40,42 +40,4 @@ const Countdown = ({expiryDate}) => {
 
 
 
-
-// export default Countdown
-
-// import React, { useState } from 'react'
-// import { useEffect } from 'react'
-
-// function Countdown({ expiryDate }) {
-//     const [text, setText] = useState(undefined)
-
-
-//     useEffect(() => {
-//         setInterval(() => {
-//             convertTime()
-//         })
-//     }, [])
-
-//     function convertTime(){
-//         const timeLeft = expiryDate - Date.now()
-//         if (timeLeft < 0){
-//             setText(undefined)
-//         }
-//         else{
-//             const seconds = Math.floor(timeLeft / 1000)
-//             const minutes = Math.floor(seconds / 60 % 60)
-//             const hours = Math.floor(seconds / 60 / 60)
-
-//             setText(`${hours.toFixed(0)}h ${minutes.toFixed(0)}m ${(seconds % 60).toFixed(0)}s `)
-//         }
-
-//     }
-
-//   return (
-//     <span>
-//       {text}
-//     </span>
-//   )
-// }
-
 export default Countdown
