606bca374aa5e9bd823b802966e5fd97691e7c9a
Anson Chow <108892941+Anson-Chow@users.noreply.github.com>
2022-11-11 23:08:14 -0500

    Revert "Anson new items"

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -39,13 +39,21 @@ const ExploreItems = () => {
                   <button>Buy Now</button>
                   <div className="nft__item_share">
                     <h4>Share</h4>
-                    <a href="" target="_blank" rel="noreferrer">
+                    <a
+                      href="https://www.facebook.com/sharer/sharer.php?u=https://gigaland.io"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
                       <i className="fa fa-facebook fa-lg"></i>
                     </a>
-                    <a href="" target="_blank" rel="noreferrer">
+                    <a
+                      href="https://twitter.com/intent/tweet?url=https://gigaland.io"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
                       <i className="fa fa-twitter fa-lg"></i>
                     </a>
-                    <a href="">
+                    <a href="mailto:?subject=I wanted you to see this site&amp;body=Check out this site https://gigaland.io">
                       <i className="fa fa-envelope fa-lg"></i>
                     </a>
                   </div>
