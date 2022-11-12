69d4df03f099fd67c1f85ed9df726c75fef38ea9
Anson <chow.anson25@gmail.com>
2022-11-12 18:26:26 -0500

    Revert "exploreItems"
    This reverts commit 710f38e2d2721d3329a63754a87f3feaa7551d90.

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,104 +1,86 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
-import Countdown from "../UI/Countdown";
-import NFTcard from "../UI/NFTcard";
-import Skeleton from "../UI/Skeleton";
+import AuthorImage from "../../images/author_thumbnail.jpg";
+import nftImage from "../../images/nftImage.jpg";
 
 const ExploreItems = () => {
-  const [exploreItems, setExporeItems] = useState();
-  const [loading, setLoading] = useState();
-  const [loadIndex, setLoadIndex] = useState(8);
-
-  function addToIndex() {
-    setLoadIndex((prevIndex) => prevIndex + 4);
-  }
-
-  async function exploreData() {
-    setLoading(true);
-    const { data } = await axios.get(
-      "https://us-central1-nft-cloud-functions.cloudfunctions.net/explore"
-    );
-    setExporeItems(data);
-    setLoading(false);
-    console.log(exploreItems);
-  }
-
-  useEffect(() => {
-    exploreData();
-  }, []);
-
-  async function filterNFTS(filter) {
-    setLoading(false)
-    if (filter === "Sort") {
-      const { data } = await axios.get(
-        "https://us-central1-nft-cloud-functions.cloudfunctions.net/explore"
-      );
-      setExporeItems(data);
-    }
-    if (filter === "price_low_to_high") {
-      const { data } = await axios.get(
-        "https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=price_low_to_high"
-      );
-      setExporeItems(data);
-    }
-    if (filter === "price_high_to_low") {
-      const { data } = await axios.get(
-        "https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=price_high_to_low"
-      );
-      setExporeItems(data);
-    }
-    if (filter === 'likes_high_to_low') {
-      const { data } = await axios.get(
-        "https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=likes_high_to_low"
-      );
-      setExporeItems(data);
-    }
-  }
-
   return (
     <>
       <div>
-        <select
-          id="filter-items"
-          defaultValue="Default"
-          onChange={(event) => filterNFTS(event.target.value)}
-        >
-          <option value="Default" disabled>
-            Default
-          </option>
+        <select id="filter-items" defaultValue="">
+          <option value="">Default</option>
           <option value="price_low_to_high">Price, Low to High</option>
           <option value="price_high_to_low">Price, High to Low</option>
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {!loading
-        ? exploreItems
-            ?.slice(0, loadIndex)
-            .map((data) => <NFTcard data={data} />)
-        : new Array(8).fill(0).map((_, index) => (
-            <div
-              key={index}
-              className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-              style={{ display: "block", backgroundSize: "cover" }}
-            >
-              <Skeleton width={335} height={420} borderRadius={12} />
+      {new Array(8).fill(0).map((_, index) => (
+        <div
+          key={index}
+          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+          style={{ display: "block", backgroundSize: "cover" }}
+        >
+          <div className="nft__item">
+            <div className="author_list_pp">
+              <Link
+                to="/author"
+                data-bs-toggle="tooltip"
+                data-bs-placement="top"
+              >
+                <img className="lazy" src={AuthorImage} alt="" />
+                <i className="fa fa-check"></i>
+              </Link>
             </div>
-          ))}
+            <div className="de_countdown">5h 30m 32s</div>
 
-      {loadIndex != 16 && (
-        <div className="col-md-12 text-center">
-          <button
-            id="loadmore"
-            className="btn-main lead"
-            onClick={() => {
-              addToIndex();
-            }}
-          >
-            Load more
-          </button>
+            <div className="nft__item_wrap">
+              <div className="nft__item_extra">
+                <div className="nft__item_buttons">
+                  <button>Buy Now</button>
+                  <div className="nft__item_share">
+                    <h4>Share</h4>
+                    <a
+                      href="https://www.facebook.com/sharer/sharer.php?u=https://gigaland.io"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <i className="fa fa-facebook fa-lg"></i>
+                    </a>
+                    <a
+                      href="https://twitter.com/intent/tweet?url=https://gigaland.io"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <i className="fa fa-twitter fa-lg"></i>
+                    </a>
+                    <a href="mailto:?subject=I wanted you to see this site&amp;body=Check out this site https://gigaland.io">
+                      <i className="fa fa-envelope fa-lg"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <Link to="/item-details">
+                <img src={nftImage} className="lazy nft__item_preview" alt="" />
+              </Link>
+            </div>
+            <div className="nft__item_info">
+              <Link to="/item-details">
+                <h4>Pinky Ocean</h4>
+              </Link>
+              <div className="nft__item_price">1.74 ETH</div>
+              <div className="nft__item_like">
+                <i className="fa fa-heart"></i>
+                <span>69</span>
+              </div>
+            </div>
+          </div>
         </div>
-      )}
+      ))}
+      <div className="col-md-12 text-center">
+        <Link to="" id="loadmore" className="btn-main lead">
+          Load more
+        </Link>
+      </div>
     </>
   );
 };
