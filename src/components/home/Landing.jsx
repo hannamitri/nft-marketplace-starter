46e9a860db52cb76bcb2ed9c8da5f5c07643d6e7
KMalizei <kkouroshm@yahoo.com>
2022-11-14 18:24:17 -0700

    Animations for Landing page

--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -18,24 +18,24 @@ const Landing = () => {
             <div className="col-md-6">
               <div className="spacer-single"></div>
               <h6>
-                <span className="text-uppercase id-color-2">
+                <span className="text-uppercase id-color-2" data-aos="fade-up" data-aos-duration="1500">
                   Ultraverse Market
                 </span>
               </h6>
-              <div className="spacer-10"></div>
-              <h1>Create, sell or collect digital items.</h1>
-              <p className="lead">
+              <div className="spacer-10 " data-aos="fade-up" data-aos-duration="1500"></div>
+              <h1 data-aos="fade-up" data-aos-duration="1500">Create, sell or collect digital items.</h1>
+              <p className="lead" data-aos="fade-up" data-aos-duration="1500">
                 Unit of data stored on a digital ledger, called a blockchain,
                 that certifies a digital asset to be unique and therefore not
                 interchangeable
               </p>
-              <div className="spacer-10"></div>
-              <Link className="btn-main lead" to="/explore">
+              <div className="spacer-10" data-aos="fade-up" data-aos-duration="1500"></div>
+              <Link className="btn-main lead" to="/explore" data-aos="fade-in" data-aos-duration="2000" data-aos-delay="500">
                 Explore
               </Link>
-              <div className="mb-sm-30"></div>
+              <div className="mb-sm-30" data-aos="fade-up" data-aos-duration="1500"></div>
             </div>
-            <div className="col-md-6 xs-hide">
+            <div className="col-md-6 xs-hide" data-aos="fade-up" data-aos-duration="1500">
               <img src={NFT} className="lazy img-fluid" alt="" />
             </div>
           </div>
