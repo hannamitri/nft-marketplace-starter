1e87aa2a72a0da1253960900f42580b2a8c079ea
gflo1997 <gonzalo.fj@hotmail.com>
2022-11-17 13:16:41 -0500

    Hot Collections

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -121,7 +121,6 @@ const HotCollections = () => {
                             alt=""
                           />
                         </Link>
-                        <i className="fa fa-check"></i>
                       </div>
                       <div className="nft_coll_info">
                         <Link to="/explore">
