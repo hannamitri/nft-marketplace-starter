a45fb092c76ed29313e0f010f70bb588f74868b1
Misadah Ali <75273707+mali1930@users.noreply.github.com>
2022-11-26 18:40:19 -0600

    test commit

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -7,6 +7,8 @@ const Explore = () => {
     window.scrollTo(0, 0);
   }, []);
 
+  //does this work?
+  //does this work?
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
