0b7e32de0aa186dd895879814bdf521213637649
Sahdif <sahdif.islam3@gmail.com>
2022-11-28 23:12:46 +1100

    Animation-feature

--- src/components/home/LandingIntro.jsx
+++ src/components/home/LandingIntro.jsx
@@ -7,36 +7,36 @@ const LandingIntro = () => {
         <div className="row">
           <div className="col-lg-4 col-md-6 mb-sm-30">
             <div className="feature-box f-boxed style-3">
-              <i className="bg-color-2 i-boxed icon_wallet"></i>
+            <i className="bg-color-2 i-boxed icon_wallet" data-aos="fade-in" data-aos-duration="1000"></i>
               <div className="text">
-                <h4 className="">Set up your wallet</h4>
-                <p>
+              <h4 className="" data-aos="fade-in" data-aos-duration="1000">Set up your wallet</h4>
+                <p data-aos="fade-in" data-aos-duration="1000">
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                   accusantium doloremque laudantium, totam rem.
                 </p>
               </div>
-              <i className="wm icon_wallet"></i>
+              <i className="wm icon_wallet" data-aos="fade-in" data-aos-duration="1000"></i>
             </div>
           </div>
           <div className="col-lg-4 col-md-6 mb-sm-30">
             <div className="feature-box f-boxed style-3">
-              <i className="bg-color-2 i-boxed icon_cloud-upload_alt"></i>
+            <i className="bg-color-2 i-boxed icon_cloud-upload_alt" data-aos="fade-in" data-aos-duration="1000"></i>
               <div className="text">
-                <h4 className="">Add your NFT's</h4>
-                <p>
+              <h4 className=""  data-aos="fade-in" data-aos-duration="1000">Add your NFT's</h4>
+                <p data-aos="fade-in" data-aos-duration="1000">
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                   accusantium doloremque laudantium, totam rem.
                 </p>
               </div>
-              <i className="wm icon_cloud-upload_alt"></i>
+              <i className="wm icon_cloud-upload_alt" data-aos="fade-in" data-aos-duration="1000"></i>
             </div>
           </div>
           <div className="col-lg-4 col-md-6 mb-sm-30">
             <div className="feature-box f-boxed style-3">
-              <i className="bg-color-2 i-boxed icon_tags_alt"></i>
+            <i className="bg-color-2 i-boxed icon_tags_alt" data-aos="fade-in" data-aos-duration="1000"></i>
               <div className="text">
-                <h4 className="">Sell your NFT's</h4>
-                <p>
+              <h4 className=""  data-aos="fade-in" data-aos-duration="1000">Sell your NFT's</h4>
+                <p data-aos="fade-in" data-aos-duration="1000">
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                   accusantium doloremque laudantium, totam rem.
                 </p>
