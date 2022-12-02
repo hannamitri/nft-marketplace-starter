a284f0b67c0ad5fc8b70102bfa99ed1f087becb2
LPedroSF <59021127+LPedroSF@users.noreply.github.com>
2022-12-02 13:48:15 +0000

    added a carousel to hot collections

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,51 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import { render } from "@testing-library/react";
+import axios from "axios";
+import React, { useEffect, useState, useRef} from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import OwlCarousel from 'react-owl-carousel';
+import Skeleton from "../UI/Skeleton";
+
 
 const HotCollections = () => {
+  const { nftId } = useParams();
+  const [posts, setPosts] = useState([]);
+  const settings = {
+    responsive:{
+      500: {
+        items: 2
+      },
+      900: {
+        items:3
+      },
+      1200: {
+        items:4
+      }
+    }
+  }
+  const[img, setImg] = useState();
+  const mountedRef = useRef(true);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const { data } = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections/nftId=${nftId}`)
+      setPosts(data);
+    }
+    fetchPosts();    
+  },[]);
+
+  useEffect(() => {
+    const image = new Image();
+    image.src = posts.nftImage;
+    image.onload = () => {
+      if(mountedRef.current){
+        setImg(image);
+      }
+    };
+    return () => {
+      mountedRef.current = false;
+    }
+  });
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,29 +56,71 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
-                </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <span>ERC-192</span>
-                </div>
-              </div>
-            </div>
-          ))}
+          {
+            img ? 
+              <>
+                <OwlCarousel
+                  loop
+                  margin={10}
+                  nav
+                  {...settings}
+                >
+                  {posts.map((post) => (
+                      <div className="nft_coll">
+                        <div className="nft_wrap">
+                          <Link to={`/item-details/`}>
+                            <Skeleton />
+                          </Link>
+                        </div>
+                        <div className="nft_coll_pp">
+                          <Link to="/author">
+                            <Skeleton />
+                          </Link>
+                          <i className="fa fa-check"></i>
+                        </div>
+                        <div className="nft_coll_info">
+                          <Link to="/explore">
+                            <Skeleton />
+                          </Link>
+                          <span>ERC-{post.code}</span>
+                        </div>
+                      </div>
+                  ))} 
+                </OwlCarousel> 
+              </>
+            : <>
+              <OwlCarousel
+                loop
+                margin={10}
+                nav
+                {...settings}
+              >
+                {posts.map((post) => (
+                    <div className="nft_coll">
+                      <div className="nft_wrap">
+                        <Link to={`/item-details/`}>
+                          <img src={post.nftImage} className="lazy img-fluid" alt=""/>
+                        </Link>
+                      </div>
+                      <div className="nft_coll_pp">
+                        <Link to="/author">
+                          <img className="lazy pp-coll" src={post.authorImage} alt=""/>
+                        </Link>
+                        <i className="fa fa-check"></i>
+                      </div>
+                      <div className="nft_coll_info">
+                        <Link to="/explore">
+                          <h4>{post.title}</h4>
+                        </Link>
+                        <span>ERC-{post.code}</span>
+                      </div>
+                    </div>
+                ))} 
+              </OwlCarousel>  
+            </>
+          }
+          
+          
         </div>
       </div>
     </section>
