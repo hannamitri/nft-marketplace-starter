adc1d9e81f6580e81e19f532742d901e6e69c6f5
Ryan Norin <103920941+NironR@users.noreply.github.com>
2022-12-07 13:30:28 +1100

    Added dynamic properties and implemented timer

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/explore" element={<Explore />} />
-        <Route path="/author" element={<Author />} />
-        <Route path="/item-details" element={<ItemDetails />} />
+        <Route path="/author:id" element={<Author />} />
+        <Route path="/item-details:id" element={<ItemDetails />} />
       </Routes>
       <Footer />
     </Router>
