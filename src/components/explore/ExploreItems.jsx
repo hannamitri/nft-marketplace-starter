77aef705af703355544de22aac1a47c265ce6279
Rajinth <rajinth3@gmail.com>
2022-12-09 11:41:55 +0530

    Explore Items

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,77 +1,83 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import Skeleton from "../UI/Skeleton";
+import Card from "../Card";
 
 const ExploreItems = () => {
+  const [items, setItems] = useState([]);
+  const [itemsCount, setItemsCount] = useState(8);
+  const [loading, setLoading] = useState(true);
+
+  const getItems = async () => {
+    const { data } = await axios.get(
+      "https://us-central1-nft-cloud-functions.cloudfunctions.net/explore"
+    );
+    setItems(data);
+  };
+
+  async function filterItems(filter) {
+    setLoading(false);
+    const { data } = await axios.get(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=${filter}`
+    );
+    setItems(data);
+    setLoading(true);
+  }
+
+  useEffect(() => {
+    getItems();
+  }, []);
+
+  const skeletonLoading = new Array(8).fill(0).map((_, index) => (
+    <div
+      key={index}
+      className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+      style={{ display: "block", backgroundSize: "cover" }}
+    >
+      <div className="nft__item">
+        <Skeleton width="100%" height="400px" />
+      </div>
+    </div>
+  ));
+
   return (
     <>
       <div>
-        <select id="filter-items" defaultValue="">
+        <select
+          onChange={(e) => filterItems(e.target.value)}
+          id="filter-items"
+          defaultValue=""
+        >
           <option value="">Default</option>
           <option value="price_low_to_high">Price, Low to High</option>
           <option value="price_high_to_low">Price, High to Low</option>
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {new Array(8).fill(0).map((_, index) => (
-        <div
-          key={index}
-          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-          style={{ display: "block", backgroundSize: "cover" }}
-        >
-          <div className="nft__item">
-            <div className="author_list_pp">
-              <Link
-                to="/author"
-                data-bs-toggle="tooltip"
-                data-bs-placement="top"
-              >
-                <img className="lazy" src={AuthorImage} alt="" />
-                <i className="fa fa-check"></i>
-              </Link>
-            </div>
-            <div className="de_countdown">5h 30m 32s</div>
-
-            <div className="nft__item_wrap">
-              <div className="nft__item_extra">
-                <div className="nft__item_buttons">
-                  <button>Buy Now</button>
-                  <div className="nft__item_share">
-                    <h4>Share</h4>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-facebook fa-lg"></i>
-                    </a>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-twitter fa-lg"></i>
-                    </a>
-                    <a href="">
-                      <i className="fa fa-envelope fa-lg"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <Link to="/item-details">
-                <img src={nftImage} className="lazy nft__item_preview" alt="" />
-              </Link>
-            </div>
-            <div className="nft__item_info">
-              <Link to="/item-details">
-                <h4>Pinky Ocean</h4>
-              </Link>
-              <div className="nft__item_price">1.74 ETH</div>
-              <div className="nft__item_like">
-                <i className="fa fa-heart"></i>
-                <span>69</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      ))}
+      {items.length && loading
+        ? items
+            .slice(0, itemsCount)
+            .map((item) => (
+              <Card
+                key={item.id}
+                item={item}
+                responsiveStyling={"col-lg-3 col-md-6 col-sm-6 col-xs-12"}
+              />
+            ))
+        : skeletonLoading}
       <div className="col-md-12 text-center">
-        <Link to="" id="loadmore" className="btn-main lead">
-          Load more
-        </Link>
+        {itemsCount !== 16 && (
+          <Link
+            onClick={() => setItemsCount(itemsCount + 4)}
+            to=""
+            id="loadmore"
+            data-aos="fade-up"
+            className="btn-main lead"
+          >
+            Load More
+          </Link>
+        )}
       </div>
     </>
   );
