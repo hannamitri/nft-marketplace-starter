a3cd8243ea56e303162b1850364295fdb40d0cde
Dildeep <dildeepgill@hotmail.com>
2022-12-20 15:32:09 -0500

    added images from api

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
 
 const HotCollections = () => {
+  const [data, setData] = useState();
+  useEffect(() => {
+    axios({
+      url: "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections ",
+      method: "GET",
+      dataResponse: "json",
+    }).then((response) => {
+      setData(response.data);
+    });
+  }, []);
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,25 +24,33 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+          {data.map((obj) => (
+            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={obj.id}>
               <div className="nft_coll">
                 <div className="nft_wrap">
                   <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
+                    <img
+                      src={obj.nftImage}
+                      className="lazy img-fluid"
+                      alt="nftimage"
+                    />
                   </Link>
                 </div>
                 <div className="nft_coll_pp">
                   <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
+                    <img
+                      className="lazy pp-coll"
+                      src={obj.authorImage}
+                      alt="author img"
+                    />
                   </Link>
                   <i className="fa fa-check"></i>
                 </div>
                 <div className="nft_coll_info">
                   <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
+                    <h4>{obj.title}</h4>
                   </Link>
-                  <span>ERC-192</span>
+                  <span>ERC- {obj.code}</span>
                 </div>
               </div>
             </div>
