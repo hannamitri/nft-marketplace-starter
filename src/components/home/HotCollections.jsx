473c807ede3c8b7ebe1d5f07b84ea743ab26215a
lteranl <jjteran2018@gmail.com>
2022-12-21 13:21:54 -0800

    destructuring prop getHotCollections, and using .map to get nftImage, authoerImage, title, and code

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -3,44 +3,57 @@ import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
 
-const HotCollections = () => {
-  return (
-    <section id="section-collections" className="no-bottom">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <div className="text-center">
-              <h2>Hot Collections</h2>
-              <div className="small-border bg-color-2"></div>
+const HotCollections = ({ getHotCollections }) => {
+    // console.log(getHotCollections);
+    return (
+        <section id="section-collections" className="no-bottom">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="text-center">
+                            <h2>Hot Collections</h2>
+                            <div className="small-border bg-color-2"></div>
+                        </div>
+                    </div>
+                    {/* // This is the map function that is looping through the data from the API call */}
+                    {getHotCollections.map((nft, index) => (
+                        <div
+                            className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
+                            key={index}
+                        >
+                            <div className="nft_coll">
+                                <div className="nft_wrap">
+                                    <Link to="/item-details">
+                                        <img
+                                            src={nft.nftImage}
+                                            className="lazy img-fluid"
+                                            alt=""
+                                        />
+                                    </Link>
+                                </div>
+                                <div className="nft_coll_pp">
+                                    <Link to="/author">
+                                        <img
+                                            className="lazy pp-coll"
+                                            src={nft.authorImage}
+                                            alt=""
+                                        />
+                                    </Link>
+                                    <i className="fa fa-check"></i>
+                                </div>
+                                <div className="nft_coll_info">
+                                    <Link to="/explore">
+                                        <h4>{nft.title}</h4>
+                                    </Link>
+                                    <span>ERC-{nft.code}</span>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
-          </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
-                </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <span>ERC-192</span>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+        </section>
+    );
 };
 
 export default HotCollections;
