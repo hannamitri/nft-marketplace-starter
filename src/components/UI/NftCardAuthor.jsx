b8835662caeed90c1407e787fe631bc37c8332dc
kemenike <kanayoemenike37@gmail.com>
2022-12-23 08:47:42 -0800

    Author Page and Links to Page.

---
+++ src/components/UI/NftCardAuthor.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Timer from './Timer';
+
+const NftCardAuthor = ({ nftItem, author }) => {
+    return (
+        <div
+        className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+        style={{ display: "block", backgroundSize: "cover" }}
+      >
+        <div className="nft__item">
+          <div className="author_list_pp">
+            <Link
+              to={`/author/${author.authorId}`}
+              data-bs-toggle="tooltip"
+              data-bs-placement="top"
+            >
+              <img className="lazy" src={author.authorImage} alt="" />
+              <i className="fa fa-check"></i>
+            </Link>
+          </div>
+          <div className="nft__item_wrap">
+            <div className="nft__item_extra">
+              <div className="nft__item_buttons">
+                <button>Buy Now</button>
+                <div className="nft__item_share">
+                  <h4>Share</h4>
+                  <a href="" target="_blank" rel="noreferrer">
+                    <i className="fa fa-facebook fa-lg"></i>
+                  </a>
+                  <a href="" target="_blank" rel="noreferrer">
+                    <i className="fa fa-twitter fa-lg"></i>
+                  </a>
+                  <a href="">
+                    <i className="fa fa-envelope fa-lg"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <Link to={`/item-details/${nftItem.nftId}`}>
+              <img src={nftItem.nftImage} className="lazy nft__item_preview" alt="" />
+            </Link>
+          </div>
+          <div className="nft__item_info">
+            <Link to={`/item-details/${nftItem.nftId}`}>
+              <h4>{nftItem.title}</h4>
+            </Link>
+            <div className="nft__item_price">{nftItem.price} ETH</div>
+            <div className="nft__item_like">
+              <i className="fa fa-heart"></i>
+              <span>{nftItem.likes}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default NftCardAuthor;
