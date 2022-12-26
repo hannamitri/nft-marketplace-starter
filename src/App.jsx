f90ef90278fe4fa0ce87d9c86667b8041ef562fe
Dildeep <dildeepgill@hotmail.com>
2022-12-26 18:27:46 -0500

    landing animations done

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ import Author from "./pages/Author";
 import ItemDetails from "./pages/ItemDetails";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
+AOS.init();
 function App() {
   return (
     <Router>
