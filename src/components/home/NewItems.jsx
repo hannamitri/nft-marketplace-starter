7c1898c98b457d412e1c808b552f3fc572d51972
Dildeep <dildeepgill@hotmail.com>
2022-12-26 19:10:13 -0500

    completed animations

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -38,7 +38,7 @@ const NewItems = () => {
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
-        <div className="row">
+        <div className="row" data-aos="fade-in">
           <div className="col-lg-12">
             <div className="text-center">
               <h2>New Items</h2>
--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -26,7 +26,7 @@ const TopSellers = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          <div className="col-md-12">
+          <div className="col-md-12" data-aos="fade-in">
             <ol className="author_list">
               {sellers.length
                 ? sellers.map((seller) => (
