ed3aaf8bdc7cafd485f3d99e065796618225309d
Dildeep <dildeepgill@hotmail.com>
2022-12-26 19:27:10 -0500

    added a href

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -21,13 +21,13 @@ const AuthorItems = ({ author }) => {
                       <button>Buy Now</button>
                       <div className="nft__item_share">
                         <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
+                        <a href="/" target="_blank" rel="noreferrer">
                           <i className="fa fa-facebook fa-lg"></i>
                         </a>
-                        <a href="" target="_blank" rel="noreferrer">
+                        <a href="/" target="_blank" rel="noreferrer">
                           <i className="fa fa-twitter fa-lg"></i>
                         </a>
-                        <a href="">
+                        <a href="/">
                           <i className="fa fa-envelope fa-lg"></i>
                         </a>
                       </div>
--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -78,13 +78,13 @@ const NewItems = () => {
                           <button>Buy Now</button>
                           <div className="nft__item_share">
                             <h4>Share</h4>
-                            <a href="" target="_blank" rel="noreferrer">
+                            <a href="/" target="_blank" rel="noreferrer">
                               <i className="fa fa-facebook fa-lg"></i>
                             </a>
-                            <a href="" target="_blank" rel="noreferrer">
+                            <a href="/" target="_blank" rel="noreferrer">
                               <i className="fa fa-twitter fa-lg"></i>
                             </a>
-                            <a href="">
+                            <a href="/">
                               <i className="fa fa-envelope fa-lg"></i>
                             </a>
                           </div>
