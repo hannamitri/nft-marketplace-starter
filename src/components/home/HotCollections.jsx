124731f5015afae324794432017d71282e2e3988
lteranl <jjteran2018@gmail.com>
2023-01-04 15:15:33 -0800

    skeleton loading section added

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -6,6 +6,13 @@ import "slick-carousel/slick/slick-theme.css";
 import "../../css/styles/HotCollections.css";
 
 const HotCollections = ({ getHotCollections }) => {
+    const [loading, setLoading] = useState(true);
+
+    // skeleton loading
+    setTimeout(() => {
+        setLoading(false);
+    }, 4000);
+
     //slider settings
     const settings = {
         // dots: true,
@@ -16,7 +23,7 @@ const HotCollections = ({ getHotCollections }) => {
         initialSlide: 0,
         responsive: [
             {
-                breakpoint: 1024,
+                breakpoint: 1200,
                 settings: {
                     slidesToShow: 3,
                     slidesToScroll: 3,
@@ -24,7 +31,7 @@ const HotCollections = ({ getHotCollections }) => {
                 },
             },
             {
-                breakpoint: 600,
+                breakpoint: 980,
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 2,
@@ -32,7 +39,7 @@ const HotCollections = ({ getHotCollections }) => {
                 },
             },
             {
-                breakpoint: 480,
+                breakpoint: 760,
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1,
@@ -55,41 +62,89 @@ const HotCollections = ({ getHotCollections }) => {
                     </div>
                     {/* // This is the map function that is looping through the data from the API call */}
                     {/* slider */}
+                    {/* skeleton loading */}
+
                     <Slider {...settings}>
                         {getHotCollections.map((nft, index) => (
-                            <div
-                                style={{ width: "100%" }}
-                                className="nft_card"
-                                key={index}
-                            >
-                                <div className="nft_coll">
-                                    <div className="nft_wrap">
-                                        <Link to="/item-details">
-                                            <img
-                                                src={nft.nftImage}
-                                                className="lazy img-fluid"
-                                                alt=""
-                                            />
-                                        </Link>
-                                    </div>
-                                    <div className="nft_coll_pp">
-                                        <Link to="/author">
-                                            <img
-                                                className="lazy pp-coll"
-                                                src={nft.authorImage}
-                                                alt=""
-                                            />
-                                        </Link>
-                                        <i className="fa fa-check"></i>
-                                    </div>
-                                    <div className="nft_coll_info">
-                                        <Link to="/explore">
-                                            <h4>{nft.title}</h4>
-                                        </Link>
-                                        <span>ERC-{nft.code}</span>
-                                    </div>
+                            <>
+                                {/* skeleton loading */}
+
+                                <div
+                                    style={{ width: "100%" }}
+                                    className="nft_card"
+                                    key={index}
+                                >
+                                    {loading ? (
+                                        <div className="nft_coll">
+                                            <div className="nft_wrap">
+                                                <div
+                                                    className="skeleton__box"
+                                                    style={{
+                                                        width: "100%",
+                                                        height: "200px",
+                                                    }}
+                                                ></div>
+                                            </div>
+                                            <div className="nft_coll_pp">
+                                                <div
+                                                    className="skeleton__box"
+                                                    style={{
+                                                        width: "50px",
+                                                        height: "50px",
+                                                        borderRadius: "50%",
+                                                    }}
+                                                ></div>
+                                                <i className="fa fa-check"></i>
+                                            </div>
+                                            <div className="nft_coll_info">
+                                                <div
+                                                    className="skeleton__box"
+                                                    style={{
+                                                        width: "100px",
+                                                        height: "20px",
+                                                    }}
+                                                ></div>
+                                                <br />
+                                                <div
+                                                    className="skeleton__box"
+                                                    style={{
+                                                        width: "60px",
+                                                        height: "20px",
+                                                    }}
+                                                ></div>
+                                            </div>
+                                        </div>
+                                    ) : (
+                                        <div className="nft_coll">
+                                            <div className="nft_wrap">
+                                                <Link to="/item-details">
+                                                    <img
+                                                        src={nft.nftImage}
+                                                        className="lazy img-fluid"
+                                                        alt=""
+                                                    />
+                                                </Link>
+                                            </div>
+                                            <div className="nft_coll_pp">
+                                                <Link to="/author">
+                                                    <img
+                                                        className="lazy pp-coll"
+                                                        src={nft.authorImage}
+                                                        alt=""
+                                                    />
+                                                </Link>
+                                                <i className="fa fa-check"></i>
+                                            </div>
+                                            <div className="nft_coll_info">
+                                                <Link to="/explore">
+                                                    <h4>{nft.title}</h4>
+                                                </Link>
+                                                <span>ERC-{nft.code}</span>
+                                            </div>
+                                        </div>
+                                    )}
                                 </div>
-                            </div>
+                            </>
                         ))}
                     </Slider>
                 </div>
