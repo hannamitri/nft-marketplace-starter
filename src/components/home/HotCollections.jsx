d6ecb4980a073d8c7d78fca9e9b906dd61d241b1
Joe <josephlewismoulton@gmail.com>
2023-01-09 12:55:18 +0000

    Collecting data from api using axios

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-const HotCollections = () => {
+function HotCollections() {
+  const [loading, setLoading] = useState(true);
+  const [hotCollections, setHotCollections] = useState([]);
+
+  async function fetchCollections() {
+    setLoading(true);
+    const { data } = await axios.get(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections`
+    );
+    setHotCollections(data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchCollections();
+  }, []);
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,25 +30,25 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+          {hotCollections.map((item) => (
+            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={item.id}>
               <div className="nft_coll">
                 <div className="nft_wrap">
                   <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
+                    <img src={item.nftImage} className="lazy img-fluid" alt="" />
                   </Link>
                 </div>
                 <div className="nft_coll_pp">
                   <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
+                    <img className="lazy pp-coll" src={item.authorImage} alt="" />
                   </Link>
                   <i className="fa fa-check"></i>
                 </div>
                 <div className="nft_coll_info">
                   <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
+                    <h4>{item.title}</h4>
                   </Link>
-                  <span>ERC-192</span>
+                  <span>{`ERC-${item.code}`}</span>
                 </div>
               </div>
             </div>
@@ -41,6 +57,6 @@ const HotCollections = () => {
       </div>
     </section>
   );
-};
+}
 
 export default HotCollections;
