a6866a8bbc4814eeab8be23f12f60b9c85ff7f41
Joe <josephlewismoulton@gmail.com>
2023-01-11 16:09:31 +0000

    Author name icon and follow functionality addded

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -2,9 +2,44 @@ import React from "react";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
 import { Link } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Skeleton } from "@mui/material";
 
 const Author = () => {
+  const [loading, setLoading] = useState(true);
+  const [authorData, setAuthorData] = useState([]);
+  const [followerCount, setFollowerCount] = useState(0);
+  const [followed, setFollowed] = useState(false);
+
+  async function loadData() {
+    setLoading(true);
+    const { data } = await axios.get(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=73855012`
+    );
+    setAuthorData(data);
+    setFollowerCount(data.followers);
+    setLoading(false);
+  }
+
+  function increaseFollow() {
+    setFollowerCount(followerCount + 1);
+    setFollowed(true);
+  }
+
+  function decreaseFollow() {
+    setFollowerCount(followerCount - 1);
+    setFollowed(false);
+  }
+
+  function changeFollowerCount() {
+    followed ? decreaseFollow() : increaseFollow();
+  }
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
@@ -25,15 +60,17 @@ const Author = () => {
                 <div className="d_profile de-flex">
                   <div className="de-flex-col">
                     <div className="profile_avatar">
-                      <img src={AuthorImage} alt="" />
+                      <img src={authorData.authorImage} alt="" />
 
                       <i className="fa fa-check"></i>
                       <div className="profile_name">
                         <h4>
-                          Monica Lucas
-                          <span className="profile_username">@monicaaaa</span>
+                          {authorData.authorName}
+                          <span className="profile_username">
+                            @{authorData.tag}
+                          </span>
                           <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
+                            {authorData.address}
                           </span>
                           <button id="btn_copy" title="Copy Text">
                             Copy
@@ -44,9 +81,13 @@ const Author = () => {
                   </div>
                   <div className="profile_follow de-flex">
                     <div className="de-flex-col">
-                      <div className="profile_follower">573 followers</div>
-                      <Link to="#" className="btn-main">
-                        Follow
+                      <div className="profile_follower">{`${followerCount} followers`}</div>
+                      <Link
+                        to="#"
+                        className="btn-main"
+                        onClick={changeFollowerCount}
+                      >
+                        {followed ? `Unfollow` : `Follow`}
                       </Link>
                     </div>
                   </div>
