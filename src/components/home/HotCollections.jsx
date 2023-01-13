2ed7c4672b1395ef7270278d2ee0626997433360
Jackson <z5397791@ad.unsw.edu.au>
2023-01-13 20:42:49 +1100

    Add responsive options to react owl carousel

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -24,8 +24,17 @@ const HotCollections = () => {
 
   const options = {
     items: 4,
+    margin: 10,
+    nav: true,
     loop: true,
     dots: false,
+    responsiveRefreshRate: 50,
+    responsive: {
+      1200: {items: 4},
+      768: {items: 3},
+      470: {items: 2},
+      0: {items: 1}
+    }
   };
 
   return (
