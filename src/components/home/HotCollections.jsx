4b45c17904974288fe351d6decc3ea830723deb3
mifouts <fouts.mitch15@gmail.com>
2023-01-28 00:19:41 -0500

    dynamically displaying data

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
 import axios from "axios";
 import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
