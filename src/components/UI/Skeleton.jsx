4106f855619b2f81dec45c73d1f79fe25399905b
Sam <samarooniii@yahoo.com>
2023-02-09 23:57:03 -0500

    skeleton loading somewhat working

--- src/components/UI/Skeleton.jsx
+++ src/components/UI/Skeleton.jsx
@@ -1,15 +1,48 @@
 import React from "react";
+import { Box, Grid, Skeleton as Skelly } from "@mui/material";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
 
-const Skeleton = ({ width, height, borderRadius }) => {
+const Skeleton = ({ cards, loading }) => {
   return (
-    <div
-      className="skeleton-box"
-      style={{
-        width,
-        height,
-        borderRadius,
-      }}
-    ></div>
+    <div className="d-flex">
+      {new Array(6).fill(0).map((index) => (
+        <Card sx={{ maxWidth: 345, mr: 3 }} key={index}>
+          {loading ? (
+            <Skelly
+              animation="pulse"
+              variant="rectangular"
+              height={200}
+              width="100%"
+            />
+          ) : (
+            <Card component="img" height={200} img={cards.nftImage} />
+          )}
+          <CardContent>
+            {loading ? (
+              <>
+                <Skelly
+                  animation="pulse"
+                  variant="circular"
+                  height={40}
+                  width={40}
+                  style={{ marginBottom: 6 }}
+                />
+              </>
+            ) : (
+              <Typography>
+                {cards.title}
+                {cards.code}
+              </Typography>
+            )}
+          </CardContent>
+        </Card>
+      ))}
+    </div>
   );
 };
 
--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -30,6 +30,8 @@ const HotCollections = () => {
     setLoading(false);
   }
 
+  console.log(loading);
+
   useEffect(() => {
     fetchHotCollections();
   }, []);
@@ -44,39 +46,43 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          <Slider {...settings}>
-            {cards.map((card, id) => (
-              <div className="col-lg-12 col-md-6 col-sm-6 col-xs-12" key={id}>
-                <div className="nft_coll">
-                  <div className="nft_wrap">
-                    <Link to="/item-details">
-                      <img
-                        src={card.nftImage}
-                        className="lazy img-fluid"
-                        alt=""
-                      />
-                    </Link>
-                  </div>
-                  <div className="nft_coll_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-coll"
-                        src={card.authorImage}
-                        alt=""
-                      />
-                    </Link>
-                    <i className="fa fa-check"></i>
-                  </div>
-                  <div className="nft_coll_info">
-                    <Link to="/explore">
-                      <h4>{card.title}</h4>
-                    </Link>
-                    <span>{card.code}</span>
+          {loading ? (
+            <Skeleton cards={cards} loading={loading} />
+          ) : (
+            <Slider {...settings}>
+              {cards.map((card, id) => (
+                <div className="col-lg-12 col-md-6 col-sm-6 col-xs-12" key={id}>
+                  <div className="nft_coll">
+                    <div className="nft_wrap">
+                      <Link to="/item-details">
+                        <img
+                          src={card.nftImage}
+                          className="lazy img-fluid"
+                          alt=""
+                        />
+                      </Link>
+                    </div>
+                    <div className="nft_coll_pp">
+                      <Link to="/author">
+                        <img
+                          className="lazy pp-coll"
+                          src={card.authorImage}
+                          alt=""
+                        />
+                      </Link>
+                      <i className="fa fa-check"></i>
+                    </div>
+                    <div className="nft_coll_info">
+                      <Link to="/explore">
+                        <h4>{card.title}</h4>
+                      </Link>
+                      <span>{card.code}</span>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </Slider>
+              ))}
+            </Slider>
+          )}
         </div>
       </div>
     </section>
