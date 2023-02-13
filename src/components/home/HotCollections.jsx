77c1bced24113518ee45a47f3557bc2ddb26603f
unbeqem <tristank2005@web.de>
2023-02-13 17:14:48 +0100

    display hotCollections

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,17 @@
+import axios from "axios"
 import React from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
 
 const HotCollections = () => {
+    async function GetHotCollectionData() {
+      const hotCollectionInfo = await axios.get('https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections')
+        const hotCollectionData = hotCollectionInfo.data
+        console.log(hotCollectionData)
+    }
+  
+  GetHotCollectionData()
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,7 +22,7 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
+          {new Array(6).fill(0).map((_, index) => (
             <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
               <div className="nft_coll">
                 <div className="nft_wrap">
