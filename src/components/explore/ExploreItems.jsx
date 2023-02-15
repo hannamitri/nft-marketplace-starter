c05062d3babb3d4ffe78369f9f9954187f505301
kevinchristyy <kevin.10@hotmail.no>
2023-02-15 19:57:32 +0100

    kevinexplorefinal

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -3,14 +3,15 @@ import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import ExpiryTime from "../home/ExpiryTime";
-import Skeleton from '../UI/Skeleton'
+import Skeleton from "../UI/Skeleton";
 
 const ExploreItems = () => {
   const [exploreNft, setExploreNft] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [loader, setLoader] = useState(8);
 
   async function getExploreItems() {
-    setLoading(true)
+    setLoading(true);
     const { data } = await axios.get(
       `https://us-central1-nft-cloud-functions.cloudfunctions.net/explore`
     );
@@ -18,12 +19,19 @@ const ExploreItems = () => {
     setLoading(false);
   }
 
+  async function filterNFTItems(filterCategory) {
+    const { data } = await axios.get(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=${filterCategory}`
+    );
+    setExploreNft(data);
+  }
+
   useState(() => {
     getExploreItems();
   }, []);
   return (
     <>
-    {loading ? (
+      {loading ? (
         <>
           {new Array(8).fill(0).map((_, index) => (
             <div
@@ -36,18 +44,22 @@ const ExploreItems = () => {
               <Skeleton width={300} height={400} borderRadius={1} />
             </div>
           ))}
-          </>
-    ) : (
-      <div>
-        <select id="filter-items" defaultValue="">
-          <option value="">Default</option>
-          <option value="price_low_to_high">Price, Low to High</option>
-          <option value="price_high_to_low">Price, High to Low</option>
-          <option value="likes_high_to_low">Most liked</option>
-        </select>
-      </div>
-    )}
-      {exploreNft.slice(0, 8).map((_, index) => (
+        </>
+      ) : (
+        <div>
+          <select
+            id="filter-items"
+            defaultValue=""
+            onChange={(e) => filterNFTItems(e.target.value)}
+          >
+            <option value="">Default</option>
+            <option value="price_low_to_high">Price, Low to High</option>
+            <option value="price_high_to_low">Price, High to Low</option>
+            <option value="likes_high_to_low">Most liked</option>
+          </select>
+        </div>
+      )}
+      {exploreNft.slice(0, loader).map((_, index) => (
         <div
           key={index}
           className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
@@ -64,7 +76,7 @@ const ExploreItems = () => {
                 <i className="fa fa-check"></i>
               </Link>
             </div>
-            
+
             {_.expiryDate ? <ExpiryTime expiryTime={_.expiryDate} /> : ""}
 
             <div className="nft__item_wrap">
@@ -100,15 +112,19 @@ const ExploreItems = () => {
               <div className="nft__item_price">{_.price} ETH</div>
               <div className="nft__item_like">
                 <i className="fa fa-heart"></i>
-                <span>69</span>
+                <span>{_.likes}</span>
               </div>
             </div>
           </div>
         </div>
-      )
-      )}
+      ))}
       <div className="col-md-12 text-center">
-        <Link to="" id="loadmore" className="btn-main lead">
+        <Link
+          to=""
+          id="loadmore"
+          className="btn-main lead"
+          onClick={() => setLoader(loader + 4)}
+        >
           Load more
         </Link>
       </div>
