299724ed6b181f7e902b3d4a4989ff189cf1dc3d
kevinchristyy <kevin.10@hotmail.no>
2023-02-15 20:21:25 +0100

    kevnewitems

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -4,6 +4,8 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import OwlCarousel from "react-owl-carousel";
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
 import Skeleton from "../UI/Skeleton";
 import ExpiryTime from "./ExpiryTime";
 
