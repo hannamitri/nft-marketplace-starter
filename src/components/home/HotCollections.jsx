36bf006b785dbc9bd9f8cb292b4e9409b2a2d1d4
PubertsOscar <esquivelkazuli@gmail.com>
2023-02-15 15:12:52 -0500

    Add carousel,Loading state, responsive

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import OwlCarousel from "react-owl-carousel";
+import Skeleton from "../UI/Skeleton";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
 
 const HotCollections = () => {
+  const [author, setAuthor] = useState([]);
+
+  async function fetchHotCollectionsData() {
+    const { data } = await axios.get(
+      "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections"
+    );
+    setAuthor(data);
+  }
+  useEffect(() => {
+    fetchHotCollectionsData();
+  }, []);
+
+  const options = {
+    nav: true,
+    loop: true,
+    items: 4,
+    margin: 10,
+    dots: false,
+    responsive: {
+      0: { items: 1 },
+      470: { items: 2 },
+      768: { items: 3 },
+      1200: { items: 4 },
+    },
+  };
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,29 +43,58 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft_coll">
+
+          <OwlCarousel {...options}>
+            {author.length ? (
+              author.map((auth) => (
+                <div className="nft_coll" key={auth.id}>
+                  <div className="nft_wrap">
+                    <Link to={`/item-details/${auth.nftId}`}>
+                      <img
+                        src={auth.nftImage}
+                        className="lazy img-fluid"
+                        alt=""
+                      />
+                    </Link>
+                  </div>
+                  <div className="nft_coll_pp">
+                    <Link to="/author">
+                      <img
+                        className="lazy pp-coll"
+                        src={auth.authorImage}
+                        alt=""
+                      />
+                    </Link>
+                    <i className="fa fa-check"></i>
+                  </div>
+                  <div className="nft_coll_info">
+                    <Link to="/explore">
+                      <h4>{auth.title}</h4>
+                    </Link>
+                    <span>ERC-{auth.code}</span>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <div className="nft_coll ">
                 <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
+                  <Skeleton width={"100%"} height={"200px"} />
                 </div>
                 <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
-                  </Link>
+                  <Skeleton
+                    width={"60px"}
+                    height={"60px"}
+                    borderRadius={"999px"}
+                  />
                   <i className="fa fa-check"></i>
                 </div>
                 <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <span>ERC-192</span>
+                  <Skeleton width={"100px"} borderRadius={"8px"} />
                 </div>
+                <Skeleton width={"50px"} borderRadius={"8px"} />
               </div>
-            </div>
-          ))}
+            )}
+          </OwlCarousel>
         </div>
       </div>
     </section>
