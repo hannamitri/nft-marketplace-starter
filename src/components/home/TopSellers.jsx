2d66567ae0c4170bac5d389c2dd01a685edfccc8
Torben Winterbach <torbenwinterbach@gmail.com>
2023-02-15 13:16:52 +0200

    dynamic top sellers section

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
+import axios from "axios";
+import Skeleton from "../UI/Skeleton";
 
 const TopSellers = () => {
+  const [topSellers, setTopSellers] = useState([])
+
+    useEffect(() => {
+      async function getData() {
+        const { data } = await axios.get('https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers');
+        console.log(data);
+        setTopSellers(data);
+      }
+      getData();
+    }, [])
+
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,21 +29,22 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
+              {
+                topSellers.map((seller) => (
+                <li key={seller.id}>
                   <div className="author_list_pp">
                     <Link to="/author">
                       <img
                         className="lazy pp-author"
-                        src={AuthorImage}
+                        src={seller.authorImage}
                         alt=""
                       />
                       <i className="fa fa-check"></i>
                     </Link>
                   </div>
                   <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
+                    <Link to="/author">{seller.authorName}</Link>
+                    <span>{seller.price} ETH</span>
                   </div>
                 </li>
               ))}
