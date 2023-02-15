7162a83027570b108542ec9c55fb68b33769bec1
Torben Winterbach <torbenwinterbach@gmail.com>
2023-02-15 11:14:36 +0200

    dynamic new items section

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ReactOwlCarousel from "react-owl-carousel";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import Skeleton from "../UI/Skeleton";
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+import axios from "axios";
+import NewItem from "./NewItem";
+import NewItemSkeleton from "./NewItemSkeleton";
+
 
 const NewItems = () => {
+
+  const [newItems, setNewItems] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const { data } = await axios.get('https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems');
+      console.log(data)
+      setNewItems(data);
+    }
+    getData();
+  }, [])
+
+
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
@@ -14,62 +36,66 @@ const NewItems = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link
-                    to="/author"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    title="Creator: Monica Lucas"
-                  >
-                    <img className="lazy" src={AuthorImage} alt="" />
-                    <i className="fa fa-check"></i>
-                  </Link>
-                </div>
-                <div className="de_countdown">5h 30m 32s</div>
-
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-
-                  <Link to="/item-details">
-                    <img
-                      src={nftImage}
-                      className="lazy nft__item_preview"
-                      alt=""
-                    />
-                  </Link>
-                </div>
-                <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <div className="nft__item_price">3.08 ETH</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>69</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
+          {
+            newItems.length ? 
+            (
+              <OwlCarousel
+                margin={10} 
+                loop
+                nav={true}
+                responsive={{
+                  0: {
+                    items: 1
+                  },
+                  480: {
+                    items: 2
+                  },
+                  1000: {
+                    items: 3
+                  },
+                  1200: {
+                    items: 4
+                  }
+                }}
+              >
+                {
+                  newItems.map((item) => (
+                    <NewItem item={item}  key={item.id} />
+                  ))
+                }
+              </OwlCarousel>
+            )
+            :
+            (
+              <>
+                <OwlCarousel
+                margin={10} 
+                  loop
+                  nav={true}
+                  responsive={{
+                    0: {
+                      items: 1
+                    },
+                    480: {
+                      items: 2
+                    },
+                    1000: {
+                      items: 3
+                    },
+                    1200: {
+                      items: 4
+                    }
+                  }}
+              >
+              {
+                    new Array(6).fill(0).map((_, index) => (
+                      <NewItemSkeleton key={index} />
+                    ))
+                  }
+              </OwlCarousel>
+              </>
+            )
+          }
         </div>
       </div>
     </section>
