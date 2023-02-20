accf2dbc4a87d9fdb15b931779836e732659d2f1
Abderrahmane <tiamani3939@gmail.com>
2023-02-20 16:11:49 +0100

    fixed countdown

--- src/components/UI/Countdown.jsx
+++ src/components/UI/Countdown.jsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-function Countdown({item}) {
-    const timeRemaining = Math.floor((item.expiryDate - Date.now())/1000)
-    const hours = Math.floor(timeRemaining/3600);
-    const mins = Math.floor((timeRemaining%3600)/60);
-    const secs = timeRemaining%60;
+function Countdown({item}) {    
+    const [timeNow, setTimeNow] = useState(Date.now());
+
+    const [hours, setHours] = useState(Math.floor((item.expiryDate - timeNow) / 1000 / 3600))
+    const [mins, setMins] = useState(Math.floor((item.expiryDate - timeNow) / 1000 % 3600 / 60))
+    const [secs, setSecs] = useState(Math.floor((item.expiryDate - timeNow) / 1000 % 60))
+
+    function displayCount() {
+        const secsRemaining = Math.floor((item.expiryDate - timeNow) / 1000)
+        setHours(Math.floor(secsRemaining / 3600))
+        setMins((Math.floor((secsRemaining % 3600) / 60)))
+        setSecs(secsRemaining % 60)
+    }
+
+    useEffect(() => {
+        displayCount()
+        setTimeout(() => {
+          setTimeNow(Date.now())
+        }, 1000);
+    }, [timeNow])
+
     return (
         <div className="de_countdown">
             {
-               item.expiryDate - Date.now() > 0
+                item.expiryDate - timeNow > 0
                 &&
                 `${hours}h ${mins}m ${secs}s`
-           }
+            }
         </div>
-    );
+    )
 }
+
 
 export default Countdown;
