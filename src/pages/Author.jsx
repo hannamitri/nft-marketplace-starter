bb09cfa1f33277c66d71a399ae3de4976fc8f0a5
OmarSerranoSosa <omar.serranososa@gmail.com>
2023-02-23 22:15:17 -0600

    Author part is completed (dynamic API and S.L.S)

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
-import { Link } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import Skeleton from "../components/UI/Skeleton";
 
 const Author = () => {
+
+  const { authorId } = useParams()
+  const [author, setAuthor] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [follorwing, setFollowing] = useState(false)
+
+  async function getAuthors() {
+    setLoading(true)
+    const { data } = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${authorId}`)
+    setAuthor(data)
+    setLoading(false)
+  }
+
+  function follow() {
+    setFollowing(true)
+    const followerscount = author.followers
+    setAuthor({...author, followers: followerscount + 1})
+  }
+
+  function unfollow() {
+    setFollowing(false)
+    const followerscount = author.followers
+    setAuthor({...author, followers: followerscount - 1})
+  }
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    getAuthors()
+  }, [])
+
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
@@ -15,54 +46,112 @@ const Author = () => {
           aria-label="section"
           className="text-light"
           data-bgimage="url(images/author_banner.jpg) top"
-          style={{ background: `url(${AuthorBanner}) top` }}
+          style={{ background: `url(${AuthorBanner}) top` , backgroundRepeat: "no-repeat",
+          backgroundSize:"cover" }}
         ></section>
+        {!loading 
+        ? (
+          <section aria-label="section">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="d_profile de-flex">
+                <div className="de-flex-col">
+                  <div className="profile_avatar">
+                    <img src={author.authorImage} alt="" />
 
-        <section aria-label="section">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12">
-                <div className="d_profile de-flex">
-                  <div className="de-flex-col">
-                    <div className="profile_avatar">
-                      <img src={AuthorImage} alt="" />
-
-                      <i className="fa fa-check"></i>
-                      <div className="profile_name">
-                        <h4>
-                          Monica Lucas
-                          <span className="profile_username">@monicaaaa</span>
-                          <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
-                          </span>
-                          <button id="btn_copy" title="Copy Text">
-                            Copy
-                          </button>
-                        </h4>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="profile_follow de-flex">
-                    <div className="de-flex-col">
-                      <div className="profile_follower">573 followers</div>
-                      <Link to="#" className="btn-main">
-                        Follow
-                      </Link>
+                    <i className="fa fa-check"></i>
+                    <div className="profile_name">
+                      <h4>
+                        {author.authorName}
+                        <span className="profile_username">@{author.tag}</span>
+                        <span id="wallet" className="profile_wallet">
+                          {author.address}
+                        </span>
+                        <button id="btn_copy" title="Copy Text">
+                          Copy
+                        </button>
+                      </h4>
                     </div>
                   </div>
                 </div>
-              </div>
-
-              <div className="col-md-12">
-                <div className="de_tab tab_simple">
-                  <AuthorItems />
+                <div className="profile_follow de-flex">
+                  <div className="de-flex-col">
+                    <div className="profile_follower">{author.followers} followers</div>
+                    {!follorwing 
+                    ? (
+                      <Link to="#" className="btn-main" 
+                    onClick={() => follow()}>
+                      Follow
+                    </Link>
+                    ) 
+                    : (
+                      <Link to="#" className="btn-main" 
+                    onClick={() => unfollow()}>
+                      Unfollow
+                    </Link>
+                    )}
+                    
+                    
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </section>
+
+            <div className="col-md-12">
+                  <div className="de_tab tab_simple">
+                    <AuthorItems />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          ) 
+        : (
+          <section aria-label="section">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="d_profile de-flex">
+                    <div className="de-flex-col">
+                      <div className="profile_avatar">
+                        <Skeleton width={"150px"} height={"150px"} borderRadius={"50%"}/>
+                        <div className="profile_name">
+                          <h4>
+                            <Skeleton width={"200px"} height={"30px"} borderRadius={"12px"} />
+                            <span className="profile_username">
+                              <Skeleton width={"100px"} height={"20px"} borderRadius={"8px"} />
+                            </span>
+                            <span id="wallet" className="profile_wallet">
+                              <Skeleton width={"200px"} height={"15px"} borderRadius={"6px"} />
+                            </span>
+                          </h4>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="profile_follow de-flex">
+                      <div className="de-flex-col">
+                        <div className="profile_follower">
+                          <Skeleton width={"104px"} height={"26px"} borderRadius={"8px"}/>
+                        </div>
+                        <Skeleton width={"122px"} height={"42px"} borderRadius={"8px"}/>    
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-md-12">
+                  <div className="de_tab tab_simple">
+                    <AuthorItems />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        )}
+        </div>
       </div>
-    </div>
+          
   );
 };
 
