805c87f0afe27485dc89ed8d22e03cb8b8107d12
Sergio <deguzmansergei@gmail.com>
2023-02-26 06:27:40 -0700

    added dynamic link

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -89,7 +89,7 @@ const NewItems = () => {
                     </Link>
                   </div>
                   <div className="nft__item_info">
-                    <Link to="/item-details">
+                    <Link to={`/item-details/${item.nftId}`}>
                       <h4>{item.title}</h4>
                     </Link>
                     <div className="nft__item_price">{item.price} ETH</div>
