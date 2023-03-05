f08065f2b67cd59d8b0fe5f74aba2d51aec44bc9
stelko <stilyanmtoskov@gmail.com>
2023-03-05 18:11:55 +0000

    new items carousel, skeleton loading

---
+++ src/components/hooks/useCountDown.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+
+const useCountdown = (expiryDate) => {
+  const countDownDate = new Date(expiryDate).getTime();
+
+  const [countDown, setCountDown] = useState(
+    countDownDate - new Date().getTime()
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountDown(countDownDate - new Date().getTime());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [countDownDate]);
+
+  return getReturnValues(countDown);
+};
+
+const getReturnValues = (countDown) => {
+  const hours = Math.floor(
+    (countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
+
+  return [hours, minutes, seconds];
+};
+
+export { useCountdown };
