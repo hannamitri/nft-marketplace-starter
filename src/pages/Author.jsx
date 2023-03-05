a6a50d6a3602ed81fab9dfb741b9eb1bf293f247
stelko <stilyanmtoskov@gmail.com>
2023-03-05 21:09:17 +0000

    fetch author items and display them

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,51 +1,76 @@
-import React from "react";
-import AuthorBanner from "../images/author_banner.jpg";
-import AuthorItems from "../components/author/AuthorItems";
-import { Link } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
+import { useState, useEffect } from 'react';
+import AuthorBanner from '../images/author_banner.jpg';
+import AuthorItems from '../components/author/AuthorItems';
+import { Link, useParams } from 'react-router-dom';
+import AuthorImage from '../images/author_thumbnail.jpg';
+import axios from 'axios';
 
 const Author = () => {
-  return (
-    <div id="wrapper">
-      <div className="no-bottom no-top" id="content">
-        <div id="top"></div>
+  const [loading, setLoading] = useState(true);
+  const [author, setAuthor] = useState();
+  const { authorId } = useParams();
+
+  useEffect(() => {
+    const fetchAuthor = async () => {
+      const response = await axios(
+        `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${authorId}`
+      ).then((response) => response.data);
+      setAuthor(response);
+      setLoading(false);
+    };
+    fetchAuthor();
+  }, [authorId]);
+
+  const authorObj = {
+    authorId: author?.authorId,
+    authorImage: author?.authorImage,
+  };
+
+  return loading ? null : (
+    <div id='wrapper'>
+      <div className='no-bottom no-top' id='content'>
+        <div id='top'></div>
 
         <section
-          id="profile_banner"
-          aria-label="section"
-          className="text-light"
-          data-bgimage="url(images/author_banner.jpg) top"
+          id='profile_banner'
+          aria-label='section'
+          className='text-light'
+          data-bgimage='url(images/author_banner.jpg) top'
           style={{ background: `url(${AuthorBanner}) top` }}
         ></section>
 
-        <section aria-label="section">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12">
-                <div className="d_profile de-flex">
-                  <div className="de-flex-col">
-                    <div className="profile_avatar">
-                      <img src={AuthorImage} alt="" />
+        <section aria-label='section'>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-md-12'>
+                <div className='d_profile de-flex'>
+                  <div className='de-flex-col'>
+                    <div className='profile_avatar'>
+                      <img src={author.authorImage} alt='' />
 
-                      <i className="fa fa-check"></i>
-                      <div className="profile_name">
+                      <i className='fa fa-check'></i>
+                      <div className='profile_name'>
                         <h4>
-                          Monica Lucas
-                          <span className="profile_username">@monicaaaa</span>
-                          <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
+                          {author.authorName}
+                          <span className='profile_username'>
+                            @{author.tag}
                           </span>
-                          <button id="btn_copy" title="Copy Text">
+                          <span id='wallet' className='profile_wallet'>
+                            {author.address}
+                          </span>
+                          <button id='btn_copy' title='Copy Text'>
                             Copy
                           </button>
                         </h4>
                       </div>
                     </div>
                   </div>
-                  <div className="profile_follow de-flex">
-                    <div className="de-flex-col">
-                      <div className="profile_follower">573 followers</div>
-                      <Link to="#" className="btn-main">
+                  <div className='profile_follow de-flex'>
+                    <div className='de-flex-col'>
+                      <div className='profile_follower'>
+                        {author.followers} followers
+                      </div>
+                      <Link to='#' className='btn-main'>
                         Follow
                       </Link>
                     </div>
@@ -53,9 +78,13 @@ const Author = () => {
                 </div>
               </div>
 
-              <div className="col-md-12">
-                <div className="de_tab tab_simple">
-                  <AuthorItems />
+              <div className='col-md-12'>
+                <div className='de_tab tab_simple'>
+                  <AuthorItems
+                    nftCollection={author.nftCollection.map((nft) =>
+                      Object.assign(nft, authorObj)
+                    )}
+                  />
                 </div>
               </div>
             </div>
