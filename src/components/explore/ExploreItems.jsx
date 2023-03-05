b7146a981ca4402120f6c53de010b0083465c336
stelko <stilyanmtoskov@gmail.com>
2023-03-05 18:36:50 +0000

    fetching explore data and displaying

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,75 +1,149 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import AuthorImage from '../../images/author_thumbnail.jpg';
+import nftImage from '../../images/nftImage.jpg';
+import CountDown from '../home/CountDown';
 
 const ExploreItems = () => {
+  const [exploreItems, setExploreItems] = useState([]);
+
+  useEffect(() => {
+    const fetchExploreItems = async () => {
+      const response = await axios(
+        'https://us-central1-nft-cloud-functions.cloudfunctions.net/explore'
+      ).then((response) => response.data);
+      setExploreItems(response);
+    };
+    fetchExploreItems();
+  }, []);
+
   return (
     <>
       <div>
-        <select id="filter-items" defaultValue="">
-          <option value="">Default</option>
-          <option value="price_low_to_high">Price, Low to High</option>
-          <option value="price_high_to_low">Price, High to Low</option>
-          <option value="likes_high_to_low">Most liked</option>
+        <select id='filter-items' defaultValue=''>
+          <option value=''>Default</option>
+          <option value='price_low_to_high'>Price, Low to High</option>
+          <option value='price_high_to_low'>Price, High to Low</option>
+          <option value='likes_high_to_low'>Most liked</option>
         </select>
       </div>
-      {new Array(8).fill(0).map((_, index) => (
+      {exploreItems.map((item, index) => (
         <div
           key={index}
-          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-          style={{ display: "block", backgroundSize: "cover" }}
+          className='d-item col-lg-3 col-md-6 col-sm-6 col-xs-12'
+          style={{ display: 'block', backgroundSize: 'cover' }}
         >
-          <div className="nft__item">
-            <div className="author_list_pp">
+          <div className='nft__item'>
+            <div className='author_list_pp'>
               <Link
-                to="/author"
-                data-bs-toggle="tooltip"
-                data-bs-placement="top"
+                to={`/author/${item.authorId}`}
+                data-bs-toggle='tooltip'
+                data-bs-placement='top'
               >
-                <img className="lazy" src={AuthorImage} alt="" />
-                <i className="fa fa-check"></i>
+                <img className='lazy' src={item.authorImage} alt='' />
+                <i className='fa fa-check'></i>
               </Link>
             </div>
-            <div className="de_countdown">5h 30m 32s</div>
+            {item.expiryDate ? (
+              <CountDown expiryDate={item.expiryDate} />
+            ) : null}
 
-            <div className="nft__item_wrap">
-              <div className="nft__item_extra">
-                <div className="nft__item_buttons">
+            <div className='nft__item_wrap'>
+              <div className='nft__item_extra'>
+                <div className='nft__item_buttons'>
                   <button>Buy Now</button>
-                  <div className="nft__item_share">
+                  <div className='nft__item_share'>
                     <h4>Share</h4>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-facebook fa-lg"></i>
+                    <a href='' target='_blank' rel='noreferrer'>
+                      <i className='fa fa-facebook fa-lg'></i>
                     </a>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-twitter fa-lg"></i>
+                    <a href='' target='_blank' rel='noreferrer'>
+                      <i className='fa fa-twitter fa-lg'></i>
                     </a>
-                    <a href="">
-                      <i className="fa fa-envelope fa-lg"></i>
+                    <a href=''>
+                      <i className='fa fa-envelope fa-lg'></i>
                     </a>
                   </div>
                 </div>
               </div>
-              <Link to="/item-details">
-                <img src={nftImage} className="lazy nft__item_preview" alt="" />
+              <Link to={`/item-details/${item.nftId}`}>
+                <img
+                  src={item.nftImage}
+                  className='lazy nft__item_preview'
+                  alt=''
+                />
               </Link>
             </div>
-            <div className="nft__item_info">
-              <Link to="/item-details">
-                <h4>Pinky Ocean</h4>
+            <div className='nft__item_info'>
+              <Link to={`/item-details/${item.nftId}`}>
+                <h4>{item.title}</h4>
               </Link>
-              <div className="nft__item_price">1.74 ETH</div>
-              <div className="nft__item_like">
-                <i className="fa fa-heart"></i>
-                <span>69</span>
+              <div className='nft__item_price'>{item.price} ETH</div>
+              <div className='nft__item_like'>
+                <i className='fa fa-heart'></i>
+                <span>{item.likes}</span>
               </div>
             </div>
           </div>
         </div>
       ))}
-      <div className="col-md-12 text-center">
-        <Link to="" id="loadmore" className="btn-main lead">
+      {/* {new Array(8).fill(0).map((_, index) => (
+        <div
+          key={index}
+          className='d-item col-lg-3 col-md-6 col-sm-6 col-xs-12'
+          style={{ display: 'block', backgroundSize: 'cover' }}
+        >
+          <div className='nft__item'>
+            <div className='author_list_pp'>
+              <Link
+                to='/author'
+                data-bs-toggle='tooltip'
+                data-bs-placement='top'
+              >
+                <img className='lazy' src={AuthorImage} alt='' />
+                <i className='fa fa-check'></i>
+              </Link>
+            </div>
+            <div className='de_countdown'>5h 30m 32s</div>
+
+            <div className='nft__item_wrap'>
+              <div className='nft__item_extra'>
+                <div className='nft__item_buttons'>
+                  <button>Buy Now</button>
+                  <div className='nft__item_share'>
+                    <h4>Share</h4>
+                    <a href='' target='_blank' rel='noreferrer'>
+                      <i className='fa fa-facebook fa-lg'></i>
+                    </a>
+                    <a href='' target='_blank' rel='noreferrer'>
+                      <i className='fa fa-twitter fa-lg'></i>
+                    </a>
+                    <a href=''>
+                      <i className='fa fa-envelope fa-lg'></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <Link to='/item-details'>
+                <img src={nftImage} className='lazy nft__item_preview' alt='' />
+              </Link>
+            </div>
+            <div className='nft__item_info'>
+              <Link to='/item-details'>
+                <h4>Pinky Ocean</h4>
+              </Link>
+              <div className='nft__item_price'>1.74 ETH</div>
+              <div className='nft__item_like'>
+                <i className='fa fa-heart'></i>
+                <span>69</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))} */}
+      <div className='col-md-12 text-center'>
+        <Link to='' id='loadmore' className='btn-main lead'>
           Load more
         </Link>
       </div>
