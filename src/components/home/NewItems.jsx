233cc53822c23fa0887406b2862aff3635a593e0
mifouts <fouts.mitch15@gmail.com>
2023-03-09 23:47:44 -0500

    trying to fix css issue

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -2,8 +2,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import OwlCarousel from "react-owl-carousel";
-import "owl.carousel/dist/assets/owl.carousel.css";
-import "owl.carousel/dist/assets/owl.theme.default.css";
 import axios from "axios";
 import Skeleton from "../UI/Skeleton";
 
