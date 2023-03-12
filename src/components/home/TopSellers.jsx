d7942cf0eea81ab5873dc46464c8f2ef302599e1
mifouts <fouts.mitch15@gmail.com>
2023-03-12 18:34:01 -0400

    starting creating skeleton loading state

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -66,7 +66,7 @@ const TopSellers = () => {
                       </div>
                       <div className="author_list_info">
                         <Link to={`/author/${item.authorID}`}>
-                          Monica Lucas
+                          {item.authorName}
                         </Link>
                         <span>{item.price} ETH</span>
                       </div>
@@ -74,26 +74,29 @@ const TopSellers = () => {
                   ))}
                 </OwlCarousel>
               ) : (
-                <></>
+                <>
+                  <OwlCarousel className="owl-theme" {...options}>
+                    {new Array(12).fill(0).map((_, index) => (
+                      <li key={index}>
+                        <div className="author_list_pp">
+                          <Link to="/author">
+                            <img
+                              className="lazy pp-author"
+                              src={AuthorImage}
+                              alt=""
+                            />
+                            <i className="fa fa-check"></i>
+                          </Link>
+                        </div>
+                        <div className="author_list_info">
+                          <Link to="/author">Monica Lucas</Link>
+                          <span>2.1 ETH</span>
+                        </div>
+                      </li>
+                    ))}
+                  </OwlCarousel>
+                </>
               )}
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
-                  </div>
-                </li>
-              ))}
             </ol>
           </div>
         </div>
