a34d7f52bf35df2e39345e8e3c4d193b65302a26
mifouts <fouts.mitch15@gmail.com>
2023-03-12 14:32:24 -0400

    starting dynamic countdown clock for each NFT

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Countdown() {
+const Countdown = ({ expiryDate }) => {
+  const [time, setTime] = useState("");
+  const [intervaId, setIntervalId] = useState();
+
+  React.useEffect(() => {
+    calculateTime();
+  }, 1000);
+  }
+
+  function calculateTime() {
+    const millisLeft = expiryDate - Date.now();
+
+    if (millisLeft < 0) {
+      clearInterval(intervalId);
+      setTime("EXPIRED");
+      return;
+      
+    }
+  }
   return <div>Countdown</div>;
 }
 
