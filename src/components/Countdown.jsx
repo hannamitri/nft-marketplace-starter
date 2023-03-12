f9b1c0369050a56f849ea9aa20dd81cd20697335
mifouts <fouts.mitch15@gmail.com>
2023-03-12 14:40:32 -0400

    finishing up dynamic countdown clock

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -2,12 +2,21 @@ import React, { useState } from "react";
 
 const Countdown = ({ expiryDate }) => {
   const [time, setTime] = useState("");
-  const [intervaId, setIntervalId] = useState();
+  const [intervalId, setIntervalId] = useState();
 
   React.useEffect(() => {
     calculateTime();
-  }, 1000);
-  }
+
+    const intervalId = setInterval(() => {
+      calculateTime();
+    }, 1000);
+
+    setIntervalId(intervalId);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
   function calculateTime() {
     const millisLeft = expiryDate - Date.now();
@@ -16,10 +25,19 @@ const Countdown = ({ expiryDate }) => {
       clearInterval(intervalId);
       setTime("EXPIRED");
       return;
-      
     }
+
+    const secondsLeft = millisLeft / 1000;
+    const minutesLeft = secondsLeft / 60;
+    const hoursLeft = minutesLeft / 60;
+
+    setTime(
+      `${Math.floor(hoursLeft)}h ${Math.floor(minutesLeft % 60)}m ${Math.floor(
+        secondsLeft % 60
+      )}s`
+    );
   }
-  return <div>Countdown</div>;
-}
+  return <div className="de_countdown">{time}</div>;
+};
 
 export default Countdown;
