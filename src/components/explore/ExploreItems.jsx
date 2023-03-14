adc1934acfb064199b6ba5c4941f23cdab4d542f
mifouts <fouts.mitch15@gmail.com>
2023-03-14 16:24:47 -0400

    using nft item component for cleaner code

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -5,6 +5,7 @@ import nftImage from "../../images/nftImage.jpg";
 import axios from "axios";
 import Skeleton from "../UI/Skeleton";
 import Countdown from "../Countdown";
+import NFTitem from "../NFTitem";
 
 const ExploreItems = () => {
   const [explore, setExplore] = useState([]);
@@ -51,39 +52,7 @@ const ExploreItems = () => {
             className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
             style={{ display: "block", backgroundSize: "cover" }}
           >
-            <div className="nft__item">
-              <div className="author_list_pp">
-                <Link
-                  to={`/author/${item.authorId}`}
-                  data-bs-toggle="tooltip"
-                  data-bs-placement="top"
-                >
-                  <img className="lazy" src={item.AuthorImage} alt="" />
-                  <i className="fa fa-check"></i>
-                </Link>
-              </div>
-              {item.expiryDate && <Countdown expiryDate={item.expiryDate} />}
-
-              <div className="nft__item_wrap">
-                <Link to={`/item-details/${item.nftId}`}>
-                  <img
-                    src={item.nftImage}
-                    className="lazy nft__item_preview"
-                    alt=""
-                  />
-                </Link>
-              </div>
-              <div className="nft__item_info">
-                <Link to="/item-details">
-                  <h4>{item.title}</h4>
-                </Link>
-                <div className="nft__item_price">{item.price} ETH</div>
-                <div className="nft__item_like">
-                  <i className="fa fa-heart"></i>
-                  <span>{item.likes}</span>
-                </div>
-              </div>
-            </div>
+            <NFTitem />
           </div>
         ))
       ) : (
