e7950af6b9e38922accfd3802624398733d05ff2
mifouts <fouts.mitch15@gmail.com>
2023-03-14 16:01:30 -0400

    making explore items dynamic/styling skeletonState

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -65,26 +65,9 @@ const ExploreItems = () => {
               {item.expiryDate && <Countdown expiryDate={item.expiryDate} />}
 
               <div className="nft__item_wrap">
-                <div className="nft__item_extra">
-                  <div className="nft__item_buttons">
-                    <button>Buy Now</button>
-                    <div className="nft__item_share">
-                      <h4>Share</h4>
-                      <a href="" target="_blank" rel="noreferrer">
-                        <i className="fa fa-facebook fa-lg"></i>
-                      </a>
-                      <a href="" target="_blank" rel="noreferrer">
-                        <i className="fa fa-twitter fa-lg"></i>
-                      </a>
-                      <a href="">
-                        <i className="fa fa-envelope fa-lg"></i>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                <Link to="/item-details">
+                <Link to={`/item-details/${item.nftId}`}>
                   <img
-                    src={nftImage}
+                    src={item.nftImage}
                     className="lazy nft__item_preview"
                     alt=""
                   />
@@ -92,12 +75,12 @@ const ExploreItems = () => {
               </div>
               <div className="nft__item_info">
                 <Link to="/item-details">
-                  <h4>Pinky Ocean</h4>
+                  <h4>{item.title}</h4>
                 </Link>
-                <div className="nft__item_price">1.74 ETH</div>
+                <div className="nft__item_price">{item.price} ETH</div>
                 <div className="nft__item_like">
                   <i className="fa fa-heart"></i>
-                  <span>69</span>
+                  <span>{item.likes}</span>
                 </div>
               </div>
             </div>
@@ -111,7 +94,7 @@ const ExploreItems = () => {
               className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
               style={{ display: "block", backgroundSize: "cover" }}
             >
-              <Skeleton />
+              <Skeleton width="100%" height="400px" />
             </div>
           ))}
         </>
