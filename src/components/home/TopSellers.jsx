ebf5da91fcb3b7b7c6ed4cd57854075d25fd0f62
zacharychiko16 <zacharychiko16@gmail.com>
2023-03-17 23:41:18 -0400

    forgot to clean up Top sellers

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,67 +1,3 @@
-// import React, {useEffect, useState} from "react";
-// import { Link } from "react-router-dom";
-// import axios from "axios";
-
-// const TopSellers = () => {
-//   const [topSellers, setTopSellers] = useState([]);
-//   const [isLoading, setIsLoading] = useState(true); 
-//   useEffect(() => {
-//     axios
-//       .get(
-//         "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers"
-//       )
-//       .then((response) => {
-//         setTopSellers(response.data);
-//         console.log(response.data)
-//         setIsLoading(false);
-//       })
-//       .catch((error) => {
-//         console.error(error);
-//       });
-//   }, []);
-
-//   return (
-//     <section id="section-popular" className="pb-5">
-//       <div className="container">
-//         <div className="row">
-//           <div className="col-lg-12">
-//             <div className="text-center">
-//               <h2>Top Sellers</h2>
-//               <div className="small-border bg-color-2"></div>
-//             </div>
-//           </div>
-//           <div className="col-md-12">
-//             <ol className="author_list">
-//               {topSellers.map((topSeller) => (
-//                 <li key={topSeller.id}>
-//                   <div className="author_list_pp">
-//                     <Link to={`/author/${topSeller.authorId}`}>
-//                       <img
-//                         className="lazy pp-author"
-//                         src={topSeller.authorImage}
-//                         alt=""
-//                       />
-//                       <i className="fa fa-check"></i>
-//                     </Link>
-//                   </div>
-//                   <div className="author_list_info">
-//                     <Link  to={`/author/${topSeller.authorId}`}>
-//                      {topSeller.authorName}
-//                       </Link>
-//                     <span>{topSeller.price} ETH</span>
-//                   </div>
-//                 </li>
-//               ))}
-//             </ol>
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default TopSellers;
-
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
@@ -77,7 +13,6 @@ const TopSellers = () => {
       )
       .then((response) => {
         setTopSellers(response.data);
-        console.log(response.data);
         setIsLoading(false);
       })
       .catch((error) => {
