f9bd26feb775f690aa616714f1ab089f487049e1
Karol <K_wlodarczyk@interia.pl>
2023-03-20 17:03:04 +0100

    loading state 2

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -6,20 +6,24 @@ import { useEffect } from "react";
 
 import "../../css/HotCollections.css"
 
-import OwlCarousel from 'react-owl-carousel';
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel/dist/assets/owl.theme.default.css';
+// import OwlCarousel from 'react-owl-carousel';
+// import 'owl.carousel/dist/assets/owl.carousel.css';
+// import 'owl.carousel/dist/assets/owl.theme.default.css';
 import Skeleton from "react-loading-skeleton";
+
+import Slider from 'react-slick' 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const HotCollections = () => {
 
   const [imgs, setImgs] = useState([])
-  const [loading, setLoading] = useState()
+  const [loading, setLoading] = useState(true)
   
   async function Imgdata() {
-    setLoading(false)
     const {data} = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections")
     setImgs(data)
+    setLoading(false)
   }
   
   
@@ -27,32 +31,101 @@ const HotCollections = () => {
     Imgdata()
   }, [])
 
-  const options = {
-    items:4,
-    responsive: {
-      0: {
-        items:1
-      },
-      640: {
-        items:2
-      },
-      1050:{
-        items:3
-      },
-      1620:{
-        items:4
-      }
-    },
-    loop: true,
-    margin: 15,
-    nav: true, 
+  const settings = {
+    slidesToShow: 4,
+    accessibility: true,
+    draggable: false,
     dots: false,
-    smartSpeed: 150,
+    arrows: true,
+    infinite: true,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          arrows: true,
+           infinite: true,
+          
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          arrows: true,
+           infinite: true,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: true,
+          infinite: true,
+        }
+      }
+    ]
   }
 
+  function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "black",}}
+        onClick={onClick}
+      />
+    );
+  }
+  
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "black" }}
+        onClick={onClick}
+      />
+    );
+  }
+
+  // const options = {
+  //   items:4,
+  //   loop: true,
+  //   margin: 10,
+  //   nav: true, 
+  //   dots: false,
+  //   responsive: {
+  //     0: {
+  //       items:1
+  //     },
+  //     640: {
+  //       items:2
+  //     },
+  //     1050:{
+  //       items:3
+  //     },
+  //     1620:{
+  //       items:4
+  //     }
+  //   },
+  // }
 
   return (
     <section id="section-collections" className="no-bottom">
+      <div className="post">
+            <div className="post__title">
+              <div className="post__title--skeleton"></div>
+            </div>
+            <div className="post__body">
+              <p className="post__body--skeleton"></p>
+            </div>
+          </div>
       <div className="container">
         <div className="row " >
         
@@ -62,8 +135,8 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          
-          <OwlCarousel className="owl-theme" {...options}>
+
+          <Slider  {...settings}>
           {
             loading ? (
               new Array(4).fill(0).map((_, index) =>{
@@ -78,37 +151,117 @@ const HotCollections = () => {
               })
             )
             :
-            (
-                imgs.map(img => (
-                  <div className="nft_coll">
-                <div className="nft_wrap ">
-                  <Link to="item-details" 
-                  // {`/${item-details}`}
-                  >
-                    <img src={img.nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
-                </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author"
-                  // {`/${author}`}
-                  >
-                    <img className="lazy pp-coll" src={img.authorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore"
-                  // {`/${explore}`}
-                  >
-                    <h4>{img.title}</h4>
-                  </Link>
-                  <span>ERC-${img.code}</span>
-                </div>
-              </div>  
-            ))
+              (
+              imgs.map(img => (
+                <div className="nft_coll">
+              <div className="nft_wrap ">
+                <Link to="item-details" 
+                // {`/${item-details}`}
+                >
+                  <img src={img.nftImage} className="lazy img-fluid" alt="" />
+                </Link>
+              </div>
+              <div className="nft_coll_pp">
+                <Link to="/author"
+                // {`/${author}`}
+                >
+                  <img className="lazy pp-coll" src={img.authorImage} alt="" />
+                </Link>
+                <i className="fa fa-check"></i>
+              </div>
+              <div className="nft_coll_info">
+                <Link to="/explore"
+                // {`/${explore}`}
+                >
+                  <h4>{img.title}</h4>
+                </Link>
+                <span>ERC-${img.code}</span>
+              </div>
+            </div>
+              ))
           )
         }
-        </OwlCarousel>
+      </Slider>
+
+        {/* <Slider  {...settings}>
+        {
+          imgs.map(img => (
+                   <div className="nft_coll">
+                 <div className="nft_wrap ">
+                   <Link to="item-details" 
+                   // {`/${item-details}`}
+                   >
+                     <img src={img.nftImage} className="lazy img-fluid" alt="" />
+                   </Link>
+                 </div>
+                 <div className="nft_coll_pp">
+                   <Link to="/author"
+                   // {`/${author}`}
+                   >
+                     <img className="lazy pp-coll" src={img.authorImage} alt="" />
+                   </Link>
+                   <i className="fa fa-check"></i>
+                 </div>
+                 <div className="nft_coll_info">
+                   <Link to="/explore"
+                   // {`/${explore}`}
+                   >
+                     <h4>{img.title}</h4>
+                   </Link>
+                   <span>ERC-${img.code}</span>
+                 </div>
+               </div>
+          ))
+        }
+      </Slider> */}
+
+          { 
+          /* {
+            loading ? (
+              new Array(4).fill(0).map((_, index) =>{
+                <div className="post" key={index}>
+            <div className="post__title">
+              <div className="post__title--skeleton"></div>
+            </div>
+            <div className="post__body">
+              <p className="post__body--skeleton"></p>
+            </div>
+          </div>
+              })
+            )
+            :
+            ( */
+              //   imgs.map(img => (
+              //     <div className="nft_coll">
+              //   <div className="nft_wrap ">
+              //     <Link to="item-details" 
+              //     // {`/${item-details}`}
+              //     >
+              //       <img src={img.nftImage} className="lazy img-fluid" alt="" />
+              //     </Link>
+              //   </div>
+              //   <div className="nft_coll_pp">
+              //     <Link to="/author"
+              //     // {`/${author}`}
+              //     >
+              //       <img className="lazy pp-coll" src={img.authorImage} alt="" />
+              //     </Link>
+              //     <i className="fa fa-check"></i>
+              //   </div>
+              //   <div className="nft_coll_info">
+              //     <Link to="/explore"
+              //     // {`/${explore}`}
+              //     >
+              //       <h4>{img.title}</h4>
+              //     </Link>
+              //     <span>ERC-${img.code}</span>
+              //   </div>
+              // </div>  
+            // ))
+          // )
+        }
+
+
         </div>
       </div>
     </section>
