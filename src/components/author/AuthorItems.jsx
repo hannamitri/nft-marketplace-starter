73332a1cc274d59fbe006c3915a316c0e2163158
mifouts <fouts.mitch15@gmail.com>
2023-03-23 15:29:27 -0400

    making nfts display dynamically

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -1,8 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
 import Skeleton from "../UI/Skeleton";
 
 const AuthorItems = ({ author }) => {
@@ -18,7 +16,7 @@ const AuthorItems = ({ author }) => {
         <div className="row">
           {!author || !author.nftCollection
             ? skeletonLoading
-            : author.nftCollection.map((_, index) => (
+            : author.nftCollection.map((item, index) => (
                 <div
                   className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
                   key={index}
@@ -26,44 +24,27 @@ const AuthorItems = ({ author }) => {
                   <div className="nft__item">
                     <div className="author_list_pp">
                       <Link to="">
-                        <img className="lazy" src={AuthorImage} alt="" />
+                        <img className="lazy" src={author.AuthorImage} alt="" />
                         <i className="fa fa-check"></i>
                       </Link>
                     </div>
                     <div className="nft__item_wrap">
-                      <div className="nft__item_extra">
-                        <div className="nft__item_buttons">
-                          <button>Buy Now</button>
-                          <div className="nft__item_share">
-                            <h4>Share</h4>
-                            <a href="" target="_blank" rel="noreferrer">
-                              <i className="fa fa-facebook fa-lg"></i>
-                            </a>
-                            <a href="" target="_blank" rel="noreferrer">
-                              <i className="fa fa-twitter fa-lg"></i>
-                            </a>
-                            <a href="">
-                              <i className="fa fa-envelope fa-lg"></i>
-                            </a>
-                          </div>
-                        </div>
-                      </div>
-                      <Link to="/item-details">
+                      <Link to={`/item-details/${item.nftId}`}>
                         <img
-                          src={nftImage}
+                          src={item.nftImage}
                           className="lazy nft__item_preview"
                           alt=""
                         />
                       </Link>
                     </div>
                     <div className="nft__item_info">
-                      <Link to="/item-details">
-                        <h4>Pinky Ocean</h4>
+                      <Link to={`/item-details/${item.nftId}`}>
+                        <h4>{item.title}</h4>
                       </Link>
-                      <div className="nft__item_price">2.52 ETH</div>
+                      <div className="nft__item_price">{item.price} ETH</div>
                       <div className="nft__item_like">
                         <i className="fa fa-heart"></i>
-                        <span>97</span>
+                        <span>{item.likes}</span>
                       </div>
                     </div>
                   </div>
