e6e174e130047ea9a2301e7a785838e662c89d55
liveMehdi <mo@mehdi.life>
2023-03-24 20:33:29 +0000

    Added top sellers feature, tweaked dynamic routing for new items feature

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -67,7 +67,7 @@ const NewItems = () => {
                   <div className="nft__item">
                     <div className="author_list_pp">
                       <Link
-                        to="/author"
+                        to={ `/author/${item.authorId}`}
                         data-bs-toggle="tooltip"
                         data-bs-placement="top"
                         title="Creator: Monica Lucas"
--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 
 const TopSellers = () => {
+  const [sellers, setSellers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  async function getSellers() {
+    const { data } = await axios.get(
+      "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers"
+    );
+    setSellers(data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    getSellers();
+  }, []);
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,24 +31,61 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
+              {!loading &&
+                sellers.map((seller, index) => (
+                  <li key={index}>
+                    <div className="author_list_pp">
+                      <Link to={`/author/${seller.authorId}`}>
+                        <img
+                          className="lazy pp-author"
+                          src={seller.authorImage}
+                          alt=""
+                        />
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <Link to={`/author/${seller.authorId}`}>
+                        {seller.authorName}
+                      </Link>
+                      <span>{seller.price} ETH</span>
+                    </div>
+                  </li>
+                ))}
+              {loading &&
+                new Array(12).fill(0).map((_, index) => (
+                  <li key={index}>
+                    <div className="author_list_pp">
+                      <div
+                        className="skeleton-box"
                         alt=""
-                      />
+                        style={{
+                          width: "50px",
+                          height: "50px",
+                          borderRadius: "50px",
+                        }}
+                      ></div>
                       <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
-                  </div>
-                </li>
-              ))}
+                    </div>
+                    <div className="author_list_info">
+                      <div
+                        className="skeleton-box"
+                        style={{
+                          width: "100px",
+                          height: "20px",
+                        }}
+                      ></div>
+                      <div
+                        className="skeleton-box"
+                        style={{
+                          width: "40px",
+                          height: "20px",
+                          display: "block",
+                        }}
+                      ></div>
+                    </div>
+                  </li>
+                ))}
             </ol>
           </div>
         </div>
