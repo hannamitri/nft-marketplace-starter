c9d24f2454b67ea35b9a001efeb863778bc709ee
ElieMitri <eliegmitri7@gmail.com>
2023-03-26 17:38:02 +0300

    fixed

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,102 @@
-import React from "react";
+import React, { Component } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import "./hotcollection.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const HotCollections = () => {
+  const [hotCollection, setHotCollection] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  async function fetchData() {
+    setLoading(true);
+    const response = await axios(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections`
+    );
+
+    setHotCollection(response.data);
+    console.log(response.data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const PrevArrow = ({ onClick }) => (
+    <button className="slick-prev custom-prev" onClick={onClick}>
+      <svg viewBox="0 0 24 24">
+        <path d="M15 4L7 12L15 20" />
+      </svg>
+    </button>
+  );
+
+  const NextArrow = ({ onClick }) => (
+    <button className="slick-next custom-next" onClick={onClick}>
+      <svg viewBox="0 0 24 24">
+        <path d="M9 4L17 12L9 20" />
+      </svg>
+    </button>
+  );
+
+  const settings = {
+    // dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />,
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1190,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 990,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 920,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 763,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      
+    ],
+  };
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,29 +107,85 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
-                </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <span>ERC-192</span>
-                </div>
-              </div>
-            </div>
-          ))}
+          <div className="carousel-container">
+            <Slider {...settings}>
+              {loading
+                ? new Array(6).fill(0).map((_, id) => (
+                    <div
+                      className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
+                      key={id}
+                    >
+                      <div className="nft_coll">
+                        <div className="nft_wrap">
+                          <div className="main-item-input">
+                            <div className="animated-poster-background-input"></div>
+                            <div className="animated-title-background-input"></div>
+                            <Link to="/item-details">
+                              <img
+                                className="animated-title-background-input lazy img-fluid"
+                                alt=""
+                              />
+                            </Link>
+                          </div>
+                          <div className="nft_coll_pp">
+                            <Link to="/author">
+                              <img
+                                className="animated-title-background-input lazy pp-coll"
+                                alt=""
+                              />
+                            </Link>
+                            <i className="fa fa-check"></i>
+                          </div>
+                          <div className="nft_coll_info">
+                            <Link to="/explore">
+                              <h4 className="animated-title-background-input"></h4>
+                            </Link>
+                            <span className="animated-title-background-input"></span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ))
+                : hotCollection.map((response, id) => (
+                    <div className="response__wrapper">
+                      <div
+                        className="col-lg-3 col-md-6 col-sm-6 col-xs-12 response__wrap"
+                        key={id}
+                      >
+                        <div className="nft__wrapper">
+                          <div className="nft_coll">
+                            <div className="nft_wrap">
+                              <Link to="/item-details">
+                                <img
+                                  src={response.nftImage}
+                                  className="lazy img-fluid"
+                                  alt=""
+                                />
+                              </Link>
+                            </div>
+                            <div className="nft_coll_pp">
+                              <Link to="/author">
+                                <img
+                                  className="lazy pp-coll"
+                                  src={response.authorImage}
+                                  alt=""
+                                />
+                              </Link>
+                              <i className="fa fa-check"></i>
+                            </div>
+                            <div className="nft_coll_info">
+                              <Link to="/explore">
+                                <h4>{response.title}</h4>
+                              </Link>
+                              <span>ERC-{response.code}</span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+            </Slider>
+          </div>
         </div>
       </div>
     </section>
--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,22 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
 import EthImage from "../images/ethereum.svg";
 import { Link } from "react-router-dom";
 import AuthorImage from "../images/author_thumbnail.jpg";
 import nftImage from "../images/nftImage.jpg";
+import axios from "axios";
 
 const ItemDetails = () => {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+  const { nftId } = useParams();
+  const [hotCollection, setMovie] = useState([]);
+  const [loading, setLoading] = useState(false);
+  
+  async function fetchData() {
+    setLoading(true);
+    const response = await axios.get(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollection${nftId}`
+      );
+      setMovie(response.data);
+      setLoading(false);
+      // const item = response.data.find((item) => +item.nftId === nftId);
+    }
+    
+    useEffect(() => {
+      window.scrollTo(0, 0);
+      fetchData()
+    }, []);
 
-  return (
+    
+    
+    return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
         <div id="top"></div>
         <section aria-label="section" className="mt90 sm-mt-0">
           <div className="container">
             <div className="row">
-              <div className="col-md-6 text-center">
+              {
+              <>
+               <div className="col-md-6 text-center">
                 <img
                   src={nftImage}
                   className="img-fluid img-rounded mb-sm-30 nft-image"
@@ -83,6 +104,76 @@ const ItemDetails = () => {
                   </div>
                 </div>
               </div>
+              </>
+              }
+              {/* <div className="col-md-6 text-center">
+                <img
+                  src={nftImage}
+                  className="img-fluid img-rounded mb-sm-30 nft-image"
+                  alt=""
+                />
+              </div>
+              <div className="col-md-6">
+                <div className="item_info">
+                  <h2>Rainbow Style #194</h2>
+
+                  <div className="item_info_counts">
+                    <div className="item_info_views">
+                      <i className="fa fa-eye"></i>
+                      100
+                    </div>
+                    <div className="item_info_like">
+                      <i className="fa fa-heart"></i>
+                      74
+                    </div>
+                  </div>
+                  <p>
+                    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+                    illo inventore veritatis et quasi architecto beatae vitae
+                    dicta sunt explicabo.
+                  </p>
+                  <div className="d-flex flex-row">
+                    <div className="mr40">
+                      <h6>Owner</h6>
+                      <div className="item_author">
+                        <div className="author_list_pp">
+                          <Link to="/author">
+                            <img className="lazy" src={AuthorImage} alt="" />
+                            <i className="fa fa-check"></i>
+                          </Link>
+                        </div>
+                        <div className="author_list_info">
+                          <Link to="/author">Monica Lucas</Link>
+                        </div>
+                      </div>
+                    </div>
+                    <div></div>
+                  </div>
+                  <div className="de_tab tab_simple">
+                    <div className="de_tab_content">
+                      <h6>Creator</h6>
+                      <div className="item_author">
+                        <div className="author_list_pp">
+                          <Link to="/author">
+                            <img className="lazy" src={AuthorImage} alt="" />
+                            <i className="fa fa-check"></i>
+                          </Link>
+                        </div>
+                        <div className="author_list_info">
+                          <Link to="/author">Monica Lucas</Link>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="spacer-40"></div>
+                    <h6>Price</h6>
+                    <div className="nft-item-price">
+                      <img src={EthImage} alt="" />
+                      <span>1.85</span>
+                    </div>
+                  </div>
+                </div>
+              </div> */}
+              
             </div>
           </div>
         </section>
