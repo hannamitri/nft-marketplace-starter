4bccc8763209d547befd50682620e40ad0c0e42b
mifouts <fouts.mitch15@gmail.com>
2023-04-03 22:51:16 -0400

    importing aos and putting animations in

--- src/components/home/LandingIntro.jsx
+++ src/components/home/LandingIntro.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const LandingIntro = () => {
+  useEffect(() => {
+    Aos.init();
+  }, []);
   return (
     <section id="section-intro" className="no-top no-bottom">
       <div className="container">
         <div className="row">
           <div className="col-lg-4 col-md-6 mb-sm-30">
-            <div className="feature-box f-boxed style-3">
+            <div
+              className="feature-box f-boxed style-3"
+              data-aos="fade-right"
+              data-aos-duration="600"
+            >
               <i className="bg-color-2 i-boxed icon_wallet"></i>
               <div className="text">
                 <h4 className="">Set up your wallet</h4>
@@ -19,7 +28,11 @@ const LandingIntro = () => {
             </div>
           </div>
           <div className="col-lg-4 col-md-6 mb-sm-30">
-            <div className="feature-box f-boxed style-3">
+            <div
+              className="feature-box f-boxed style-3"
+              data-aos="fade-up"
+              data-aos-duration="600"
+            >
               <i className="bg-color-2 i-boxed icon_cloud-upload_alt"></i>
               <div className="text">
                 <h4 className="">Add your NFT's</h4>
@@ -32,7 +45,11 @@ const LandingIntro = () => {
             </div>
           </div>
           <div className="col-lg-4 col-md-6 mb-sm-30">
-            <div className="feature-box f-boxed style-3">
+            <div
+              className="feature-box f-boxed style-3"
+              data-aos="fade-left"
+              data-aos-duration="600"
+            >
               <i className="bg-color-2 i-boxed icon_tags_alt"></i>
               <div className="text">
                 <h4 className="">Sell your NFT's</h4>
