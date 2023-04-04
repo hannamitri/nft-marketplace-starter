6858813e51a46e9f9fb3a5152bcc372f58cd159a
mifouts <fouts.mitch15@gmail.com>
2023-04-03 21:31:54 -0400

    importing aos

--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NFT from "../../images/nft.png";
 import backgroundImage from "../../images/bg-shape-1.jpg";
 import { Link } from "react-router-dom";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const Landing = () => {
+  useEffect(() => {
+    Aos.init();
+  }, []);
   return (
     <section
       id="section-hero"
