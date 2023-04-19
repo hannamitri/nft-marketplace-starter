1ab870bd75bae707e965f1e617ff2656b30cd943
username <Ryan>
2023-04-19 16:49:26 -0500

    items-details update/edit

--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import EthImage from "../images/ethereum.svg";
 import { Link, useParams } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
-import nftImage from "../images/nftImage.jpg";
 import axios from "axios";
 import Skeleton, { ItemDetailSkeleton } from "../components/UI/Skeleton";
 
