be9595af4bade86e067f86f59153f3f50b3b2c28
PatrikasDapsys <patrikas.dapsys@gmail.com>
2023-04-26 21:29:53 +0300

    fetch hotCollections data and display it

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
 
 const HotCollections = () => {
+  const [collectionsData, setCollectionsData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const { data } = await axios.get(
+        "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections"
+      );
+      setCollectionsData(data);
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(collectionsData)
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,25 +28,36 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+          {collectionsData.map((elem) => (
+            <div
+              className="col-lg-3 col-md-6 col-sm-6 col-xs-12 keen-slider__slide"
+              key={elem.id}
+            >
               <div className="nft_coll">
                 <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
+                  <Link to={`/item-details/${elem.nftId}`}>
+                    <img
+                      src={elem.nftImage}
+                      className="lazy img-fluid"
+                      alt=""
+                    />
                   </Link>
                 </div>
                 <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
+                  <Link to={`/author/${elem.authorId}`}>
+                    <img
+                      className="lazy pp-coll"
+                      src={elem.authorImage}
+                      alt=""
+                    />
                   </Link>
                   <i className="fa fa-check"></i>
                 </div>
                 <div className="nft_coll_info">
                   <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
+                    <h4>{elem.title}</h4>
                   </Link>
-                  <span>ERC-192</span>
+                  <span>ERC-{elem.code}</span>
                 </div>
               </div>
             </div>
