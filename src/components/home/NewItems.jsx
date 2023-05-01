32be0992c33bf88c07cd147629b5223f7e17413e
Hatem Soliman <hatemthedev@gmail.com>
2023-05-01 18:41:41 +0300

    Implemented Owl Carousel to New Items section

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
 import axios from "axios";
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+import Skeleton from "../UI/Skeleton";
 
 const NewItems = () => {
 
@@ -32,62 +34,134 @@ const NewItems = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {newItems.map((newItem) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={newItem.id}>
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link
-                    to="/author"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    title="Creator: Monica Lucas"
-                  >
-                    <img className="lazy" src={newItem.authorImage} alt={`${newItem.title}'s Author Image`} />
+          {loading ? (
+            <OwlCarousel
+              className="owl-theme"
+              loop
+              margin={10}
+              responsiveClass
+              nav
+              responsive={{
+                0: {
+                  items: 1,
+                },
+                600: {
+                  items: 2,
+                },
+                1000: {
+                  items: 4,
+                },
+              }}
+            >
+              {new Array(8).fill(0).map((_, index) => (
+                <div className="nft_coll" key={index}>
+                  <div className="nft_wrap">
+                    <Link to={``}>
+                      <Skeleton width="100%" height="200px" />
+                    </Link>
+                  </div>
+                  <div className="nft_coll_pp">
+                    <Link to={``}>
+                      <Skeleton
+                        width="50px"
+                        height="50px"
+                        borderRadius="50%"
+                      />
+                    </Link>
                     <i className="fa fa-check"></i>
-                  </Link>
+                  </div>
+                  <div className="nft_coll_info">
+                    <Link to="">
+                      <Skeleton width="100px" height="20px" />
+                    </Link>
+                    <br />
+                    <Skeleton width="60px" height="20px" />
+                  </div>
                 </div>
-                <div className="de_countdown">{newItem.expiryDate}</div>
+              ))}
+            </OwlCarousel>
+          ) : (
+            <OwlCarousel
+              className="owl-theme"
+              loop
+              margin={10}
+              responsiveClass
+              nav
+              responsive={{
+                0: {
+                  items: 1,
+                },
+                600: {
+                  items: 2,
+                },
+                1000: {
+                  items: 4,
+                },
+              }}
+            >
+              {newItems.map((newItem) => (
+                <div className="nft__item" key={newItem.id}>
+                  <div className="author_list_pp">
+                    <Link
+                      to={`/author/${newItem.authorId}`}
+                      data-bs-toggle="tooltip"
+                      data-bs-placement="top"
+                      title="Creator: Monica Lucas"
+                    >
+                      <img className="lazy" src={newItem.authorImage} alt="" />
+                      <i className="fa fa-check"></i>
+                    </Link>
+                  </div>
 
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
+                  <div className="nft__item_wrap">
+                    <div className="nft__item_extra">
+                      <div className="nft__item_buttons">
+                        <button>Buy Now</button>
+                        <div className="nft__item_share">
+                          <h4>Share</h4>
+                          <a
+                            href="https://www.facebook.com/sharer/sharer.php?u=https://gigaland.io"
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            <i className="fa fa-facebook fa-lg"></i>
+                          </a>
+                          <a
+                            href="https://twitter.com/intent/tweet?url=https://gigaland.io"
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            <i className="fa fa-twitter fa-lg"></i>
+                          </a>
+                          <a href="mailto:?subject=I wanted you to see this site&amp;body=Check out this site https://gigaland.io">
+                            <i className="fa fa-envelope fa-lg"></i>
+                          </a>
+                        </div>
                       </div>
                     </div>
-                  </div>
 
-                  <Link to="/item-details">
-                    <img
-                      src={newItem.nftImage}
-                      className="lazy nft__item_preview"
-                      alt={newItem.title}
-                    />
-                  </Link>
-                </div>
-                <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>{newItem.title}</h4>
-                  </Link>
-                  <div className="nft__item_price">{newItem.price} ETH</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>{newItem.likes}</span>
+                    <Link to={`/item-details/${newItem.nftId}`}>
+                      <img
+                        src={newItem.nftImage}
+                        className="lazy nft__item_preview"
+                        alt=""
+                      />
+                    </Link>
+                  </div>
+                  <div className="nft__item_info">
+                    <Link to={`/item-details/${newItem.nftId}`}>
+                      <h4>{newItem.title}</h4>
+                    </Link>
+                    <div className="nft__item_price">{newItem.price} ETH</div>
+                    <div className="nft__item_like">
+                      <i className="fa fa-heart"></i>
+                      <span>{newItem.likes}</span>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
-          ))}
+              ))}
+            </OwlCarousel>
+          )}
         </div>
       </div>
     </section>
