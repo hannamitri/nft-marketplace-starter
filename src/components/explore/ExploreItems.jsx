132bb1e224332d70f4dc8791165b4f5cb267d56a
Hatem Soliman <hatemthedev@gmail.com>
2023-05-02 05:04:46 +0300

    Implemented Skeleton Loading, API-filter edition, & Load button

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,95 +1,113 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
 import axios from "axios";
+import Skeleton from "../UI/Skeleton";
+import Countdown from "../Countdown";
 
 const ExploreItems = () => {
 
-  const [nfts, setNFTS] = useState([]);
+  const [nfts, setNFTs] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [itemCount, setItemCount] = useState(8);
 
   useEffect(() => {
     fetchNFTs();
-  },[!loading])
+  },[])
 
   async function fetchNFTs() {
     const { data } = await axios.get(
       "https://us-central1-nft-cloud-functions.cloudfunctions.net/explore"
     );
-    setNFTS(data);
+    setNFTs(data);
+  }
+
+  async function filterNFTs(filter) {
     setLoading(false);
-    console.log(data)
+    const { data } = await axios.get(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=${filter}`
+    );
+
+    setNFTs(data);
+    setLoading(true);
   }
 
   return (
     <>
       <div>
-        <select id="filter-items" defaultValue="">
+        <select
+          id="filter-items"
+          defaultValue=""
+          onChange={(event) => filterNFTs(event.target.value)}
+        >
           <option value="">Default</option>
           <option value="price_low_to_high">Price, Low to High</option>
           <option value="price_high_to_low">Price, High to Low</option>
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {new Array(8).fill(0).map((_, index) => (
-        <div
-          key={index}
-          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-          style={{ display: "block", backgroundSize: "cover" }}
-        >
-          <div className="nft__item">
-            <div className="author_list_pp">
-              <Link
-                to="/author"
-                data-bs-toggle="tooltip"
-                data-bs-placement="top"
-              >
-                <img className="lazy" src={AuthorImage} alt="" />
-                <i className="fa fa-check"></i>
-              </Link>
-            </div>
-            <div className="de_countdown">5h 30m 32s</div>
-
-            <div className="nft__item_wrap">
-              <div className="nft__item_extra">
-                <div className="nft__item_buttons">
-                  <button>Buy Now</button>
-                  <div className="nft__item_share">
-                    <h4>Share</h4>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-facebook fa-lg"></i>
-                    </a>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-twitter fa-lg"></i>
-                    </a>
-                    <a href="">
-                      <i className="fa fa-envelope fa-lg"></i>
-                    </a>
-                  </div>
-                </div>
+      {nfts && loading ? (
+        nfts.slice(0, itemCount).map((nft, index) => (
+          <div
+            key={index}
+            className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+            style={{ display: "block", backgroundSize: "cover" }}
+          >
+            <div className="nft__item">
+              <div className="author_list_pp">
+                <Link
+                  to={`/author/${nft.authorId}`}
+                  data-bs-toggle="tooltip"
+                  data-bs-placement="top"
+                >
+                  <img className="lazy" src={nft.authorImage} alt="" />
+                  <i className="fa fa-check"></i>
+                </Link>
               </div>
-              <Link to="/item-details">
-                <img src={nftImage} className="lazy nft__item_preview" alt="" />
-              </Link>
-            </div>
-            <div className="nft__item_info">
-              <Link to="/item-details">
-                <h4>Pinky Ocean</h4>
-              </Link>
-              <div className="nft__item_price">1.74 ETH</div>
-              <div className="nft__item_like">
-                <i className="fa fa-heart"></i>
-                <span>69</span>
+              {nft.expiryDate && <Countdown expiryDate={nft.expiryDate} />}
+              <div className="nft__item_wrap">
+                <Link to={`/item-details/${nft.nftId}`}>
+                  <img
+                    src={nft.nftImage}
+                    className="lazy nft__item_preview"
+                    alt=""
+                  />
+                </Link>
+              </div>
+              <div className="nft__item_info">
+                <Link to={`/item-details/${nft.nftId}`}>
+                  <h4>{nft.title}</h4>
+                </Link>
+                <div className="nft__item_price">
+                  {nft.price} ETH<span>{nft.date}</span>
+                </div>
+                <div className="nft__item_like">
+                  <i className="fa fa-heart"></i>
+                  <span>{nft.likes}</span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))
+      ) : (
+        <>
+          {new Array(8).fill(0).map((_, index) => (
+            <div key={index} className="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+              <Skeleton width="100%" height="400px" />
+            </div>
+          ))}
+        </>
+      )}
       <div className="col-md-12 text-center">
-        <Link to="" id="loadmore" className="btn-main lead">
-          Load more
-        </Link>
+        {itemCount < 16 && (
+          <Link
+            onClick={() => setItemCount(itemCount + 4)}
+            to=""
+            id="loadmore"
+            className="btn-main lead"
+          >
+            Load more
+          </Link>
+        )}
       </div>
     </>
   );
