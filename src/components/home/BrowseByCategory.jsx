791c87a14cf73f1f5a1ab28fb2ef2b4371db9e51
Fatou <117177588+fatougit05@users.noreply.github.com>
2023-05-04 19:54:29 +0000

    Update BrowseByCategory.jsx

--- src/components/home/BrowseByCategory.jsx
+++ src/components/home/BrowseByCategory.jsx
@@ -1,16 +1,10 @@
-import React , {useEffect}from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import AOS from "aos";
-import "aos/dist/aos.css"
+
 const BrowseByCategory = () => {
-
-  useEffect(() =>{
-    AOS.init({duration:1600})
-  },[])
-
   return (
     <section id="section-category" className="no-top">
-      <div className="container" data-aos="fade-up">
+      <div className="container">
         <div className="row">
           <div className="col-lg-12">
             <div className="text-center">
