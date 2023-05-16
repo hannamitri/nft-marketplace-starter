79774f77e97402a88d9ef7d0b7ba77b4e432525d
ayoubeleshi <ayoubeleshi@gmail.com>
2023-05-16 09:50:20 +0100

    hot collections feature finished

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState,} from 'react';
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
-import OwlCarousel from 'react-owl-carousel';
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel/dist/assets/owl.theme.default.css';
+import Slider from 'react-slick';
 
-
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const HotCollections = () => {
   const [collections, setCollections] = useState([]);
@@ -13,7 +12,9 @@ const HotCollections = () => {
   useEffect(() => {
     const fetchCollections = async () => {
       try {
-        const response = await axios.get('https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections');
+        const response = await axios.get(
+          'https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections'
+        );
         setCollections(response.data);
       } catch (error) {
         console.error('Error fetching collections:', error);
@@ -23,27 +24,28 @@ const HotCollections = () => {
     fetchCollections();
   }, []);
 
-  const options = {
-    items: 4,
+  const settings = {
     dots: true,
-    nav: false,
-    autoplay: true,
-    autoplayHoverPause: true,
-    responsive: {
-      0: {
-        items: 1,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+        },
       },
-      576: {
-        items: 2,
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
       },
-      768: {
-        items: 3,
-      },
-      992: {
-        items: 4,
-      },
-    },
+    ],
   };
+
 
   return (
     <section id="section-collections" className="no-bottom">
@@ -55,10 +57,10 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          <OwlCarousel className="owl-theme" {...options}>
-            {collections.map((collection, index) => (
-              <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-                <div className="nft_coll">
+          <div className="col-lg-12">
+            <Slider {...settings}>
+              {collections.map((collection, index) => (
+                <div className="nft_coll" key={index}>
                   <div className="nft_wrap">
                     <Link to="/item-details">
                       <img src={collection.nftImage} className="lazy img-fluid" alt="" />
@@ -77,9 +79,9 @@ const HotCollections = () => {
                     <span>{collection.type}</span>
                   </div>
                 </div>
-              </div>
-            ))}
-          </OwlCarousel>
+              ))}
+            </Slider>
+          </div>
         </div>
       </div>
     </section>
