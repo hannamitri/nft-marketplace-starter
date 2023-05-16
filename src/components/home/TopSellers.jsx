d6f95be17f038fe4c72316d3657a92568ef06a34
withdrw <finesse@usa.com>
2023-05-16 15:09:26 -0500

    TopSeller done

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
+import axios from "axios";
 
 const TopSellers = () => {
+
+
+    const [sell , setSell] = useState()
+
+
+    useEffect(() => {
+      const sellData =  axios.get(`${`https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers`}`).then((response) => {
+        setSell(response.data)
+      })
+
+    },[])
+
+    console.log()
+
+    
+    
+    
+    
+
+
+
+
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,7 +38,7 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
+              {/* {new Array(12).fill(0).map((_, index) => (
                 <li key={index}>
                   <div className="author_list_pp">
                     <Link to="/author">
@@ -32,7 +55,29 @@ const TopSellers = () => {
                     <span>2.1 ETH</span>
                   </div>
                 </li>
-              ))}
+              ))} */}
+
+
+  
+
+              {sell?.map((sells) => (
+                <li key={sells?.id}>
+                <div className="author_list_pp">
+                <Link to={`/author/${sells.authorId}`}>
+                <img
+                className="lazy pp-author"
+                src={sells?.authorImage}
+                alt=""
+                />
+                <i className="fa fa-check"></i>
+                </Link>
+                </div>
+                <div className="author_list_info">
+                <Link to= {`/author/${sells.authorId}`} >{sells?.authorName}</Link>
+                <span>{sells?.price} ETH</span>
+                </div>
+                    </li>
+                    ))}
             </ol>
           </div>
         </div>
