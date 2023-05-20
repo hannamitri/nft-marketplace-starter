c806d67acc2996d99439c7a04b47b638a54c2d09
Ethan <ethan.nordahl1@gmail.com>
2023-05-19 18:58:17 -0700

    Changed the carousel to slick slider, also made it more responsive, and styled.

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
-import OwlCarousel from "react-owl-carousel";
-import "owl.carousel/dist/assets/owl.carousel.css";
-import "owl.carousel/dist/assets/owl.theme.default.css";
-import "../../css/styles/style.css";
 import { Link } from "react-router-dom";
+import OwlCarousel from "react-owl-carousel";
+// import "owl.carousel/dist/assets/owl.carousel.css";
+// import "owl.carousel/dist/assets/owl.theme.default.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "../../css/styles/style.css";
 import axios from "axios";
 import Skeleton from "../UI/Skeleton";
 
@@ -29,21 +32,70 @@ const HotCollections = () => {
     hotCollectionsData();
   }, []);
 
-  const carouselResponsive = {
-    0: {
-      items: 1,
-    },
-    550: {
-      items: 2,
-    },
-    1024: {
-      items: 3,
-    },
-    1400: {
-      items: 4,
-    },
-  };
 
+  // Arrow styling for carousel
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "flex" }}
+        onClick={onClick}
+      />
+    );
+  }
+  function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "flex" }}
+        onClick={onClick}
+      />
+    );
+  }
+
+  const settings = {
+    dots: false,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    infinite: true,
+    speed: 500,
+    slidesToScroll: 1,
+    slidesToShow: 4,
+    responsive: [
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          arrows: false,
+          slidesToScroll: 2,
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 1124,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
   return (
     <section id="section-collections" className="no-bottom">
@@ -58,48 +110,34 @@ const HotCollections = () => {
           <div className="carousel-container">
             {isLoading && (
               <>
-              <OwlCarousel
-                className="owl-theme"
-                loop
-                margin={10}
-                dots={false}
-                responsive={carouselResponsive}
-                nav
-                >
-                {skeletonArray.map((_, index) => (
-                  <div key={index}>
-                    <div className="nft_coll">
-                      <div className="nft_wrap">
-                        <Skeleton width={500} height={1000} />
-                      </div>
-                      <div className="nft_coll_pp">
-                        <Skeleton width={50} height={50} borderRadius={99} />
-                        <i className="fa fa-check"></i>
-                      </div>
-                      <div className="nft_coll_info">
-                        <h4>
-                          <Skeleton height={20} width="40%"/>
-                        </h4>
-                        <span>
-                          <Skeleton height={20} width="20%" />
-                        </span>
+                <Slider {...settings}>
+                  {skeletonArray.map((_, index) => (
+                    <div key={index}>
+                      <div className="nft_coll">
+                        <div className="nft_wrap">
+                          <Skeleton width={500} height={1000} />
+                        </div>
+                        <div className="nft_coll_pp">
+                          <Skeleton width={50} height={50} borderRadius={99} />
+                          <i className="fa fa-check"></i>
+                        </div>
+                        <div className="nft_coll_info">
+                          <h4>
+                            <Skeleton height={20} width="40%" />
+                          </h4>
+                          <span>
+                            <Skeleton height={20} width="20%" />
+                          </span>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                ))}
-              </ OwlCarousel>
+                  ))}
+                </Slider>
               </>
             )}
-              
+
             {!isLoading && (
-              <OwlCarousel
-                className="owl-theme"
-                loop
-                margin={10}
-                dots={false}
-                responsive={carouselResponsive}
-                nav
-              >
+              <Slider {...settings}>
                 {nftInfo.map((nft) => (
                   <div key={nft.id}>
                     <div className="nft_coll">
@@ -131,15 +169,13 @@ const HotCollections = () => {
                     </div>
                   </div>
                 ))}
-              </OwlCarousel>
+              </Slider>
             )}
           </div>
         </div>
       </div>
     </section>
   );
-  
 };
-
 
 export default HotCollections;
