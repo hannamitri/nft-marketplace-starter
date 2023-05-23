b7f932ab7089918f38cf81447b09e2703944953b
Armand <armandcodes@gmail.com>
2023-05-23 09:06:08 +0200

    finished TopSellers Feature

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import OwlCarousel from 'react-owl-carousel'
+import Skeleton from "../UI/Skeleton";
+import CountDown from '../CountDown'
 
 const NewItems = () => {
+  const [newItems, setNewItems] = useState([]);
+
+  const getNewItems = async () => {
+    const response = await axios.get(
+      "https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems"
+    );
+
+    setNewItems(response.data);
+  };
+
+  const options = {
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      900: {
+        items: 3,
+      },
+      1200: {
+        items: 4,
+      },
+    },
+  };
+
+  useEffect(() => {
+    getNewItems();
+  }, []);
+
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
@@ -14,62 +50,140 @@ const NewItems = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link
-                    to="/author"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    title="Creator: Monica Lucas"
-                  >
-                    <img className="lazy" src={AuthorImage} alt="" />
-                    <i className="fa fa-check"></i>
-                  </Link>
-                </div>
-                <div className="de_countdown">5h 30m 32s</div>
+          
+          {newItems.length ? (
+            <OwlCarousel className="owl-theme" {...options}>
+              {newItems.map((item, index) => (
+                <div className="nft__item" key={index}>
+                  <div className="author_list_pp">
+                    <Link
+                      to={`/author/${item.authorId}`}
+                      data-bs-toggle="tooltip"
+                      data-bs-placement="top"
+                      title="Creator: Monica Lucas"
+                    >
+                      <img className="lazy" src={item.authorImage} alt="" />
+                      <i className="fa fa-check"></i>
+                    </Link>
+                  </div>
+                  {item.expiryDate && (
+                    <CountDown expiryDate={item.expiryDate} />
+                  )}
 
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
+                  <div className="nft__item_wrap">
+                    <div className="nft__item_extra">
+                      <div className="nft__item_buttons">
+                        <button>Buy Now</button>
+                        <div className="nft__item_share">
+                          <h4>Share</h4>
+                          <a
+                            href="https://www.facebook.com/sharer/sharer.php?u=https://gigaland.io"
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            <i className="fa fa-facebook fa-lg"></i>
+                          </a>
+                          <a
+                            href="https://twitter.com/intent/tweet?url=https://gigaland.io"
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            <i className="fa fa-twitter fa-lg"></i>
+                          </a>
+                          <a href="mailto:?subject=I wanted you to see this site&amp;body=Check out this site https://gigaland.io">
+                            <i className="fa fa-envelope fa-lg"></i>
+                          </a>
+                        </div>
                       </div>
                     </div>
-                  </div>
 
-                  <Link to="/item-details">
-                    <img
-                      src={nftImage}
-                      className="lazy nft__item_preview"
-                      alt=""
-                    />
-                  </Link>
-                </div>
-                <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <div className="nft__item_price">3.08 ETH</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>69</span>
+                    <Link to={`/item-details/${item.nftId}`}>
+                      <img
+                        src={item.nftImage}
+                        className="lazy nft__item_preview"
+                        alt=""
+                      />
+                    </Link>
+                  </div>
+                  <div className="nft__item_info">
+                    <Link to={`/item-details/${item.nftId}`}>
+                      <h4>{item.title}</h4>
+                    </Link>
+                    <div className="nft__item_price">{item.price} ETH</div>
+                    <div className="nft__item_like">
+                      <i className="fa fa-heart"></i>
+                      <span>{item.likes}</span>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
-          ))}
+              ))}
+            </OwlCarousel>
+          ) : (
+            <>
+              <OwlCarousel className="owl-theme" {...options}>
+                {new Array(8).fill(0).map((_, index) => (
+                  <div className="nft__item" key={index}>
+                    <div className="author_list_pp">
+                      <Link
+                        to={``}
+                        data-bs-toggle="tooltip"
+                        data-bs-placement="top"
+                        title="Creator: Monica Lucas"
+                      >
+                        <Skeleton
+                          width="50px"
+                          height="50px"
+                          borderRadius="50%"
+                        />
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+
+                    <div className="nft__item_wrap">
+                      <div className="nft__item_extra">
+                        <div className="nft__item_buttons">
+                          <button>Buy Now</button>
+                          <div className="nft__item_share">
+                            <h4>Share</h4>
+                            <a
+                              href="https://www.facebook.com/sharer/sharer.php?u=https://gigaland.io"
+                              target="_blank"
+                              rel="noreferrer"
+                            >
+                              <i className="fa fa-facebook fa-lg"></i>
+                            </a>
+                            <a
+                              href="https://twitter.com/intent/tweet?url=https://gigaland.io"
+                              target="_blank"
+                              rel="noreferrer"
+                            >
+                              <i className="fa fa-twitter fa-lg"></i>
+                            </a>
+                            <a href="mailto:?subject=I wanted you to see this site&amp;body=Check out this site https://gigaland.io">
+                              <i className="fa fa-envelope fa-lg"></i>
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+
+                      <Link to={``}>
+                        <Skeleton width="100%" height="350px" />
+                      </Link>
+                    </div>
+                    <div className="nft__item_info">
+                      <Link to={``}>
+                        <Skeleton width="180px" height="30px" />
+                      </Link>
+                      <Skeleton width="100px" height="20px" />
+                    </div>
+                    <div className="nft__item_like">
+                      <Skeleton width="30px" height="15px" />
+                    </div>
+                  </div>
+                ))}
+              </OwlCarousel>
+            </>
+          )}
         </div>
       </div>
     </section>
--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
+import axios from "axios";
+import Skeleton from "../UI/Skeleton";
 
 const TopSellers = () => {
+  const [topSellers, setTopSellers] = useState([]);
+
+  async function fetchTopSellers() {
+    const { data } = await axios.get(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers`
+    );
+    setTopSellers(data);
+  };
+
+  useEffect(() => {
+    fetchTopSellers();
+  }, []);
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -13,27 +27,56 @@ const TopSellers = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          <div className="col-md-12">
-            <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
-                  </div>
-                </li>
-              ))}
-            </ol>
+          <div className="col-md-12 fadeIn">
+            {topSellers.length ? (
+              <ol className="author_list">
+                {topSellers.map((item, index) => (
+                  <li key={index}>
+                    <div className="author_list_pp">
+                      <Link to={`/author/${item.authorId}`}>
+                        <img
+                          className="lazy pp-author"
+                          src={item.authorImage}
+                          alt=""
+                        />
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <Link to={`/author/${item.authorId}`}>
+                        {item.authorName}
+                      </Link>
+                      <span>{item.price} ETH</span>
+                    </div>
+                  </li>
+                ))}
+              </ol>
+            ) : (
+              <ol className="author_list">
+                {new Array(12).fill(0).map((item, index) => (
+                  <li key={index}>
+                    <div className="author_list_pp">
+                      <Link to={``}>
+                        <Skeleton
+                          width="50px"
+                          height="50px"
+                          borderRadius="50%"
+                        />
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <Link to={``}>
+                        <Skeleton width="100px" height="20px" />
+                      </Link>
+                      <span>
+                        <Skeleton width="40px" height="20px" />
+                      </span>
+                    </div>
+                  </li>
+                ))}
+              </ol>
+            )}
           </div>
         </div>
       </div>
