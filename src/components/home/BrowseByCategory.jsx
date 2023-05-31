837909b5ba64c4bf1c1f3851ab4df06abf18ce0f
Justin Ong <justin-ong@live.com.au>
2023-05-31 23:10:28 +1000

    Animate on Scroll

--- src/components/home/BrowseByCategory.jsx
+++ src/components/home/BrowseByCategory.jsx
@@ -12,37 +12,55 @@ const BrowseByCategory = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
+          <div
+            className="col-md-2 col-sm-4 col-6 mb-sm-30"
+            data-aos="fade-left"
+            data-aos-delay="100">
             <Link to="/explore" className="icon-box style-2 rounded">
               <i className="fa fa-image"></i>
               <span>Art</span>
             </Link>
           </div>
-          <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
+          <div
+            className="col-md-2 col-sm-4 col-6 mb-sm-30"
+            data-aos="fade-left"
+            data-aos-delay="200">
             <Link to="/explore" className="icon-box style-2 rounded">
               <i className="fa fa-music"></i>
               <span>Music</span>
             </Link>
           </div>
-          <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
+          <div
+            className="col-md-2 col-sm-4 col-6 mb-sm-30"
+            data-aos="fade-left"
+            data-aos-delay="300">
             <Link to="/explore" className="icon-box style-2 rounded">
               <i className="fa fa-search"></i>
               <span>Domain Names</span>
             </Link>
           </div>
-          <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
+          <div
+            className="col-md-2 col-sm-4 col-6 mb-sm-30"
+            data-aos="fade-left"
+            data-aos-delay="400">
             <Link to="/explore" className="icon-box style-2 rounded">
               <i className="fa fa-globe"></i>
               <span>Virtual Worlds</span>
             </Link>
           </div>
-          <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
+          <div
+            className="col-md-2 col-sm-4 col-6 mb-sm-30"
+            data-aos="fade-left"
+            data-aos-delay="500">
             <Link to="/explore" className="icon-box style-2 rounded">
               <i className="fa fa-vcard"></i>
               <span>Trading Cards</span>
             </Link>
           </div>
-          <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
+          <div
+            className="col-md-2 col-sm-4 col-6 mb-sm-30"
+            data-aos="fade-left"
+            data-aos-delay="600">
             <Link to="/explore" className="icon-box style-2 rounded">
               <i className="fa fa-th"></i>
               <span>Collectibles</span>
--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import NFT from "../../images/nft.png";
 import backgroundImage from "../../images/bg-shape-1.jpg";
 import { Link } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init();
 
 const Landing = () => {
   return (
@@ -10,33 +13,62 @@ const Landing = () => {
       aria-label="section"
       className="no-top no-bottom vh-100"
       data-bgimage="url(images/bg-shape-1.jpg) bottom"
-      style={{ background: `url(${backgroundImage}) bottom / cover` }}
-    >
+      style={{ background: `url(${backgroundImage}) bottom / cover` }}>
       <div className="v-center">
         <div className="container">
           <div className="row align-items-center">
             <div className="col-md-6">
               <div className="spacer-single"></div>
               <h6>
-                <span className="text-uppercase id-color-2">
+                <span
+                  className="text-uppercase id-color-2"
+                  data-aos="fade-up"
+                  data-aos-delay="500"
+                  data-aos-duration="500"
+                  data-aos-easing="ease-in-out">
                   Ultraverse Market
                 </span>
               </h6>
               <div className="spacer-10"></div>
-              <h1>Create, sell or collect digital items.</h1>
-              <p className="lead">
+              <h1
+                data-aos="fade-up"
+                data-aos-delay="750"
+                data-aos-duration="750"
+                data-aos-easing="ease-in-out">
+                Create, sell or collect digital items.
+              </h1>
+              <p
+                className="lead"
+                data-aos="fade-up"
+                data-aos-delay="1000"
+                data-aos-duration="1000"
+                data-aos-easing="ease-in-out">
                 Unit of data stored on a digital ledger, called a blockchain,
                 that certifies a digital asset to be unique and therefore not
                 interchangeable
               </p>
               <div className="spacer-10"></div>
-              <Link className="btn-main lead" to="/explore">
+              <Link
+                className="btn-main lead"
+                data-aos="fade-up"
+                data-aos-delay="1250"
+                data-aos-duration="1250"
+                data-aos-easing="ease-in-out"
+                to="/explore">
                 Explore
               </Link>
               <div className="mb-sm-30"></div>
             </div>
             <div className="col-md-6 xs-hide">
-              <img src={NFT} className="lazy img-fluid" alt="" />
+              <img
+                src={NFT}
+                className="lazy img-fluid"
+                alt=""
+                data-aos="fade-in"
+                data-aos-delay="1250"
+                data-aos-duration="1250"
+                data-aos-easing="ease-in-out"
+              />
             </div>
           </div>
         </div>
