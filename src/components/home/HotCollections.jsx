c7f883d8dd4569fd368d522c1d685cd23548e9cb
Filip <filip.tokar@seznam.cz>
2023-06-05 13:40:39 +0200

    working,responsive carousel

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import axios from "axios";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import HotItem from "../utility/HotItem";
 
 const HotCollections = () => {
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
 
   const settings = {
     dots: false,
@@ -14,17 +14,45 @@ const HotCollections = () => {
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 1,
-  
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          dots: true,
+        }
+      }
+    ]
   };
 
   async function getData() {
-    let {data} = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections")
-    setData(data)
+    let { data } = await axios.get(
+      "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections"
+    );
+    setData(data);
   }
-  useEffect(()=> {
-    getData()
-  }, [])
-  
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -35,35 +63,11 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          
-          
-          {data.map((nft) => (
-            
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={nft.id}>
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nft.nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
-                </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={nft.AuthorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>{nft.title}</h4>
-                  </Link>
-                  <span>ERC-{nft.code}</span>
-                </div>
-              </div>
-            </div>
-            
-          ))}
-          
-        
+          <Slider {...settings}>
+            {data.map((nft) => (
+                <HotItem nft={nft} key={nft.key}/>
+            ))}
+          </Slider>
         </div>
       </div>
     </section>
---
+++ src/components/utility/HotItem.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function HotItem({ nft }) {
+  return (
+    <div className="nft_coll">
+      <div className="nft_wrap">
+        <Link to="/item-details">
+          <img src={nft.nftImage} className="lazy img-fluid" alt="" />
+        </Link>
+      </div>
+      <div className="nft_coll_pp">
+        <Link to="/author">
+          <img className="lazy pp-coll" src={nft.authorImage} alt="" />
+        </Link>
+        <i className="fa fa-check"></i>
+      </div>
+      <div className="nft_coll_info">
+        <Link to="/explore">
+          <h4>{nft.title}</h4>
+        </Link>
+        <span>ERC-{nft.code}</span>
+      </div>
+    </div>
+  );
+}
+
+export default HotItem;
