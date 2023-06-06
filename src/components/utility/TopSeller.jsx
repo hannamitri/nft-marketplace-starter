1629d54a48f16abd9819b0720f4b33208bf54505
Filip <filip.tokar@seznam.cz>
2023-06-06 21:07:40 +0200

    dynamically displaying data from API

---
+++ src/components/utility/TopSeller.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function TopSeller({seller}) {
+  return (
+    <li key={seller.id}>
+      <div className="author_list_pp">
+        <Link to={`/author/${seller.authorId}`}>
+          <img className="lazy pp-author" src={seller.authorImage} alt="" />
+          <i className="fa fa-check"></i>
+        </Link>
+      </div>
+      <div className="author_list_info">
+        <Link to={`/author/${seller.authorId}`}>{seller.authorName}</Link>
+        <span>{seller.price} ETH</span>
+      </div>
+    </li>
+  );
+}
+
+export default TopSeller;
