f7b97f249b05f165b91d0c88f332a43f3d77727a
Filip <filip.tokar@seznam.cz>
2023-06-07 15:24:07 +0200

    added skeleton loading state

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -2,27 +2,29 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import NewItem from "../utility/NewItem";
+import NewItemLoadingState from "../utility/NewItemLoadingState";
 
 const ExploreItems = () => {
   const [exploreData, setExploreData] = useState([]);
   const [dataSlice, setDataSlice] = useState(8);
+  const [loading, isLoading] = useState();
   let maxItems = exploreData.length;
   const increment = 4;
 
   async function getExploreData() {
+    isLoading(true);
     let { data } = await axios.get(
       "https://us-central1-nft-cloud-functions.cloudfunctions.net/explore"
     );
     setExploreData(data);
+    setTimeout(() => {
+      isLoading(false);
+    }, 2000);
   }
 
   const show4MoreItems = () => {
-    setDataSlice((dataSlice) => dataSlice + increment)
+    setDataSlice((dataSlice) => dataSlice + increment);
   };
-
-  useEffect(() => {
-    console.log(dataSlice);
-  }, [dataSlice]);
 
   useEffect(() => {
     getExploreData();
@@ -38,15 +40,27 @@ const ExploreItems = () => {
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {exploreData.slice(0, dataSlice).map((exploreItem) => (
-        <div
-          key={exploreItem.id}
-          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-          style={{ display: "block", backgroundSize: "cover" }}
-        >
-          <NewItem newItem={exploreItem} />
-        </div>
-      ))}
+      {!loading
+        ? exploreData.slice(0, dataSlice).map((exploreItem) => (
+            <div
+              key={exploreItem.id}
+              className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+              style={{ display: "block", backgroundSize: "cover" }}
+            >
+              <NewItem newItem={exploreItem} />
+            </div>
+          ))
+        : new Array(exploreData.length)
+            .fill(0)
+            .map((_, index) => 
+            <div
+              key={index}
+              className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+              style={{ display: "block", backgroundSize: "cover" }}
+              >
+              <NewItemLoadingState/>
+            </div>
+            )}
       {dataSlice < maxItems && (
         <div className="col-md-12 text-center">
           <Link
