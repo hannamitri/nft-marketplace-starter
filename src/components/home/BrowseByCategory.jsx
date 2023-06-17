554ff0ae14b6c88d10238187956e5203805400e3
Filip <filip.tokar@seznam.cz>
2023-06-17 21:54:28 +0200

    browse fade right

--- src/components/home/BrowseByCategory.jsx
+++ src/components/home/BrowseByCategory.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const BrowseByCategory = () => {
+  useEffect(()=> {
+    AOS.init()
+  },[])
   return (
     <section id="section-category" className="no-top">
-      <div className="container">
+      <div className="container" data-aos="fade-right" data-aos-duration="1250">
         <div className="row">
           <div className="col-lg-12">
             <div className="text-center">
