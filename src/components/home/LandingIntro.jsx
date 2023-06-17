611675ef9954623407969fd4ea7980733f64c8f4
Filip <filip.tokar@seznam.cz>
2023-06-17 21:33:32 +0200

    landingIntro animations

--- src/components/home/LandingIntro.jsx
+++ src/components/home/LandingIntro.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 
 const LandingIntro = () => {
+  useEffect(() => {
+    AOS.init();
+  }, [])
   return (
     <section id="section-intro" className="no-top no-bottom">
       <div className="container">
         <div className="row">
           <div className="col-lg-4 col-md-6 mb-sm-30">
-            <div className="feature-box f-boxed style-3">
+            <div className="feature-box f-boxed style-3" data-aos="fade-up" data-aos-duration="500">
               <i className="bg-color-2 i-boxed icon_wallet"></i>
               <div className="text">
                 <h4 className="">Set up your wallet</h4>
@@ -19,7 +25,7 @@ const LandingIntro = () => {
             </div>
           </div>
           <div className="col-lg-4 col-md-6 mb-sm-30">
-            <div className="feature-box f-boxed style-3">
+            <div className="feature-box f-boxed style-3" data-aos="fade-up" data-aos-duration="500">
               <i className="bg-color-2 i-boxed icon_cloud-upload_alt"></i>
               <div className="text">
                 <h4 className="">Add your NFT's</h4>
@@ -32,7 +38,7 @@ const LandingIntro = () => {
             </div>
           </div>
           <div className="col-lg-4 col-md-6 mb-sm-30">
-            <div className="feature-box f-boxed style-3">
+            <div className="feature-box f-boxed style-3" data-aos="fade-up" data-aos-duration="500">
               <i className="bg-color-2 i-boxed icon_tags_alt"></i>
               <div className="text">
                 <h4 className="">Sell your NFT's</h4>
