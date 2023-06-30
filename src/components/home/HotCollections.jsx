fb55a02f2a88b2e9d1798edc1e5a478068ef0a2b
Jonathan Lee <jonathanlee882@gmail.com>
2023-06-30 14:02:57 +1000

    printed the unique titles, author codes, author images and nftimages after retrieving data from a backed API

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -6,11 +6,14 @@ import nftImage from "../../images/nftImage.jpg";
 
 
 const HotCollections = () => {
+  const [data, setData] = useState([])
+
   useEffect(() => {
     axios.get('https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections')
       .then(response => {
         // Handle the successful response
-        console.log(response.data);
+        setData(response.data)
+        console.log(`lorem data is now ${data}`);
       })
       .catch(error => {
         // Handle the error
@@ -27,25 +30,26 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
+          {console.log(`hello? data is ${data}`)}
+          {data.map((element, index) => (
             <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
               <div className="nft_coll">
                 <div className="nft_wrap">
                   <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
+                    <img src={element.nftImage} className="lazy img-fluid" alt="" />
                   </Link>
                 </div>
                 <div className="nft_coll_pp">
                   <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
+                    <img className="lazy pp-coll" src={element.authorImage} alt="" />
                   </Link>
                   <i className="fa fa-check"></i>
                 </div>
                 <div className="nft_coll_info">
                   <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
+                    <h4>{element.title}</h4>
                   </Link>
-                  <span>ERC-192</span>
+                  <span>ERC-{element.code}</span>
                 </div>
               </div>
             </div>
@@ -55,5 +59,7 @@ const HotCollections = () => {
     </section>
   );
 };
+
+
 
 export default HotCollections;
