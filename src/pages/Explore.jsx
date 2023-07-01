761cc34be04cecb4fd866c351a256041041adea6
MichaelNgCen <michaelngcen@yahoo.com>
2023-07-01 16:59:24 -0400

    test commit

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import SubHeader from "../images/subheader.jpg";
 import ExploreItems from "../components/explore/ExploreItems";
 
+//Testing
 const Explore = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
