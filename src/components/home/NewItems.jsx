4727452e205fe618acad81b206d57633c26edfcb
milad <miladbannourah@outlook.com>
2023-07-05 19:43:53 +0300

    just fixed some styling issues with the carousal buttons

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -36,13 +36,13 @@ const NewItems = () => {
                       <button>Buy Now</button>
                       <div className="nft__item_share">
                         <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
+                        <a href="l" target="_blank" rel="noreferrer">
                           <i className="fa fa-facebook fa-lg"></i>
                         </a>
-                        <a href="" target="_blank" rel="noreferrer">
+                        <a href="l" target="_blank" rel="noreferrer">
                           <i className="fa fa-twitter fa-lg"></i>
                         </a>
-                        <a href="">
+                        <a href="l">
                           <i className="fa fa-envelope fa-lg"></i>
                         </a>
                       </div>
