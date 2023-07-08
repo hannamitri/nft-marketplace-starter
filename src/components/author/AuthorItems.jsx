b01a5c39ec44a59de0396deba0dfcd73c5dc9727
Roger Tan <rtan4955@uni.sydney.edu.au>
2023-07-08 11:16:07 +1000

    finished

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
 
-const AuthorItems = () => {
+const AuthorItems = ({collection, authorImage}) => {
   return (
     <div className="de_tab_content">
       <div className="tab-1">
         <div className="row">
-          {new Array(8).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+          {collection.map((item) => (
+            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={item.id}>
               <div className="nft__item">
                 <div className="author_list_pp">
                   <Link to="">
-                    <img className="lazy" src={AuthorImage} alt="" />
+                    <img className="lazy" src={authorImage} alt="authorPfp" />
                     <i className="fa fa-check"></i>
                   </Link>
                 </div>
@@ -37,7 +37,7 @@ const AuthorItems = () => {
                   </div>
                   <Link to="/item-details">
                     <img
-                      src={nftImage}
+                      src={item.nftImage }
                       className="lazy nft__item_preview"
                       alt=""
                     />
