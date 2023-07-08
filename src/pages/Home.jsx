a043656e29c23dabef597ee7da0b6433e6f90494
Rehan Ali <rali0010@student.monash.edu>
2023-07-08 16:52:06 +1000

    Top Sellers dynamic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect} from "react";
 import BrowseByCategory from "../components/home/BrowseByCategory";
 import HotCollections from "../components/home/HotCollections";
 import Landing from "../components/home/Landing";
 import LandingIntro from "../components/home/LandingIntro";
 import NewItems from "../components/home/NewItems";
 import TopSellers from "../components/home/TopSellers";
-import axios from "axios";
+
 
 const Home = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-
   
-
-  
-
-
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
