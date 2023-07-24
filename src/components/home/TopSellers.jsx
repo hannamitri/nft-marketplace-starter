99b556e5d53eb44c063aad0390ee779f8d1942b8
jose <hernandezjos1121@gmail.com>
2023-07-23 22:20:49 -0400

    added axios and fetched api from backend onto top sellers section

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
-
+import { useEffect, useState } from "react";
+import axios from "axios";
 const TopSellers = () => {
+  const [fetchedData, setFetchedData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    async function fetchCollections() {
+      try {
+        const response = await axios.get(
+          "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers"
+        );
+        const fetchData = response.data;
+        setFetchedData(fetchData);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+    }
+
+    fetchCollections();
+  }, []);
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
