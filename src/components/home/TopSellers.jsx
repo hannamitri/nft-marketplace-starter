04a76d6e1997e37d8f56f33537bdf6bbe7de30e5
jose <hernandezjos1121@gmail.com>
2023-07-23 22:45:21 -0400

    fixed a few bugs in top sellers

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Skeleton from "../UI/Skeleton";
 const TopSellers = () => {
   const [fetchedData, setFetchedData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const displaycount = Array(12).fill(null);
   useEffect(() => {
     async function fetchCollections() {
       try {
@@ -36,21 +38,42 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
+              {loading &&
+                displaycount.map((_, index) => (
+                  <li key={index}>
+                    <div className="author_list_pp">
+                      <Skeleton width={50} height={50} borderRadius={99} />
+                      <i className="fa fa-check"></i>
+                    </div>
+                    <div className="author_list_info">
+                      <h4>
+                        <Skeleton width={130} height={21} />
+                      </h4>
+
+                      <span>
+                        <Skeleton width={40} height={15} />
+                      </span>
+                    </div>
+                  </li>
+                ))}
+
+              {fetchedData.map((user) => (
+                <li key={user.id}>
                   <div className="author_list_pp">
-                    <Link to="/author">
+                    <Link to={`/author/${user.authorId}`}>
                       <img
                         className="lazy pp-author"
-                        src={AuthorImage}
+                        src={user.authorImage}
                         alt=""
                       />
                       <i className="fa fa-check"></i>
                     </Link>
                   </div>
                   <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
+                    <Link to={`/author/${user.authorId}`}>
+                      {user.authorName}
+                    </Link>
+                    <span>{user.price} ETH</span>
                   </div>
                 </li>
               ))}
