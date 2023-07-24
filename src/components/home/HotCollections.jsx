332068be1247ccfdc50eeea51db6556c42f0ab78
An10113 <antranthanh1011@gmail.com>
2023-07-24 23:30:29 +0700

    skeleton

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
@@ -10,14 +10,15 @@ import { faAngleLeft, faAngleRight, faArrowAltCircleLeft } from "@fortawesome/fr
 const HotCollections = () => {
   const [data, setData] = useState([])
   const [loading, setLoading] = useState(false)
-  useEffect(() => {
+  useEffect( () => {
     async function getData(){
-      const response = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections")
-      setData(response.data)
+      const response =await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections")
+        setData(response.data)
+        setLoading(true)
     }
     getData()
-    setLoading(true)
   },[])
+
 
   const slider = React.useRef(null);
 
@@ -60,10 +61,10 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div> 
           </div>
-          <div className="relative p-1">
+        <div className="relative p-1">
           <FontAwesomeIcon icon={faAngleLeft} className="hover:scale-110 transition-transform rounded-full w-4 h-4 p-2 left-0 border-black border-1 absolute top-[50%] bg-white" onClick={() => slider?.current?.slickPrev()} />
           <Slider {...settings}  ref={slider}> 
-          {setLoading ? data.map((nft) => (
+          {loading ? data.map((nft) => (
             <div className="px-2 sm:px-1" key={nft.id}>
               <div className="nft_coll">
                 <div className="nft_wrap">
@@ -73,26 +74,35 @@ const HotCollections = () => {
                 </div>
                 <div className="nft_coll_pp">
                   <Link to="/author">
-                    <img className="lazy pp-coll" src={nft.authorImage} alt="" />
+                  <img className="lazy pp-coll" src={nft.authorImage} alt="" />
                   </Link>
                   <i className="fa fa-check"></i>
                 </div>
                 <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>{nft.title}</h4>
-                  </Link>
-                  <span>ERC-{nft.code}</span>
+                <Link to="/explore">
+                <h4>{nft.title}</h4>
+                </Link>
+                <span>ERC-{nft.code}</span>
                 </div>
+                </div>
+                </div>
+                )) : (
+                  new Array(5).fill(0).map((_, index) => (
+                    <div className="px-2 sm:px-1" key={index}>
+                      <div className="nft_coll">
+                        <div className="nft_wrap h-[200px] skeleton"></div>
+                        <div className="nft_coll_pp w-[60px] h-[60px] rounded-full bg-gray-300"></div>
+                        <div className="nft_coll_info flex flex-col items-center">
+                          <div className="skeleton w-[90px] h-4"></div>
+                          <div className="h-2"></div>
+                          <div className="skeleton w-[50px] h-4"></div>
+                        </div>
+                      </div>
+                    </div>))
+                )}
+                </Slider>
+                <FontAwesomeIcon icon={faAngleRight} className="hover:scale-110 transition-transform p-2 rounded-full w-4 h-4 border-black border-1 absolute right-0 top-[50%] bg-white" onClick={() => slider?.current?.slickNext()} />
               </div>
-            </div>
-          )) : 
-          <>
-          
-          </>
-         }
-        </Slider>
-        <FontAwesomeIcon icon={faAngleRight} className="hover:scale-110 transition-transform p-2 rounded-full w-4 h-4 border-black border-1 absolute right-0 top-[50%] bg-white" onClick={() => slider?.current?.slickNext()} />
-        </div> 
         </div>
       </div>
     </section>
