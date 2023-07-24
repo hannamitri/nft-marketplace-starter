99781c288cd3769cdc53eb68670d627e192fd35d
Jonathan Lee <jonathanlee882@gmail.com>
2023-07-24 18:42:20 +1000

    added unique key to each child of a list

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -25,7 +25,6 @@ const ExploreItems = () => {
     async function getData() {
       let end = '';
       if (filter.length !== 0) end = `?filter=${filter}`
-      console.log(end)
       const response = await axios.get(
         `https://us-central1-nft-cloud-functions.cloudfunctions.net/explore${end}`
       );
@@ -46,67 +45,12 @@ const ExploreItems = () => {
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {/* {exploreData.map((element, index) => (
-        <div
-          key={index}
-          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-          style={{ display: "block", backgroundSize: "cover" }}
-        >
-          <div className="nft__item">
-            <div className="author_list_pp">
-              <Link
-                to="/author"
-                data-bs-toggle="tooltip"
-                data-bs-placement="top"
-              >
-                <img className="lazy" src={element.authorImage} alt="" />
-                <i className="fa fa-check"></i>
-              </Link>
-            </div>
-            <div className="de_countdown">5h 30m 32s</div>
-
-            <div className="nft__item_wrap">
-              <div className="nft__item_extra">
-                <div className="nft__item_buttons">
-                  <button>Buy Now</button>
-                  <div className="nft__item_share">
-                    <h4>Share</h4>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-facebook fa-lg"></i>
-                    </a>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-twitter fa-lg"></i>
-                    </a>
-                    <a href="">
-                      <i className="fa fa-envelope fa-lg"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <Link to="/item-details">
-                <img src={element.nftImage} className="lazy nft__item_preview" alt="" />
-              </Link>
-            </div>
-            <div className="nft__item_info">
-              <Link to="/item-details">
-                <h4>{element.title}</h4>
-              </Link>
-              <div className="nft__item_price">1.74 ETH</div>
-              <div className="nft__item_like">
-                <i className="fa fa-heart"></i>
-                <span>{element.likes}</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      ))} */}
 
       {loading 
       ? 
-        // insert skeleton state
         new Array(8).fill(0).map((element, index) => {
           return (
-            <div key={element.id} className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12" 
+            <div key={index} className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12" 
             style={{ display: "block", backgroundSize: "cover" }}
             >
             <div className="skeleton-box" style={{width: "100%", height: "400px"}}></div>
@@ -114,10 +58,9 @@ const ExploreItems = () => {
           )
         })
        : 
-        // insert loaded code
         exploreData.slice(0, loaded).map((element, index) => {
           return (
-            <div key={element.id} className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12" 
+            <div key={index} className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12" 
             style={{ display: "block", backgroundSize: "cover" }}
             >
             <UseItem card={element} />
