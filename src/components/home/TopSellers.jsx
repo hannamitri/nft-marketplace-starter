d6c8190d897c0979076472ab3a4a873d9dbf06b0
Torin <extrasmores@gmail.com>
2023-08-28 17:13:32 -0600

    finished top sellers feature

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
+import { async } from "q";
+import { func } from "prop-types";
+import axios from "axios";
+import Skeleton from "react-loading-skeleton";
 
 const TopSellers = () => {
+  const [topSellersData, setTopSellersData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  async function fetchTopSellers() {
+    const { data } = await axios.get(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers`
+    );
+    setTopSellersData(data);
+    setLoading(false);
+  }
+  useEffect(() => {
+    fetchTopSellers();
+  }, [loading]);
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,21 +33,40 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
+              {loading &&
+                new Array(12).fill(0).map((_, index) => (
+                  <li key={index}>
+                    <div className="author_list_pp">
+                      <Skeleton width={50} height={50} borderRadius={99} />
+                      <i className="fa fa-check"></i>
+                    </div>
+                    <div className="author_list_info">
+                      <h4>
+                        <Skeleton width={130} height={20} />
+                      </h4>
+                      <span>
+                        <Skeleton width={40} height={15} />
+                      </span>
+                    </div>
+                  </li>
+                ))}
+              {topSellersData.map((user) => (
+                <li key={user.id}>
                   <div className="author_list_pp">
-                    <Link to="/author">
+                    <Link to={`/author/${user.authorId}`}>
                       <img
                         className="lazy pp-author"
-                        src={AuthorImage}
+                        src={user.authorImage}
                         alt=""
                       />
                       <i className="fa fa-check"></i>
                     </Link>
                   </div>
                   <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
+                    <Link to={`/author/${user.authorId}`}>
+                      {user.authorName}
+                    </Link>
+                    <span>{user.price} ETH</span>
                   </div>
                 </li>
               ))}
