5f1894f312c05c0f6211333e7aacf6edd037ff83
AnmaritoS <95002766+AnmaritoS@users.noreply.github.com>
2023-09-02 14:47:44 -0400

    Responsive carousel done using react-slick library

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const HotCollections = () => {
   const [nftArray, setNftArray] = useState([]);
@@ -19,7 +22,63 @@ const HotCollections = () => {
 
     fetchData();
   }, []);
-  console.log(nftArray);
+
+  function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block" }}
+        onClick={onClick}
+      />
+    );
+  }
+
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block" }}
+        onClick={onClick}
+      />
+    );
+  }
+
+  var carouselSettings = {
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+    ],
+  };
 
   return (
     <section id="section-collections" className="no-bottom">
@@ -31,37 +90,39 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {nftArray.map((nft) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={nft.id}>
-              <div className="nft_coll">
-                <div className="nft_wrap ">
-                  <Link to="/item-details">
-                    <img
-                      src={nft.nftImage}
-                      className="lazy img-fluid "
-                      alt=""
-                    />
-                  </Link>
-                </div>
-                <div className="nft_coll_pp ">
-                  <Link to="/author">
-                    <img
-                      className="lazy pp-coll  "
-                      src={nft.authorImage}
-                      alt=""
-                    />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>{nft.title}</h4>
-                  </Link>
-                  <span>{`ERC-${nft.code}`}</span>
+          <Slider {...carouselSettings}>
+            {nftArray.map((nft) => (
+              <div className="carousel-slide" key={nft.id}>
+                <div className="nft_coll">
+                  <div className="nft_wrap ">
+                    <Link to="/item-details">
+                      <img
+                        src={nft.nftImage}
+                        className="lazy img-fluid "
+                        alt=""
+                      />
+                    </Link>
+                  </div>
+                  <div className="nft_coll_pp ">
+                    <Link to="/author">
+                      <img
+                        className="lazy pp-coll  "
+                        src={nft.authorImage}
+                        alt=""
+                      />
+                    </Link>
+                    <i className="fa fa-check"></i>
+                  </div>
+                  <div className="nft_coll_info">
+                    <Link to="/explore">
+                      <h4>{nft.title}</h4>
+                    </Link>
+                    <span>{`ERC-${nft.code}`}</span>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </Slider>
         </div>
       </div>
     </section>
