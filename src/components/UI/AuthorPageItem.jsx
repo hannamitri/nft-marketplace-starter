17f382ad08c99130801c30ba35b0974cb9de04d2
carloblvd <carloblvd@gmail.com>
2023-09-19 17:44:05 -0400

    small bug fix

--- src/components/UI/AuthorPageItem.jsx
+++ src/components/UI/AuthorPageItem.jsx
@@ -28,15 +28,6 @@ const AuthorPageItem = ({ nft, authorImage }) => {
                 <button>Buy Now</button>
                 <div className="nft__item_share">
                   <h4>Share</h4>
-                  <a href="" target="_blank" rel="noreferrer">
-                    <i className="fa fa-facebook fa-lg"></i>
-                  </a>
-                  <a href="" target="_blank" rel="noreferrer">
-                    <i className="fa fa-twitter fa-lg"></i>
-                  </a>
-                  <a href="">
-                    <i className="fa fa-envelope fa-lg"></i>
-                  </a>
                 </div>
               </div>
             </div>
