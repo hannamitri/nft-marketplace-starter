bd0544451d21194491621360db898f63258488a1
zubayr-parker <zubayr_parker@outlook.com>
2023-09-25 13:02:19 +1000

    added dynamic loading from backend API

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -2,8 +2,49 @@ import React from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const HotCollections = () => {
+  const [NFTs, setNFTs] = useState([]);
+
+  async function fetchNFTs() {
+    const { data } = await axios.get(
+      "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections"
+    );
+    setNFTs(data);
+  }
+
+  console.log(NFTs);
+  useEffect(() => {
+    fetchNFTs();
+  }, []);
+
+  function renderNFTs() {
+    return NFTs.map((NFT) => (
+      <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={NFT.id}>
+        <div className="nft_coll">
+          <div className="nft_wrap">
+            <Link to="/item-details">
+              <img src={NFT.nftImage} className="lazy img-fluid" alt="" />
+            </Link>
+          </div>
+          <div className="nft_coll_pp">
+            <Link to="/author">
+              <img className="lazy pp-coll" src={NFT.AuthorImage} alt="" />
+            </Link>
+            <i className="fa fa-check"></i>
+          </div>
+          <div className="nft_coll_info">
+            <Link to="/explore">
+              <h4>{NFT.title}</h4>
+            </Link>
+            <span>ERC-{NFT.code}</span>
+          </div>
+        </div>
+      </div>
+    ));
+  }
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,29 +55,7 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
-                </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <span>ERC-192</span>
-                </div>
-              </div>
-            </div>
-          ))}
+          {renderNFTs()}
         </div>
       </div>
     </section>
