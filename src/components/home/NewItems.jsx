51915210a746cc749ca4f147ba0bb2f159f59bbb
zubayr-parker <zubayr_parker@outlook.com>
2023-09-26 20:21:44 +1000

    removed loading variable from dependency array

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
 import axios from "axios";
 import Slider from "react-slick";
 import CountdownTimer from "../CountdownTimer";
@@ -15,13 +13,12 @@ const NewItems = () => {
       "https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems"
     );
     setNFTs(data);
-
     setLoading(false);
   }
 
   useEffect(() => {
     fetchNFTs();
-  }, [loading]);
+  }, []);
 
   const settings = {
     infinite: true,
@@ -70,7 +67,7 @@ const NewItems = () => {
               >
                 <div
                   className="skeleton-box"
-                  style={{ width: '50px', height: '50px', borderRadius: '50%' }}
+                  style={{ width: "50px", height: "50px", borderRadius: "50%" }}
                 ></div>
                 <i className="fa fa-check"></i>
               </a>
@@ -104,7 +101,7 @@ const NewItems = () => {
               <a href="/">
                 <div
                   className="skeleton-box"
-                  style={{ width: '100%', height: '350px' }}
+                  style={{ width: "100%", height: "350px" }}
                 ></div>
               </a>
             </div>
@@ -112,18 +109,18 @@ const NewItems = () => {
               <a href="/">
                 <div
                   className="skeleton-box"
-                  style={{ width: '180px', height: '30px' }}
+                  style={{ width: "180px", height: "30px" }}
                 ></div>
               </a>
               <div
                 className="skeleton-box"
-                style={{ width: '100px', height: '20px' }}
+                style={{ width: "100px", height: "20px" }}
               ></div>
             </div>
             <div className="nft__item_like">
               <div
                 className="skeleton-box"
-                style={{ width: '30px', height: '15px' }}
+                style={{ width: "30px", height: "15px" }}
               ></div>
             </div>
           </div>
@@ -147,7 +144,7 @@ const NewItems = () => {
             {NFT.expiryDate ? (
               <CountdownTimer expiryDate={NFT.expiryDate} />
             ) : null}
-  
+
             <div className="nft__item_wrap">
               <div className="nft__item_extra">
                 <div className="nft__item_buttons">
@@ -166,7 +163,7 @@ const NewItems = () => {
                   </div>
                 </div>
               </div>
-  
+
               <Link to={`/item-details/${NFT.nftId}`}>
                 <img
                   src={NFT.nftImage}
@@ -190,8 +187,7 @@ const NewItems = () => {
       </Slider>
     );
   }
-  
-  //testing...
+
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
@@ -203,63 +199,6 @@ const NewItems = () => {
             </div>
           </div>
           {renderNFTs()}
-
-          {/* {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link
-                    to="/author"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    title="Creator: Monica Lucas"
-                  >
-                    <img className="lazy" src={AuthorImage} alt="" />
-                    <i className="fa fa-check"></i>
-                  </Link>
-                </div>
-                <div className="de_countdown">5h 30m 32s</div>
-
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-
-                  <Link to="/item-details">
-                    <img
-                      src={nftImage}
-                      className="lazy nft__item_preview"
-                      alt=""
-                    />
-                  </Link>
-                </div>
-                <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <div className="nft__item_price">3.08 ETH</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>69</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))} */}
         </div>
       </div>
     </section>
