37418ef391c5f822f23b37fa997b9933e91c4dbf
zubayr-parker <zubayr_parker@outlook.com>
2023-09-26 21:35:52 +1000

    fetched backend api dynamically, skele loading, responsinveness, dynamic routing

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
+import axios from "axios";
 //testing...
 const TopSellers = () => {
+  const [NFTs, setNFTs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  async function fetchNFTs() {
+    const { data } = await axios.get(
+      "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers"
+    );
+    setNFTs(data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchNFTs();
+  }, []);
+  console.log(NFTs);
+
+  function renderNFTs() {
+    return loading
+      ? new Array(12).fill(0).map((_, index) => (
+          <li>
+            <div className="author_list_pp">
+              <a href="/">
+                <div
+                  className="skeleton-box"
+                  style={{ width: "50px", height: "50px", borderRadius: "50%" }}
+                ></div>
+                <i className="fa fa-check"></i>
+              </a>
+            </div>
+            <div className="author_list_info">
+              <a href="/">
+                <div
+                  className="skeleton-box"
+                  style={{ width: "100px", height: "20px" }}
+                ></div>
+              </a>
+              <span>
+                <div
+                  className="skeleton-box"
+                  style={{ width: "40px", height: "20px" }}
+                ></div>
+              </span>
+            </div>
+          </li>
+        ))
+      : NFTs.map((NFT, index) => (
+          <li key={index}>
+            <div className="author_list_pp">
+              <Link to={`author/${NFT.authorId}`}>
+                <img className="lazy pp-author" src={NFT.authorImage} alt="" />
+                <i className="fa fa-check"></i>
+              </Link>
+            </div>
+            <div className="author_list_info">
+              <Link to={`author/${NFT.authorId}`}>{NFT.authorName}</Link>
+              <span>{NFT.price} ETH</span>
+            </div>
+          </li>
+        ));
+  }
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -14,26 +76,7 @@ const TopSellers = () => {
             </div>
           </div>
           <div className="col-md-12">
-            <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
-                  </div>
-                </li>
-              ))}
-            </ol>
+            <ol className="author_list">{renderNFTs()}</ol>
           </div>
         </div>
       </div>
