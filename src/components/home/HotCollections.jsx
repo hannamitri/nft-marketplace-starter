c218335793091f81ea30d266eb1ca8f3da0eaff3
Haroon <haroonhakimi2005@gmail.com>
2023-10-11 20:19:06 -0400

    HotCollections

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -4,10 +4,9 @@ import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
 import axios from "axios";
 
-function HotCollections() 
-{
-  const [hotData, setHotData] = useState();
-  const [loading, setLoading] = useState();
+function HotCollections() {
+  const [hotData, setHotData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   function getHotData() {
     setLoading(true);
@@ -32,34 +31,44 @@ function HotCollections()
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {!loading ? 
-          hotData.map((nft) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" >
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nft.nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
+          {!loading
+            ? hotData.map((nft) => (
+                <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={nft.id}>
+                  <div className="nft_coll">
+                    <div className="nft_wrap">
+                      <Link to="/item-details">
+                        <img
+                          src={nft.nftImage}
+                          className="lazy img-fluid"
+                          alt=""
+                        />
+                      </Link>
+                    </div>
+                    <div className="nft_coll_pp">
+                      <Link to="/author">
+                        ``
+                        <img
+                          className="lazy pp-coll"
+                          src={nft.AuthorImage}
+                          alt=""
+                        />
+                      </Link>
+                      <i className="fa fa-check"></i>
+                    </div>
+                    <div className="nft_coll_info">
+                      <Link to="/explore">
+                        <h4>{nft.title}</h4>
+                      </Link>
+                      <span>ERC-{nft.code}</span>
+                    </div>
+                  </div>
                 </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    ``
-                    <img className="lazy pp-coll" src={nft.AuthorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>{nft.title}</h4>
-                  </Link>
-                  <span>ERC-{nft.code}</span>
-                </div>
-              </div>
+              ))
+            : new Array(4).fill(0).map((_, index) => 
+            <div key={index}>
+              Hi
             </div>
-)) : (
-            new Array(4).fill(0).map((_, index) => <>hi</>)
-          )
-          }
+            )}
         </div>
       </div>
     </section>
