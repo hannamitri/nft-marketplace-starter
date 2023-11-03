396aac162470ca837a5f5a10217b929eb71d84d2
Matthew Zimmerman <s20400608@gmail.com>
2023-11-03 01:35:57 -0700

    changed hot collections section

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,42 +1,158 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import Skeleton from "../UI/Skeleton";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
 
 const HotCollections = () => {
+  const [nftArray, setNftArray] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await axios.get(
+          "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections"
+          );
+        setNftArray(res.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    getData();
+  }, []);
+
+  function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "black" }}
+        onClick={onClick}
+      />
+    );
+  }
+
+  //Left arrow function for carousel
+
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "black"}}
+        onClick={onClick}
+      />
+    );
+  }
+
+  var carouselSettings = {
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+    ],
+  };
+
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
-        <div className="row">
+        <div className="row" data-aos="fade-in" data-aos-duration="1000">
           <div className="col-lg-12">
             <div className="text-center">
               <h2>Hot Collections</h2>
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
-                </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <span>ERC-192</span>
-                </div>
-              </div>
-            </div>
-          ))}
+          <Slider {...carouselSettings}>
+            {nftArray.length > 0
+              ? //Show nftArray if its length is superior to 0. In other words, when nftArray is fetched.
+                nftArray.map((nft) => (
+                  <div className="carousel-slide" key={nft?.id}>
+                    <div className="nft_coll mx-2">
+                      <div className="nft_wrap ">
+                        <Link to={`/item-details/${nft?.nftId}`}>
+                          <img
+                            src={nft?.nftImage}
+                            className="lazy img-fluid "
+                            alt=""
+                          />
+                        </Link>
+                      </div>
+                      <div className="nft_coll_pp ">
+                        <Link to={`/author/${nft?.authorId}`}>
+                          <img
+                            className="lazy pp-coll  "
+                            src={nft?.authorImage}
+                            alt=""
+                          />
+                        </Link>
+                        <i className="fa fa-check"></i>
+                      </div>
+                      <div className="nft_coll_info">
+                        <Link to="/explore">
+                          <h4>{nft?.title}</h4>
+                        </Link>
+                        <span>{`ERC-${nft?.code}`}</span>
+                      </div>
+                    </div>
+                  </div>
+                ))
+              : // Show skeleton loading state if nftArray.length isn't superior than 0. In other words, when nftArray isn't fetched. An array of length 5 is created then is filled with empty nft objects that have a loading state animation. I picked an array of length 5 instead of 6 to save memory.
+                Array.from({ length: 5 }).map((_, index) => (
+                  <div className="carousel-slide" key={index}>
+                    <div className="nft_coll">
+                      <div className="nft_wrap ">
+                        <Skeleton width={"100%"} height={"100%"} />
+                      </div>
+                      <div className="nft_coll_pp ">
+                        <Skeleton
+                          width={"60px"}
+                          height={"60px"}
+                          borderRadius={"25px"}
+                        />
+                        <i className="fa fa-check"></i>
+                      </div>
+                      <div>
+                        <Skeleton width={"120px"} height={"19.19px"} />
+                      </div>
+                      <Skeleton width={"90px"} height={"19.19px"} />
+                    </div>
+                  </div>
+                ))}
+          </Slider>
         </div>
       </div>
     </section>
