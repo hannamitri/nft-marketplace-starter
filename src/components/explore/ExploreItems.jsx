5b503263bfb0bae059a91f8031d545c6dbbf0dcb
Matthew Zimmerman <s20400608@gmail.com>
2023-11-07 08:17:04 -0700

    made explore page dynamic

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,77 +1,103 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import NftCard from "../common/NftCard";
+import Skeleton from "../UI/Skeleton";
 
 const ExploreItems = () => {
+  const [nftArray, setNftArray] = useState([]);
+  const [nftsToShow, setNftsToShow] = useState(8);
+  const [selectedFilter, setSelectedFilter] = useState(""); // State for selected filter
+
+  const loadMoreNfts = () => {
+    setNftsToShow(nftsToShow + 4);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=${selectedFilter}`
+        );
+        setNftArray(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [selectedFilter]);
+
+  // Function to handle filter selection change
+  const handleFilterChange = (event) => {
+    const selectedValue = event.target.value;
+    setSelectedFilter(selectedValue);
+  };
+
   return (
     <>
       <div>
-        <select id="filter-items" defaultValue="">
+        <select id="filter-items" defaultValue="" onChange={handleFilterChange}>
           <option value="">Default</option>
           <option value="price_low_to_high">Price, Low to High</option>
           <option value="price_high_to_low">Price, High to Low</option>
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {new Array(8).fill(0).map((_, index) => (
-        <div
-          key={index}
-          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-          style={{ display: "block", backgroundSize: "cover" }}
-        >
-          <div className="nft__item">
-            <div className="author_list_pp">
-              <Link
-                to="/author"
-                data-bs-toggle="tooltip"
-                data-bs-placement="top"
-              >
-                <img className="lazy" src={AuthorImage} alt="" />
-                <i className="fa fa-check"></i>
-              </Link>
+      {nftArray.length > 0
+        ? nftArray.slice(0, nftsToShow).map((nft) => (
+            <div
+              key={nft?.id}
+              className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+              style={{ display: "block", backgroundSize: "cover" }}
+            >
+              <NftCard nft={nft} />
             </div>
-            <div className="de_countdown">5h 30m 32s</div>
+          ))
+        : Array.from({ length: 8 }).map((_, index) => (
+            <div
+              key={index}
+              className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+              style={{ display: "block", backgroundSize: "cover" }}
+            >
+              <div className="nft__item">
+                <div className="author_list_pp">
+                  <Skeleton
+                    width={"60px"}
+                    height={"60px"}
+                    borderRadius={"25px"}
+                  />
+                  <i className="fa fa-check"></i>
+                </div>
 
-            <div className="nft__item_wrap">
-              <div className="nft__item_extra">
-                <div className="nft__item_buttons">
-                  <button>Buy Now</button>
-                  <div className="nft__item_share">
-                    <h4>Share</h4>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-facebook fa-lg"></i>
-                    </a>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-twitter fa-lg"></i>
-                    </a>
-                    <a href="">
-                      <i className="fa fa-envelope fa-lg"></i>
-                    </a>
+                <div className="nft__item_wrap">
+                  <Skeleton width={"100%"} height={"100%"} />
+                </div>
+
+                <div className="nft__item_info">
+                  <Skeleton width={"160px"} height={"20px"} marginTop={"4px"} />
+                  <div className="nft__item_price">
+                    <Skeleton width={"90px"} height={"20px"} />
+                  </div>
+                  <div className="nft__item_like">
+                    <Skeleton width={"28px"} height={"20px"} />
                   </div>
                 </div>
               </div>
-              <Link to="/item-details">
-                <img src={nftImage} className="lazy nft__item_preview" alt="" />
-              </Link>
             </div>
-            <div className="nft__item_info">
-              <Link to="/item-details">
-                <h4>Pinky Ocean</h4>
-              </Link>
-              <div className="nft__item_price">1.74 ETH</div>
-              <div className="nft__item_like">
-                <i className="fa fa-heart"></i>
-                <span>69</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      ))}
+          ))}
+
       <div className="col-md-12 text-center">
-        <Link to="" id="loadmore" className="btn-main lead">
-          Load more
-        </Link>
+        {nftsToShow < 16 && (
+          <Link
+            to=""
+            id="loadmore"
+            className="btn-main lead"
+            onClick={loadMoreNfts}
+          >
+            Load more
+          </Link>
+        )}
       </div>
     </>
   );
