33b152c0b1f872449be3df248235c68f8b2bb48e
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2023-11-20 10:25:07 -0800

    implemented an axios api request with lor hart

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
 
-const AuthorItems = () => {
+const AuthorItems = ({ author }) => {
   return (
     <div className="de_tab_content">
       <div className="tab-1">
@@ -23,13 +23,13 @@ const AuthorItems = () => {
                       <button>Buy Now</button>
                       <div className="nft__item_share">
                         <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
+                        <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
                           <i className="fa fa-facebook fa-lg"></i>
                         </a>
-                        <a href="" target="_blank" rel="noreferrer">
+                        <a href="https://twitter.com" target="_blank" rel="noreferrer">
                           <i className="fa fa-twitter fa-lg"></i>
                         </a>
-                        <a href="">
+                        <a href="mailto:?subject=Hello David! &amp;body=is this good enough?">
                           <i className="fa fa-envelope fa-lg"></i>
                         </a>
                       </div>
--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -4,6 +4,8 @@ import AuthorItems from "../components/author/AuthorItems";
 import { Link } from "react-router-dom";
 import AuthorImage from "../images/author_thumbnail.jpg";
 import axios from "axios";
+import Skeleton from "../components/UI/Skeleton";
+
 
 const Author = () => {
   const [author, setAuthor] = useState([]);
@@ -11,7 +13,7 @@ const Author = () => {
 
   async function fetchCollections() {
     const { data } = await axios.get(
-      "https://us-central1-nft-cloud-functions.cloudfunctions.net/itemDetails?nftId=17914494"
+      "https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=73855012"
     );
     setAuthor(data);
     setLoading(false);
@@ -34,49 +36,94 @@ const Author = () => {
           style={{ background: `url(${AuthorBanner}) top` }}
         ></section>
 
-        <section aria-label="section">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12">
-                <div className="d_profile de-flex">
-                  <div className="de-flex-col">
-                    <div className="profile_avatar">
-                      <img src={AuthorImage} alt="" />
+        {loading ? (
+           <div className="col-md-12">
+           <div className="d_profile de-flex">
+             <div className="de-flex-col">
+               <div className="profile_avatar">
+                 <Skeleton width={110} height={110} borderRadius={999} />
+                 <div className="profile_name">
+                   <h4>
+                     <Skeleton
+                       width={150}
+                       height={20}
+                       borderRadius={1}
+                     />
+                     <span className="profile_username">
+                       <Skeleton
+                         width={70}
+                         height={15}
+                         borderRadius={1}
+                       />
+                     </span>
+                     <span id="wallet" className="profile_wallet">
+                       <Skeleton
+                         height={13}
+                         width={150}
+                         borderRadius={1}
+                       />
+                     </span>
+                   </h4>
+                 </div>
+               </div>
+             </div>
+             <div className="profile_follow de-flex">
+               <div className="de-flex-col">
+                 <Skeleton width={90} height={25} borderRadius={1} />
+               </div>
+             </div>
+           </div>
+         </div>
+        ) : (
+          
+            <section aria-label="section">
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-12">
+                    <div className="d_profile de-flex">
+                      <div className="de-flex-col">
+                        <div className="profile_avatar">
+                          <img src={author.authorImage} alt="" />
 
-                      <i className="fa fa-check"></i>
-                      <div className="profile_name">
-                        <h4>
-                          Monica Lucas
-                          <span className="profile_username">@monicaaaa</span>
-                          <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
-                          </span>
-                          <button id="btn_copy" title="Copy Text">
-                            Copy
-                          </button>
-                        </h4>
+                          <i className="fa fa-check"></i>
+                          <div className="profile_name">
+                            <h4>
+                              {author.authorName}
+                              <span className="profile_username">
+                                @{author.tag}
+                              </span>
+                              <span id="wallet" className="profile_wallet">
+                                {author.address}
+                              </span>
+                              <button id="btn_copy" title="Copy Text">
+                                Copy
+                              </button>
+                            </h4>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="profile_follow de-flex">
+                        <div className="de-flex-col">
+                          <div className="profile_follower">
+                            {author.followers} followers
+                          </div>
+                          <Link to="#" className="btn-main">
+                            Follow
+                          </Link>
+                        </div>
                       </div>
                     </div>
                   </div>
-                  <div className="profile_follow de-flex">
-                    <div className="de-flex-col">
-                      <div className="profile_follower">573 followers</div>
-                      <Link to="#" className="btn-main">
-                        Follow
-                      </Link>
+                  <div className="col-md-12">
+                    <div className="de_tab tab_simple">
+                      <AuthorItems author={author}/>
                     </div>
                   </div>
                 </div>
               </div>
-
-              <div className="col-md-12">
-                <div className="de_tab tab_simple">
-                  <AuthorItems />
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
+            </section>
+          
+        )}
       </div>
     </div>
   );
