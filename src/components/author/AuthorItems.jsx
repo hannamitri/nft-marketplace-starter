5415b6792b7498b1ba46f9e8b5ed06c09758588f
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2023-12-08 16:30:02 -0800

    implemented a skeleton loading state, and a like and unlike button. when click the button increases and stay red, when clicked again it decreases and the color goes away

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -1,19 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
 
-const AuthorItems = ({ author }) => {
+const AuthorItems = ({ author, setAuthor }) => {
+  function likeToggle(index) {
+    const updatedNftCollection = author.nftCollection.map((item, like) => {
+      if (like === index) {
+        return {
+          ...item,
+          likes: item.likes + (item.Liked ? -1 : 1),
+          Liked: !item.Liked,
+        };
+      }
+      return item;
+    });
+
+    setAuthor((prevAuthor) => ({
+      ...prevAuthor,
+      nftCollection: updatedNftCollection,
+    }));
+  }
+
   return (
     <div className="de_tab_content">
       <div className="tab-1">
         <div className="row">
-          {new Array(8).fill(0).map((_, index) => (
+          {author.nftCollection.slice(0, 8).map((authors, index) => (
             <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
               <div className="nft__item">
                 <div className="author_list_pp">
                   <Link to="">
-                    <img className="lazy" src={AuthorImage} alt="" />
+                    <img className="lazy" src={author.authorImage} alt="" />
                     <i className="fa fa-check"></i>
                   </Link>
                 </div>
@@ -23,10 +39,18 @@ const AuthorItems = ({ author }) => {
                       <button>Buy Now</button>
                       <div className="nft__item_share">
                         <h4>Share</h4>
-                        <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
+                        <a
+                          href="https://www.facebook.com/"
+                          target="_blank"
+                          rel="noreferrer"
+                        >
                           <i className="fa fa-facebook fa-lg"></i>
                         </a>
-                        <a href="https://twitter.com" target="_blank" rel="noreferrer">
+                        <a
+                          href="https://twitter.com"
+                          target="_blank"
+                          rel="noreferrer"
+                        >
                           <i className="fa fa-twitter fa-lg"></i>
                         </a>
                         <a href="mailto:?subject=Hello David! &amp;body=is this good enough?">
@@ -35,9 +59,9 @@ const AuthorItems = ({ author }) => {
                       </div>
                     </div>
                   </div>
-                  <Link to="/item-details">
+                  <Link to={`/item-details/${authors.nftId}`}>
                     <img
-                      src={nftImage}
+                      src={authors.nftImage}
                       className="lazy nft__item_preview"
                       alt=""
                     />
@@ -45,12 +69,20 @@ const AuthorItems = ({ author }) => {
                 </div>
                 <div className="nft__item_info">
                   <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
+                    <h4>{authors.title}</h4>
                   </Link>
-                  <div className="nft__item_price">2.52 ETH</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>97</span>
+                  <div className="nft__item_price">{authors.price} ETH</div>
+                  <div
+                    className="nft__item_like"
+                    onClick={() => likeToggle(index)}
+                    style={{ color: authors.Liked ? "red" : "inherit" }}
+                  >
+                    <i
+                      className={`fa ${
+                        authors.Liked ? "fa-heart" : "fa-heart-o"
+                      }`}
+                    ></i>
+                    <span>{authors.likes}</span>
                   </div>
                 </div>
               </div>
