b361c733b81b9dab49d5cd5778e2d9fdea884eb4
Nuno Alves <nunowdev@gmail.com>
2023-12-20 16:32:05 +0000

    Implemented a slider carousel and skeleton loading

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -3,8 +3,46 @@ import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
 import Axios from "axios";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const HotCollections = () => {
+  const sliderSettings = {
+    dots: false,
+    infinite: true,
+    speed: 200,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   async function nftApiRequest() {
     setNftData([]);
     const requestedData = await Axios.get(
@@ -14,7 +52,7 @@ const HotCollections = () => {
     setNftData(requestedData.data);
   }
 
-  const [nftData, setNftData] = useState([""]);
+  const [nftData, setNftData] = useState([]);
 
   useEffect(() => {
     nftApiRequest();
@@ -30,36 +68,79 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {nftData.map((nft) => (
-            <div
-              className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
-              key={nft.code}
-            >
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nft.nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
-                </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img
-                      className="lazy pp-coll"
-                      src={nft.authorImage}
-                      alt=""
-                    />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>{nft.title}</h4>
-                  </Link>
-                  <span>ERC-{nft.code}</span>
-                </div>
-              </div>
-            </div>
-          ))}
+          <Slider {...sliderSettings}>
+            {nftData.length > 1
+              ? nftData.map((nft) => (
+                  <div className="nft_slider" key={nft.code}>
+                    <div className="nft_coll">
+                      <div className="nft_wrap">
+                        <Link to="/item-details">
+                          <img
+                            src={nft.nftImage}
+                            className="lazy img-fluid"
+                            alt=""
+                          />
+                        </Link>
+                      </div>
+                      <div className="nft_coll_pp">
+                        <Link to="/author">
+                          <img
+                            className="lazy pp-coll"
+                            src={nft.authorImage}
+                            alt=""
+                          />
+                        </Link>
+                        <i className="fa fa-check"></i>
+                      </div>
+                      <div className="nft_coll_info">
+                        <Link to="/explore">
+                          <h4>{nft.title}</h4>
+                        </Link>
+                        <span>ERC-{nft.code}</span>
+                      </div>
+                    </div>
+                  </div>
+                ))
+              : new Array(6).fill(0).map((_, index) => (
+                  <div className="nft_slider" key={index}>
+                    <div className="nft_coll">
+                      <div className="nft_wrap">
+                        <div
+                          class="skeleton-box"
+                          style={{ width: "100%", height: "93%" }}
+                        ></div>
+                      </div>
+                      <div className="nft_coll_pp">
+                        <div
+                          class="skeleton-box"
+                          style={{
+                            width: "50px",
+                            height: "50px",
+                            borderRadius: "50px",
+                          }}
+                        ></div>
+                        <i className="fa fa-check"></i>
+                      </div>
+                      <div className="nft_coll_info">
+                        <Link to="/explore">
+                          <h4>
+                            <div
+                              class="skeleton-box"
+                              style={{ width: "100px", height: "20px" }}
+                            ></div>
+                          </h4>
+                        </Link>
+                        <span>
+                          <div
+                            class="skeleton-box"
+                            style={{ width: "60px", height: "20px" }}
+                          ></div>
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+          </Slider>
         </div>
       </div>
     </section>
