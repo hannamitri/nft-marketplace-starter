278f3b21b202b227d298a71b837b82e3d1ee5974
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2023-12-24 17:36:37 -0800

    add AOS animationst

--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NFT from "../../images/nft.png";
 import backgroundImage from "../../images/bg-shape-1.jpg";
 import { Link } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Landing = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <section
       id="section-hero"
@@ -17,22 +22,31 @@ const Landing = () => {
           <div className="row align-items-center">
             <div className="col-md-6">
               <div className="spacer-single"></div>
-              <h6>
-                <span className="text-uppercase id-color-2">
-                  Ultraverse Market
-                </span>
-              </h6>
+              <div data-aos="fade-up" data-aos-duration="1000"data-aos-delay="100">
+                <h6>
+                  <span className="text-uppercase id-color-2">
+                    Ultraverse Market
+                  </span>
+                </h6>
+              </div>
+              <div data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
+                <div className="spacer-10"></div>
+                <h1>Create, sell or collect digital items.</h1>
+              </div>
+              <div data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300">
+                <p className="lead">
+                  Unit of data stored on a digital ledger, called a blockchain,
+                  that certifies a digital asset to be unique and therefore not
+                  interchangeable
+                </p>
+              </div>
               <div className="spacer-10"></div>
-              <h1>Create, sell or collect digital items.</h1>
-              <p className="lead">
-                Unit of data stored on a digital ledger, called a blockchain,
-                that certifies a digital asset to be unique and therefore not
-                interchangeable
-              </p>
-              <div className="spacer-10"></div>
-              <Link className="btn-main lead" to="/explore">
-                Explore
-              </Link>
+              <div data-aos="fade-up" data-aos-duration="1000" data-aos-delay="400">
+                <Link className="btn-main lead" to="/explore">
+                  Explore
+                </Link>
+              </div>
+
               <div className="mb-sm-30"></div>
             </div>
             <div className="col-md-6 xs-hide">
