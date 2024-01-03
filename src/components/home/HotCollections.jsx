8cd822d367cb441a37d2b7f040a673c1bd9e6986
CharlieH7 <hope91824@gmail.com>
2024-01-03 17:43:37 +1100

    added skeleton loading stage

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -3,16 +3,27 @@ import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
 import axios from 'axios';
+import SkeletonLoader from "../UI/SkeletonLoader";
 
-const HotCollections = () => { 
+const HotCollections = () => {
   const [hotCollections, setHotCollections] = useState([]);
-    async function fetchHotCollectionsData() {
+  const [loading, setLoading] = useState(true);
+
+  async function fetchHotCollectionsData() {
+    try {
       const { data } = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections");
-      setHotCollections(data.slice(0,6));
+      setHotCollections(data.slice(0, 6));
+    } catch (error) {
+      console.error("Error fetching hot collections data: ", error);
+    } finally {
+      setLoading(false);
     }
-    useEffect(() => {
-      fetchHotCollectionsData();
-    }, [])
+  }
+
+  useEffect(() => {
+    fetchHotCollectionsData();
+  }, []);
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -23,29 +34,39 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {hotCollections.map((collection) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={collection.id}>
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={collection.nftImage} className="lazy img-fluid" alt="collection.title" />
-                  </Link>
-                </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={collection.authorImage} alt={`Author ${collection.authorId}`} />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>{collection.title}</h4>
-                  </Link>
-                  <span>ERC-{collection.code}</span>
+          {loading ? (
+            // Render SkeletonLoader when data is being fetched
+            Array.from({ length: 6 }).map((_, index) => (
+              <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+                <SkeletonLoader />
+              </div>
+            ))
+          ) : (
+            // Render actual collection items when data is loaded
+            hotCollections.map((collection) => (
+              <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={collection.id}>
+                <div className="nft_coll">
+                  <div className="nft_wrap">
+                    <Link to="/item-details">
+                      <img src={collection.nftImage} className="lazy img-fluid" alt={collection.title} />
+                    </Link>
+                  </div>
+                  <div className="nft_coll_pp">
+                    <Link to="/author">
+                      <img className="lazy pp-coll" src={collection.authorImage} alt={`Author ${collection.authorId}`} />
+                    </Link>
+                    <i className="fa fa-check"></i>
+                  </div>
+                  <div className="nft_coll_info">
+                    <Link to="/explore">
+                      <h4>{collection.title}</h4>
+                    </Link>
+                    <span>ERC-{collection.code}</span>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))
+          )}
         </div>
       </div>
     </section>
