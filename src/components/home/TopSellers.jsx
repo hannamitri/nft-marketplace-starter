02bde2a3c9a6092c5180ec042c3e95ed2a8af37a
BrevenBennett <Breven.stars@gmail.com>
2024-01-11 19:21:17 -0600

    implemented top sellers feauture

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
+import axios from "axios";
+import Skeleton from "../UI/Skeleton";
 
 const TopSellers = () => {
+  const [topSellers, setTopSellers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function fetchTopSellersData() {
+    try {
+      setIsLoading(true);
+      const { data } = await axios.get(
+        "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers"
+      );
+      setTopSellers(data);
+    } catch (error) {
+      console.error("Couldn't load", error);
+    } finally {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 3000);
+    }
+  }
+
+  useEffect(() => {
+    fetchTopSellersData();
+  }, []);
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,24 +39,42 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
-                  </div>
-                </li>
-              ))}
+              {isLoading
+                ? new Array(12).fill(0).map((_, index) => (
+                    <li key={index}>
+                      <div className="author_list_pp">
+                        <Skeleton
+                          width="50px"
+                          height="50px"
+                          borderRadius={999}
+                        />
+                        <i className="fa fa-check"></i>
+                      </div>
+                      <div className="author_list_info">
+                        <Skeleton width="97px" height="21px" />
+                        <br/>
+                        <Skeleton width="50px" height="17px" />
+                      </div>
+                    </li>
+                  ))
+                : topSellers.map((topSeller) => (
+                    <li key={topSeller.id}>
+                      <div className="author_list_pp">
+                        <Link to={`/author/${topSeller.authorId}`}>
+                          <img
+                            className="lazy pp-author"
+                            src={topSeller.authorImage}
+                            alt=""
+                          />
+                          <i className="fa fa-check"></i>
+                        </Link>
+                      </div>
+                      <div className="author_list_info">
+                        <Link to={`/author/${topSeller.authorId}`}>{topSeller.authorName}</Link>
+                        <span>{topSeller.price} ETH</span>
+                      </div>
+                    </li>
+                  ))}
             </ol>
           </div>
         </div>
--- src/components/utils/Countdown.jsx
+++ src/components/utils/Countdown.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const Countdown = ({ expiryDate }) => {
   const [timeRemaining, setTimeRemaining] = useState(calculateTimeRemaining());
