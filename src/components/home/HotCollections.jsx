5d21736b61feb5fe5bd0755bb6dd9495562183c2
Kade <kadekorn@gmail.com>
2024-01-18 18:49:58 -0700

    added owl-carousel. Much much easier to use than react slick. I added loop and responsiveness. Now I need to change the fluid img

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+
 const HotCollections = () => {
   const [collections, setCollections] = useState([]);
 
@@ -12,7 +16,6 @@ const HotCollections = () => {
 
     Promise.all(urls.map((url) => axios.get(url)))
       .then((responses) => {
-        // Assuming each URL returns an array of collections
         const allData = responses.flatMap((res) => res.data);
         setCollections(allData);
       })
@@ -20,6 +23,26 @@ const HotCollections = () => {
         console.error("There was an error fetching the collections:", error);
       });
   }, []);
+
+  const options = {
+    margin: 10,
+    responsiveClass: true,
+    nav: true,
+    dots: false,
+    autoplay: false,
+    loop: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 4,
+      },
+    },
+  };
 
   return (
     <section id="section-collections" className="no-bottom">
@@ -31,32 +54,42 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {collections.map((collection) => (
-            <div
-              className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
-              key={collection.id}
-            >
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={collection.nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
-                </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={collection.authorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>{collection.title}</h4>
-                  </Link>
-                  <span>ERC-{collection.code}</span>
+          <OwlCarousel className="owl-theme" {...options}>
+            {collections.map((collection) => (
+              <div
+                // className="This was previously bootstrap"
+                key={collection.id}
+              >
+                <div className="nft_coll">
+                  <div className="nft_wrap">
+                    <Link to="/item-details">
+                      <img
+                        src={collection.nftImage}
+                        className="lazy img-fluid"
+                        alt=""
+                      />
+                    </Link>
+                  </div>
+                  <div className="nft_coll_pp">
+                    <Link to="/author">
+                      <img
+                        className="lazy pp-coll"
+                        src={collection.authorImage}
+                        alt=""
+                      />
+                    </Link>
+                    <i className="fa fa-check"></i>
+                  </div>
+                  <div className="nft_coll_info">
+                    <Link to="/explore">
+                      <h4>{collection.title}</h4>
+                    </Link>
+                    <span>ERC-{collection.code}</span>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </OwlCarousel>
         </div>
       </div>
     </section>
