5b2bd9f5db9e982f072dadb624778a76c49a92cf
Sanchaman Subba <sanchamansubba16@gmail.com>
2024-01-22 17:13:58 +0530

    added explore page functionality seccessfully

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -2,8 +2,12 @@ import React, { useEffect } from "react";
 import SubHeader from "../images/subheader.jpg";
 import ExploreItems from "../components/explore/ExploreItems";
 
+
+
+
 const Explore = () => {
   useEffect(() => {
+
     window.scrollTo(0, 0);
   }, []);
 
