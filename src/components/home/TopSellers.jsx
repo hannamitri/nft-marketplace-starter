bfe566b43f34a65254a0cfc499b352833726b3a9
orran <orran108@yahoo.co.uk>
2024-01-27 12:44:55 +0000

    top-sellers done

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
+import Skeleton from "../UI/Skeleton";
 
 const TopSellers = () => {
+  const [userData, setUserData] = useState([]);
+  const [skeleton, setSkeleton] = useState(true);
+
+  async function fetchUsers() {
+    const { data } = await axios.get(
+      "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers"
+    );
+    setUserData(data);
+    setSkeleton(false);
+  }
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const skeletonArray = Array.from({ length: 12 }).fill(null);
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,24 +33,39 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
-                  </div>
-                </li>
-              ))}
+              {skeleton
+                ? skeletonArray.map((_, index) => (
+                    <li key={index}>
+                      <div className="author_list_pp">
+                        <Skeleton width={50} height={50} borderRadius={50} />
+                        <i className="fa fa-check"></i>
+                      </div>
+                      <div className="author_list_info">
+                        <Skeleton width={100} height={20} borderRadius={4} />
+                        <Skeleton width={80} height={20} borderRadius={4} />
+                      </div>
+                    </li>
+                  ))
+                : userData.map((userProfile, index) => (
+                    <li key={index}>
+                      <div className="author_list_pp">
+                        <Link to={`/author/${userProfile.authorId}`}>
+                          <img
+                            className="lazy pp-author"
+                            src={userProfile.authorImage}
+                            alt=""
+                          />
+                          <i className="fa fa-check"></i>
+                        </Link>
+                      </div>
+                      <div className="author_list_info">
+                        <Link to={`/author/${userProfile.authorId}`}>
+                          {userProfile.authorName}
+                        </Link>
+                        <span>{userProfile.price} ETH</span>
+                      </div>
+                    </li>
+                  ))}
             </ol>
           </div>
         </div>
