0f0843f44f56e7d3955e07ba6b0612e60b338583
Kade <kadekorn@gmail.com>
2024-02-06 19:41:12 -0700

    Used Axios.get to dynamically display the hotcollectoin api section

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -4,7 +4,6 @@ import nftImage from "../../images/nftImage.jpg";
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
 
-const { data } = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections`)
 
 
 
@@ -36,25 +35,25 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+          {collections.map((collection, index) => (
+            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={collection.id || index}>
               <div className="nft_coll">
                 <div className="nft_wrap">
                   <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
+                    <img src={collection.nftImage} className="lazy img-fluid" alt="" />
                   </Link>
                 </div>
                 <div className="nft_coll_pp">
                   <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
+                    <img className="lazy pp-coll" src={collection.authorImage || AuthorImage} alt="" />
                   </Link>
                   <i className="fa fa-check"></i>
                 </div>
                 <div className="nft_coll_info">
                   <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
+                    <h4>{collection.title}</h4>
                   </Link>
-                  <span>ERC-192</span>
+                  <span>{`ERC - ${collection.code}`}</span>
                 </div>
               </div>
             </div>
