e6827a354d815ec082dba17dda4bef67f825fc9a
Kade <kadekorn@gmail.com>
2024-02-06 21:04:03 -0700

    I wanted to take it one step further and make the skeleton look more like the cards

--- src/components/UI/ShimmerEffect.jsx
+++ src/components/UI/ShimmerEffect.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import './ShimmerEffect.css'; // Make sure to import the CSS for the shimmer effect
+import './ShimmerEffect.css'; // Make sure this is the correct path to your CSS file
 
 const ShimmerEffect = () => (
   <div className="shimmer-wrapper">
     {/* Simulate an image placeholder */}
     <div className="shimmer-image shimmer"></div>
-    
-    {/* Simulate text placeholders */}
-    <div className="shimmer-text big shimmer"></div>
-    <div className="shimmer-text shimmer"></div>
+
+    {/* Simulate the profile picture placeholder */}
+    <div className="shimmer-profile-picture shimmer"></div>
+
+    {/* Simulate the main text placeholder */}
+    <div className="shimmer-text large shimmer"></div>
+
+    {/* Simulate the secondary text placeholder */}
+    <div className="shimmer-text medium shimmer"></div>
+
+    {/* Simulate the smaller text placeholder at the bottom */}
     <div className="shimmer-text small shimmer"></div>
   </div>
 );
