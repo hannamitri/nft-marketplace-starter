512c932ae90e8fbeff701197b100ef325ba7767a
Kade <kadekorn@gmail.com>
2024-02-20 06:37:57 -0700

    I have added all dynamic rendering, countdown, the load button and having it disappear when there is nothing more. Next is filter then skeleton loading state

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import Countdown from "../UI/Countdown";
 
 const ExploreItems = () => {
+  const [exploreItems, setExploreItemss] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [itemLimit, setItemLimit] = useState(8); // Initial item limit set to 8
+
+  useEffect(() => {
+    const fetchExploreItems = async () => {
+      setIsLoading(true);
+      try {
+        const response = await axios.get(
+          "https://us-central1-nft-cloud-functions.cloudfunctions.net/explore"
+        );
+        console.log("Fetched data:", response.data);
+        setExploreItemss(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchExploreItems();
+  }, []);
+
+  const loadMoreItems = () => {
+    setItemLimit((prevLimit) => prevLimit + 4); // Increase the item limit by 4 each time
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
       <div>
@@ -14,65 +45,69 @@ const ExploreItems = () => {
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {new Array(8).fill(0).map((_, index) => (
-        <div
-          key={index}
-          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-          style={{ display: "block", backgroundSize: "cover" }}
-        >
-          <div className="nft__item">
-            <div className="author_list_pp">
-              <Link
-                to="/author"
-                data-bs-toggle="tooltip"
-                data-bs-placement="top"
-              >
-                <img className="lazy" src={AuthorImage} alt="" />
-                <i className="fa fa-check"></i>
-              </Link>
-            </div>
-            <div className="de_countdown">5h 30m 32s</div>
-
-            <div className="nft__item_wrap">
-              <div className="nft__item_extra">
-                <div className="nft__item_buttons">
-                  <button>Buy Now</button>
-                  <div className="nft__item_share">
-                    <h4>Share</h4>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-facebook fa-lg"></i>
-                    </a>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-twitter fa-lg"></i>
-                    </a>
-                    <a href="">
-                      <i className="fa fa-envelope fa-lg"></i>
-                    </a>
+      {exploreItems.slice(0, itemLimit).map(
+        (
+          item,
+          index // Use slice to limit items
+        ) => (
+          <div
+            key={item.id || index}
+            className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+            style={{ display: "block", backgroundSize: "cover" }}
+          >
+            <div className="nft__item">
+              <div className="author_list_pp">
+                <Link
+                  to="/author"
+                  data-bs-toggle="tooltip"
+                  data-bs-placement="top"
+                >
+                  <img className="lazy" src={item.authorImage} alt="" />
+                  <i className="fa fa-check"></i>
+                </Link>
+              </div>
+              <div>
+                <Countdown expiryDate={item.expiryDate} />
+              </div>
+              <div className="nft__item_wrap">
+                <div className="nft__item_extra">
+                  <div className="nft__item_buttons">
+                    <button>Buy Now</button>
+                    <div className="nft__item_share">
+                      <h4>Share</h4>
+                      {/* Social links omitted for brevity */}
+                    </div>
                   </div>
                 </div>
+                <Link to="/item-details">
+                  <img
+                    src={item.nftImage}
+                    className="lazy nft__item_preview"
+                    alt=""
+                  />
+                </Link>
               </div>
-              <Link to="/item-details">
-                <img src={nftImage} className="lazy nft__item_preview" alt="" />
-              </Link>
-            </div>
-            <div className="nft__item_info">
-              <Link to="/item-details">
-                <h4>Pinky Ocean</h4>
-              </Link>
-              <div className="nft__item_price">1.74 ETH</div>
-              <div className="nft__item_like">
-                <i className="fa fa-heart"></i>
-                <span>69</span>
+              <div className="nft__item_info">
+                <Link to="/item-details">
+                  <h4>{item.title}</h4>
+                </Link>
+                <div className="nft__item_price">{item.price} ETH</div>
+                <div className="nft__item_like">
+                  <i className="fa fa-heart"></i>
+                  <span>{item.likes}</span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        )
+      )}
+      {itemLimit >= exploreItems.length ? null : (
       <div className="col-md-12 text-center">
-        <Link to="" id="loadmore" className="btn-main lead">
+        <button onClick={loadMoreItems} id="loadmore" className="btn-main lead">
           Load more
-        </Link>
+        </button>
       </div>
+      )}
     </>
   );
 };
