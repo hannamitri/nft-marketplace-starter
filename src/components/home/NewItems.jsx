f51a8346427877fa8408c051b7816d391a54c1c7
Niket Shah <shahniket643@gmail.com>
2024-03-17 12:35:57 +0530

    Added dynamic functionality to the New Items section with countdown functionality

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,9 +1,141 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+import Skeleton from "../UI/Skeleton";
+
+const useCountdown = (targetDate) => {
+  const [countdown, setCountdown] = useState("");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+      const distance = new Date(targetDate).getTime() - now.getTime();
+      if (distance < 0) {
+        clearInterval(interval);
+        setCountdown("00:00:00");
+        return;
+      }
+
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      setCountdown(
+        `${hours.toString().padStart(2, "0")}:${minutes
+          .toString()
+          .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`
+      );
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [targetDate]);
+
+  return countdown;
+};
+
+const Item = ({ item }) => {
+  const countdown = useCountdown(item.expiryDate);
+  return (
+    <div className="item">
+      <div className="nft__item">
+        <div className="author_list_pp">
+          <Link
+            to="/author"
+            data-bs-toggle="tooltip"
+            data-bs-placement="top"
+            title="Creator: Monica Lucas"
+          >
+            <img className="lazy" src={item.authorImage} alt="" />
+            <i className="fa fa-check"></i>
+          </Link>
+        </div>
+        {item.expiryDate ? (
+          <div className="de_countdown">{countdown}</div>
+        ) : (
+          ""
+        )}
+        <div className="nft__item_wrap">
+          <div className="nft__item_extra">
+            <div className="nft__item_buttons">
+              <button>Buy Now</button>
+              <div className="nft__item_share">
+                <h4>Share</h4>
+                <a href="/" target="_blank" rel="noreferrer">
+                  <i className="fa fa-facebook fa-lg"></i>
+                </a>
+                <a href="/" target="_blank" rel="noreferrer">
+                  <i className="fa fa-twitter fa-lg"></i>
+                </a>
+                <a href="/">
+                  <i className="fa fa-envelope fa-lg"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <Link to="/item-details">
+            <img
+              src={item.nftImage}
+              className="lazy nft__item_preview"
+              alt=""
+            />
+          </Link>
+        </div>
+        <div className="nft__item_info">
+          <Link to="/item-details">
+            <h4>{item.title}</h4>
+          </Link>
+          <div className="nft__item_price">{item.price}</div>
+          <div className="nft__item_like">
+            <i className="fa fa-heart"></i>
+            <span>{item.likes}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const NewItems = () => {
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems"
+        );
+        setItems(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setTimeout(() => {
+          setLoading(false);
+        }, 1500);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const options = {
+    loop: true,
+    nav: true,
+    dots: false,
+    margin: 20,
+    responsive: {
+      1440: { items: 4 },
+      1024: { items: 3 },
+      768: { items: 2 },
+      375: { items: 1 },
+    },
+  };
+
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
@@ -14,66 +146,40 @@ const NewItems = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link
-                    to="/author"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    title="Creator: Monica Lucas"
-                  >
-                    <img className="lazy" src={AuthorImage} alt="" />
-                    <i className="fa fa-check"></i>
-                  </Link>
-                </div>
-                <div className="de_countdown">5h 30m 32s</div>
-
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
+          <OwlCarousel className="owl-theme" {...options}>
+            {items.length === 0 && loading
+              ? new Array(4).fill(0).map((_, index) => (
+                <div className=" col-lg-12 col-md-12 col-xs-12" key={index}>
+                  <div className="nft_coll nft_coll--loading">
+                    <div className="nft_wrap">
+                      <Skeleton width="100%" height="80%" />
+                    </div>
+                    <div className="nft_coll_pp--loading">
+                      <Skeleton
+                        width="60px"
+                        height="60px"
+                        borderRadius={'50%'}
+                      />
+                    </div>
+                    <div className="nft_coll_info--loading">
+                      <Skeleton width="120px" height="18px" />
+                      <div className="margin">
+                        <Skeleton width="56px" height="18px" />
                       </div>
                     </div>
                   </div>
-
-                  <Link to="/item-details">
-                    <img
-                      src={nftImage}
-                      className="lazy nft__item_preview"
-                      alt=""
-                    />
-                  </Link>
                 </div>
-                <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <div className="nft__item_price">3.08 ETH</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>69</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
+              )) : (
+                items.map((item) => (
+                  <Item key={item.id} item={item} />
+                ))
+              )}
+          </OwlCarousel>
         </div>
       </div>
     </section>
   );
+
 };
 
 export default NewItems;
