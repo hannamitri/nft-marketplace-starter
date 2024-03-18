0a989cde7193399f7bdd553a55b521d5b222a10a
ExoPolit <exopolit@gmx.de>
2024-03-18 11:51:18 +0100

    top sellers dynamic, authorId added to Link

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
+import axios from "axios";
+
+const API_URL =
+  "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers";
 
 const TopSellers = () => {
+  const [newItem, setNewItem] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const { data } = await axios.get(API_URL);
+      setNewItem(data);
+      console.log(data)
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,21 +33,21 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
+              {newItem.map((item, index) => (
                 <li key={index}>
                   <div className="author_list_pp">
-                    <Link to="/author">
+                    <Link to={`/author/${item.authorId}`}>
                       <img
                         className="lazy pp-author"
-                        src={AuthorImage}
+                        src={item.authorImage}
                         alt=""
                       />
                       <i className="fa fa-check"></i>
                     </Link>
                   </div>
                   <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
+                    <Link to="/author">{item.authorName}</Link>
+                    <span>{item.price} ETH</span>
                   </div>
                 </li>
               ))}
